4df45d05317fd672f4e177f561b1a5fc7dcba64f
benfry <fry@processing.org>
2001-08-06 02:02:44 +0000

    lots of changes from sunday/monday morning in japan

--- processing/app/PdeException.java
+++ processing/app/PdeException.java
@@ -4,12 +4,23 @@ public class PdeException extends Exception {
   public PdeException() { }
 
   public PdeException(String message) {
-    super(message);
+    super(massage(message));
+    //System.out.println("message for this error is " + message);
   }
 
   public PdeException(String message, int line) {
-    super(message);
+    super(massage(message));
     this.line = line;
+  }
+
+  // make static so that super() can call it
+  static public final String massage(String msg) {
+    if (msg.indexOf("java.lang.") == 0) {
+      //int dot = msg.lastIndexOf('.');
+      msg = msg.substring("java.lang.".length());
+    }
+    return msg;
+    //return (dot == -1) ? msg : msg.substring(dot+1);
   }
 
 #ifndef PLAYER
