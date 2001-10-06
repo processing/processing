52121d46d0b20994b35623a1916967a384345545
benfry <fry@processing.org>
2001-10-06 21:23:28 +0000

    wrapping up major changes to bagel, fixed bagelpublic to now be auto with make.bat, misc todos

--- app/ProcessingApplet.java
+++ app/ProcessingApplet.java
@@ -491,22 +491,6 @@ public class ProcessingApplet extends Applet
 
   // ------------------------------------------------------------
 
-
-  public void rotateX(float angle) {
-    g.rotate(angle, 1, 0, 0);
-  }
-
-  public void rotateY(float angle) {
-    g.rotate(angle, 0, 1, 0);
-  }
-
-  public void rotateZ(float angle) {
-    g.rotate(angle, 0, 0, 1);
-  }
-
-
-  // ------------------------------------------------------------
-
   // math stuff for convenience
 
 
@@ -591,253 +575,367 @@ public class ProcessingApplet extends Applet
 
   // ------------------------------------------------------------
 
+  // public functions from bagel
+
 
   public void beginFrame() {
     g.beginFrame();
   }
 
+
+  public void clear() {
+    g.clear();
+  }
+
+
   public void endFrame() {
     g.endFrame();
   }
+
 
   public void beginShape(int kind) {
     g.beginShape(kind);
   }
 
+
   public void textureImage(BagelImage image) {
     g.textureImage(image);
   }
+
 
   public void vertexTexture(float u, float v) {
     g.vertexTexture(u, v);
   }
 
+
   public void vertexNormal(float nx, float ny, float nz) {
     g.vertexNormal(nx, ny, nz);
   }
+
 
   public void vertex(float x, float y) {
     g.vertex(x, y);
   }
 
+
   public void vertex(float x, float y, float z) {
     g.vertex(x, y, z);
   }
+
+
+  public void bezierVertex(float x, float y) {
+    g.bezierVertex(x, y);
+  }
+
+
+  public void catmullRomVertex(float x, float y) {
+    g.catmullRomVertex(x, y);
+  }
+
 
   public void endShape() {
     g.endShape();
   }
 
+
   public void point(float x, float y) {
     g.point(x, y);
   }
+
 
   public void point(float x, float y, float z) {
     g.point(x, y, z);
   }
 
+
   public void line(float x1, float y1, float x2, float y2) {
     g.line(x1, y1, x2, y2);
   }
+
 
   public void line(float x1, float y1, float z1, 
 		   float x2, float y2, float z2) {
     g.line(x1, y1, z1, x2, y2, z2);
   }
 
-  public void bezierCurve(float x1, float y1, float x2, float y2, 
-			  float x3, float y3, float x4, float y4) {
-    g.bezierCurve(x1, y1, x2, y2, x3, y3, x4, y4);
-  }
-
-  public void drawBezier(float inX[], float inY[]) {
-    g.drawBezier(inX, inY);
-  }
-
-  public void catmullRomCurve(float x1, float y1, 
-			      float cx1, float cy1,
-			      float cx2, float cy2, 
-			      float x2, float y2) {
-    g.catmullRomCurve(x1, y1, cx1, cy1, cx2, cy2, x2, y2);
-  }
 
   public void rect(float x1, float y1, float x2, float y2) {
     g.rect(x1, y1, x2, y2);
   }
 
+
   public void cube(float size) {
     g.cube(size);
   }
+
 
   public void box(float x1, float y1, float z1,
 		  float x2, float y2, float z2) {
     g.box(x1, y1, z1, x2, y2, z2);
   }
 
+
   public void circle(float x, float y, float radius) {
     g.circle(x, y, radius);
   }
 
-  public void sphere(float radius) {
-    g.sphere(radius);
+
+  public void oval(float x, float y, float hradius, float vradius) {
+    g.oval(x, y, hradius, vradius);
   }
+
+
+  public void sphere(float r) {
+    g.sphere(r);
+  }
+
 
   public void potato() {
     g.potato();
   }
 
-  public void imageRect(float x1, float y1, float x2, float y2,
-			BagelImage image) {
-    g.imageRect(x1, y1, x2, y2, image);
+
+  public void image(BagelImage image, float x1, float y1) {
+    g.image(image, x1, y1);
   }
 
-  public void imageRect(float x1, float y1, float x2, float y2,
-			BagelImage image, float maxU, float maxV) {
-    g.imageRect(x1, y1, x2, y2, image, maxU, maxV);
+
+  public void image(BagelImage image, 
+		    float x1, float y1, float x2, float y2) {
+    g.image(image, x1, y1, x2, y2);
   }
 
-  public void loadIdentityMatrix() {
-    g.loadIdentityMatrix();
+
+  public void image(BagelImage image, 
+		    float x1, float y1, float x2, float y2,
+		    float u1, float v1, float u2, float v2) {
+    g.image(image, x1, y1, x2, y2, u1, v1, u2, v2);
   }
+
+
+  public void loadFont(String s) {
+    g.loadFont(s);
+  }
+
+
+  public void setFont(String s) {
+    g.setFont(s);
+  }
+
+
+  public void text(char c) {
+    g.text(c);
+  }
+
+
+  public void text(String s) {
+    g.text(s);
+  }
+
 
   public void push() {
     g.push();
   }
 
+
   public void pop() {
     g.pop();
   }
 
-  public void multMatrix(float n00, float n01, float n02, float n03,
-			 float n10, float n11, float n12, float n13,
-			 float n20, float n21, float n22, float n23,
-			 float n30, float n31, float n32, float n33) {
-    g.multMatrix(n00, n01, n02, n03, n10, n11, n12, n13, n20, n21, n22, n23, n30, n31, n32, n33);
+
+  public void mult_matrix(float n00, float n01, float n02, float n03,
+			  float n10, float n11, float n12, float n13,
+			  float n20, float n21, float n22, float n23,
+			  float n30, float n31, float n32, float n33) {
+    g.mult_matrix(n00, n01, n02, n03, n10, n11, n12, n13, n20, n21, n22, n23, n30, n31, n32, n33);
   }
 
-  public void setupProjection(int projectionMode) {
-    g.setupProjection(projectionMode);
+
+  public void setupProjection(int projection) {
+    g.setupProjection(projection);
   }
+
 
   public void projectPoint(float x, float y, float z) {
     g.projectPoint(x, y, z);
   }
 
+
+  public void translate(float tx, float ty) {
+    g.translate(tx, ty);
+  }
+
+
   public void translate(float tx, float ty, float tz) {
     g.translate(tx, ty, tz);
   }
+
+
+  public void shearX(float angle) {
+    g.shearX(angle);
+  }
+
+
+  public void shearY(float angle) {
+    g.shearY(angle);
+  }
+
+
+  public void rotate(float angle) {
+    g.rotate(angle);
+  }
+
+
+  public void rotateX(float angle) {
+    g.rotateX(angle);
+  }
+
+
+  public void rotateY(float angle) {
+    g.rotateY(angle);
+  }
+
+
+  public void rotateZ(float angle) {
+    g.rotateZ(angle);
+  }
+
 
   public void rotate(float angle, float v0, float v1, float v2) {
     g.rotate(angle, v0, v1, v2);
   }
 
-  public void scale(float xyz) {
-    g.scale(xyz);
+
+  public void scale(float s) {
+    g.scale(s);
   }
+
 
   public void scale(float x, float y, float z) {
     g.scale(x, y, z);
   }
 
-  public void multWithPerspective() {
-    g.multWithPerspective();
+
+  public void transform(float n00, float n01, float n02, float n03,
+			float n10, float n11, float n12, float n13,
+			float n20, float n21, float n22, float n23,
+			float n30, float n31, float n32, float n33) {
+    g.transform(n00, n01, n02, n03, n10, n11, n12, n13, n20, n21, n22, n23, n30, n31, n32, n33);
   }
+
 
   public void colorMode(int colorMode) {
     g.colorMode(colorMode);
   }
 
+
   public void colorMode(int colorMode, int max) {
     g.colorMode(colorMode, max);
   }
+
 
   public void colorMode(int colorMode, 
 			int maxX, int maxY, int maxZ, int maxA) {
     g.colorMode(colorMode, maxX, maxY, maxZ, maxA);
   }
 
-  public void colorScale(int max) {
-    g.colorScale(max);
-  }
-
-  public void colorScale(int maxX, int maxY, int maxZ, int maxA) {
-    g.colorScale(maxX, maxY, maxZ, maxA);
-  }
 
   public void noFill() {
     g.noFill();
   }
 
+
   public void fill(float gray) {
     g.fill(gray);
   }
+
 
   public void fill(float gray, float alpha) {
     g.fill(gray, alpha);
   }
 
+
   public void fill(float x, float y, float z) {
     g.fill(x, y, z);
   }
+
 
   public void fill(float x, float y, float z, float a) {
     g.fill(x, y, z, a);
   }
 
+
   public void strokeWidth(float strokeWidth) {
     g.strokeWidth(strokeWidth);
   }
+
 
   public void noStroke() {
     g.noStroke();
   }
 
+
   public void stroke(float gray) {
     g.stroke(gray);
   }
+
 
   public void stroke(float gray, float alpha) {
     g.stroke(gray, alpha);
   }
 
+
   public void stroke(float x, float y, float z) {
     g.stroke(x, y, z);
   }
+
 
   public void stroke(float x, float y, float z, float a) {
     g.stroke(x, y, z, a);
   }
 
-  public void clear() {
-    g.clear();
-  }
 
   public void noBackground() {
     g.noBackground();
   }
 
+
   public void background(float gray) {
     g.background(gray);
   }
+
 
   public void background(float x, float y, float z) {
     g.background(x, y, z);
   }
 
+
   public void lightsOn() {
     g.lightsOn();
   }
+
 
   public void lightsOff() {
     g.lightsOff();
   }
 
+
+  public void smoothingOn() {
+    g.smoothingOn();
+  }
+
+
+  public void smoothingOff() {
+    g.smoothingOff();
+  }
+
+
   public void message(int level, String message) {
     g.message(level, message);
   }
 
+
   public void message(int level, String message, Exception e) {
     g.message(level, message, e);
   }
-
 }
