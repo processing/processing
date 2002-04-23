e40e51c0af4fd086ed1c120390edc98e974300ec
benfry <fry@processing.org>
2002-04-23 12:33:36 +0000

    forgot to add lots of things for the new version

---
+++ app/PdeEditorOutput.java
@@ -0,0 +1,11 @@
+import java.awt.*;
+import java.io.*;
+
+
+public class PdeEditorOutput extends Component {
+  PdeEditor editor;
+
+  public PdeEditorOutput(PdeEditor editor) {
+    this.editor = editor;
+  }
+}
