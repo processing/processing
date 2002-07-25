a3588bb75aefd0efe822f3d5034be0c5215d312e
benfry <fry@processing.org>
2002-07-25 20:10:06 +0000

    more cleaning

--- app/run.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-REM .\bin\jre -cp lib;lib\pde.jar;lib\kjc.jar;lib\comm.jar PdeApplication
-REM .\bin\jre -cp lib;lib\pde.jar;lib\jython.jar;lib\kjc.jar;lib\comm.jar PdeApplication
-REM java\bin\java -cp lib;lib\build;lib\pde.jar;lib\oro.jar;lib\kjc.jar;lib\comm.jar PdeApplication
-REM \jre-1.3.1_04-intl\bin\java -cp lib;lib\build;lib\pde.jar;lib\oro.jar;lib\kjc.jar;lib\comm.jar PdeBase
-.\java\bin\java -cp lib;lib\build;lib\pde.jar;lib\oro.jar;lib\kjc.jar;lib\comm.jar PdeBase
