3abf0655b8c5f51dc4ef8623c7da84c59fcdad73
benfry <fry@processing.org>
2002-07-27 19:14:28 +0000

    X macosx.. editor.status.font needs to be size 12 X   same under windows, just wasn't being set properly before X   header font needed to be set each time as well X introduce pde.properties_OSNAME X   tested to make sure it joins with the other pde.properties ok X setPixel(i, j, #99CC00); X   not working anymore Syntax error: unexpected token: CC00 X   problem was substitute only worked along with =

--- app/KjcEngine.java
+++ app/KjcEngine.java
@@ -215,7 +215,8 @@ public class KjcEngine extends at.dms.kjc.Main implements PdeEngine {
 
       if (PdeBase.getBoolean("compiler.inline_web_colors", true)) {
 	// convert "= #cc9988" into "= 0xffcc9988"
-	program = substipoot(program, "(=\\s*)\\#([0-9a-fA-F][0-9a-fA-F])([0-9a-fA-F][0-9a-fA-F])([0-9a-fA-F][0-9a-fA-F])", "$1 0xff$2$3$4");
+	//program = substipoot(program, "(=\\s*)\\#([0-9a-fA-F][0-9a-fA-F])([0-9a-fA-F][0-9a-fA-F])([0-9a-fA-F][0-9a-fA-F])", "$1 0xff$2$3$4");
+	program = substipoot(program, "#([0-9a-fA-F][0-9a-fA-F])([0-9a-fA-F][0-9a-fA-F])([0-9a-fA-F][0-9a-fA-F])", "0xff$1$2$3");
       }
 
       if ((program.indexOf("void setup()") != -1) ||
