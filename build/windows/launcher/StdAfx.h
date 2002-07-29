bf8b3c818eab954b52e39896fba17a678778f2df
benfry <fry@processing.org>
2002-07-29 22:08:10 +0000

    thank god a launcher

---
+++ build/windows/launcher/StdAfx.h
@@ -0,0 +1,25 @@
+// stdafx.h : include file for standard system include files,
+//  or project specific include files that are used frequently, but
+//      are changed infrequently
+//
+
+#if !defined(AFX_STDAFX_H__12C4E5A8_27C5_11D2_BB54_006008DC2F94__INCLUDED_)
+#define AFX_STDAFX_H__12C4E5A8_27C5_11D2_BB54_006008DC2F94__INCLUDED_
+
+#if _MSC_VER >= 1000
+#pragma once
+#endif // _MSC_VER >= 1000
+
+#define VC_EXTRALEAN		// Exclude rarely-used stuff from Windows headers
+
+#include <afxwin.h>         // MFC core and standard components
+#include <afxext.h>         // MFC extensions
+#ifndef _AFX_NO_AFXCMN_SUPPORT
+#include <afxcmn.h>			// MFC support for Windows Common Controls
+#endif // _AFX_NO_AFXCMN_SUPPORT
+
+
+//{{AFX_INSERT_LOCATION}}
+// Microsoft Developer Studio will insert additional declarations immediately before the previous line.
+
+#endif // !defined(AFX_STDAFX_H__12C4E5A8_27C5_11D2_BB54_006008DC2F94__INCLUDED_)
