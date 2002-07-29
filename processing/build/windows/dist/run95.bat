1ad2ec340525f97b7f148b17b82c54ce2465ab20
benfry <fry@processing.org>
2002-07-29 23:39:10 +0000

    launcher exe for windows and much joy

--- processing/build/windows/dist/run95.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-
-REM -- use this version if you're running under windows 95, 98, or ME
-REM -- (windows 2000 should use the other version)
-REM -- it will also be necessary to put processing in c:\ 
-
-C:\processing\java\bin\java -cp c:\processing\lib;c:\processing\lib\build;c:\processing\lib\oro.jar;c:\processing\lib\pde.jar;c:\processing\lib\kjc.jar PdeBase
