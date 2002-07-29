a70a9c3e71f9bd8563c59440a8288e0f759c51e2
benfry <fry@processing.org>
2002-07-29 04:52:57 +0000

    working on build

--- build/linux/dist.sh
+++ build/linux/dist.sh
@@ -4,11 +4,11 @@ REVISION=`head -c 4 ../../../todo.txt`
 
 ./make.sh
 
-echo Creating P5 distribution for revision $REVISION...
+echo Creating linux distribution for revision $REVISION...
 
 # remove any old boogers
 rm -rf processing
-rm -f processing-*.zip
+rm -f processing-*.tgz
 
 # use 'shared' files as starting point
 cp -r ../shared processing
@@ -26,7 +26,8 @@ rm -f processing/sketchbook/default/.cvsignore
 # will need to add a zillion of these for the reference..
 
 # add java (jre) files
-unzip -q -d processing jre.zip
+#tar --extract --verbose --file=jre.tgz --ungzip --directory=processing
+tar --extract --file=jre.tgz --ungzip --directory=processing
 
 # directories used by the app
 mkdir processing/lib/build
@@ -37,22 +38,23 @@ cp -r work/lib/export processing/lib/
 rm -rf processing/lib/export/CVS
 
 # get platform-specific goodies from the dist dir
-cp dist/Proce55ing.bat processing/
 #cp dist/run.bat processing/
 #cp dist/run95.bat processing/
-cp dist/lib/pde.properties_windows processing/lib/
+cp dist/Proce55ing processing/
+cp dist/lib/pde.properties_linux processing/lib/
 
-# convert notes.txt to windows LFs
+# make sure notes.txt is unix LFs
 # the 2> is because the app is a little chatty
-unix2dos processing/notes.txt 2> /dev/null
-unix2dos processing/lib/pde.properties 2> /dev/null
-unix2dos processing/lib/pde.properties_windows 2> /dev/null
+dos2unix processing/notes.txt 2> /dev/null
+dos2unix processing/lib/pde.properties 2> /dev/null
+dos2unix processing/lib/pde.properties_windows 2> /dev/null
 
 # zip it all up for release
-echo Zipping and finishing...
+echo Creating tarball and finishing...
 P5=processing-$REVISION
 mv processing $P5
-zip -rq $P5.zip $P5
+tar cfz $P5.tgz $P5
+#zip -rq $P5.zip $P5
 # nah, keep the new directory around
 #rm -rf $P5
 
