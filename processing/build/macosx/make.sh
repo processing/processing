8468ec9c58afabda86f1fdf9e66e3cb05c7a97a1
benfry <fry@processing.org>
2002-07-30 20:54:08 +0000

    macos9 ironed out.. things starting to look good

--- processing/build/macosx/make.sh
+++ processing/build/macosx/make.sh
@@ -6,7 +6,7 @@
 if test -d work
 then
 else
-  echo Setting up directories to under Mac OS X
+  echo Setting up directories to build under Mac OS X
   cp -r ../shared work
 
   mkdir work/lib/export
@@ -14,7 +14,7 @@ else
 
   mkdir work/classes
 
-  cp dist/run.bat work/
+  #cp dist/run.bat work/
   cp dist/lib/pde.properties_macosx work/lib/
 
   echo
@@ -65,7 +65,7 @@ echo Building PDE for JDK 1.3
 
 CLASSPATH=build/macosx/work/classes:build/macosx/work/lib/kjc.jar:build/macosx/work/lib/oro.jar:$MACOSX_CLASSPATH
 
-perl ../bagel/buzz.pl "jikes +D -classpath $CLASSPATH -d build/macosx/work/classes" -dJDK13 *.java
+perl ../bagel/buzz.pl "jikes +D -classpath $CLASSPATH -d build/macosx/work/classes" -dJDK13 -dMACOS *.java
 
 cd build/macosx/work/classes
 rm -f ../lib/pde.jar
