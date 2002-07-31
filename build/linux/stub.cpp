1b10e5412dcf27e2c96b77fc8cb56232e5cef527
benfry <fry@processing.org>
2002-07-31 21:55:05 +0000

    tweak to correct classpath for kjc

--- build/linux/stub.cpp
+++ build/linux/stub.cpp
@@ -1,5 +1,5 @@
 #include <stdlib.h>
 
 void main() {
-    system("java/bin/java -cp lib:lib/build:lib/pde.jar:lib/kjc.jar:lib/oro.jar PdeBase");
+    system("java/bin/java -cp lib:lib/build:lib/pde.jar:lib/kjc.jar:lib/oro.jar:java/lib/ext/comm.jar PdeBase");
 }
