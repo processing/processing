e9fcbabe0f122976fe3ab137bec8591994019144
benfry <fry@processing.org>
2002-10-21 03:37:49 +0000

    X switch to swing editor X moved build dirs in cvs

--- build/windows/make.sh
+++ build/windows/make.sh
@@ -26,15 +26,12 @@ fi
 
 ### -- START BUILDING -------------------------------------------
 
-# move to 'app' directory
+# move to base 'processing' directory
 cd ../..
 
 
-#PLATFORM_CLASSPATH=java/lib/rt.jar:java/lib/ext/comm.jar
-
-
 ### -- BUILD BAGEL ----------------------------------------------
-cd ..
+
 # make sure bagel exists, if not, check it out of cvs
 if test -d bagel
 then 
@@ -45,35 +42,38 @@ else
   cvs update -P
   cd ..
 fi
+
 cd bagel
 
-CLASSPATH=../app/build/windows/work/java/lib/rt.jar:../app/build/windows/work/java/lib/ext/comm.jar
-#CLASSPATH=$PLATFORM_CLASSPATH
+CLASSPATH=../build/windows/work/java/lib/rt.jar:../build/windows/work/java/lib/ext/comm.jar
 
 ### --- make version with serial for the application
 echo Building bagel with serial support
 perl make.pl SERIAL
-cp classes/*.class ../app/build/windows/work/classes/
+cp classes/*.class ../build/windows/work/classes/
 
 ### --- make version without serial for applet exporting
 echo Building bagel for export
 perl make.pl
-cp classes/*.class ../app/build/windows/work/lib/export/
+cp classes/*.class ../build/windows/work/lib/export/
 
 cd ..
-cd app
 
 
 ### -- BUILD PDE ------------------------------------------------
 
 echo Building PDE for JDK 1.3
 
-CLASSPATH=build/windows/work/classes:build/windows/work/lib/kjc.jar:build/windows/work/lib/oro.jar:build/windows/work/java/lib/rt.jar:build/windows/work/java/lib/ext/comm.jar
+cd app
 
-perl ../bagel/buzz.pl "jikes +D -classpath $CLASSPATH -d build/windows/work/classes" -dJDK13 *.java 
+CLASSPATH=../build/windows/work/classes:../build/windows/work/lib/kjc.jar:../build/windows/work/lib/oro.jar:../build/windows/work/java/lib/rt.jar:../build/windows/work/java/lib/ext/comm.jar
 
-cd build/windows/work/classes
+perl ../bagel/buzz.pl "jikes +D -classpath $CLASSPATH -d ../build/windows/work/classes" -dJDK13 *.java 
+
+cd ../build/windows/work/classes
 rm -f ../lib/pde.jar
 zip -0q ../lib/pde.jar *.class
-cd ../..
+
+# back to 'build' dir
+cd ../../..
 
