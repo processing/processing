6fafc6391ae10261161819fce01b6c05a3f467e1
rocha <carlos.rocha@gmail.com>
2003-04-30 06:33:43 +0000

    Added Sound Engine (with SONIC flag)!!

--- build/windows/run.sh
+++ build/windows/run.sh
@@ -3,12 +3,15 @@
 if test -d /cygdrive/c/WINNT
 then
   # windows 2000 or nt
-  QT_JAVA_PATH=\\cygdrive\\c\\WINNT\\system32\\QTJava.zip
+  QT_JAVA_PATH=C:\\WINNT\\system32\\QTJava.zip
 else
   # other versions of windows, including xp
-  QT_JAVA_PATH=\\cygdrive\\c\\WINDOWS\\system32\\QTJava.zip
+  QT_JAVA_PATH=C:\\WINDOWS\\system32\\QTJava.zip
 fi
 # another alternative
 #QT_JAVA_PATH=..\\build\\shared\\lib\\qtjava.zip
 
-cd work && ./java/bin/java -cp lib\;lib\\build\;lib\\pde.jar\;lib\\kjc.jar\;lib\\oro.jar\;java\\lib\\ext\\comm.jar\;${QT_JAVA_PATH} PdeBase
+
+CLASSPATH=lib\;lib\\build\;lib\\pde.jar\;lib\\kjc.jar\;lib\\oro.jar\;java\\lib\\ext\\comm.jar\;${QT_JAVA_PATH}
+
+cd work && ./java/bin/java -cp ${CLASSPATH} PdeBase
