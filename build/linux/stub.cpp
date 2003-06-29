689822de56f3692e8a7377e549220e82e32eb884
dmose <dmose@meer.net>
2003-06-29 23:05:12 +0000

    Fixed to work with newer versions of gcc, which require that main() return an int.

--- build/linux/stub.cpp
+++ build/linux/stub.cpp
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 
-void main() {
+int main() {
     system("java/bin/java -cp lib:lib/build:lib/pde.jar:lib/kjc.jar:lib/oro.jar:java/lib/ext/comm.jar PdeBase");
+    return 0;
 }
