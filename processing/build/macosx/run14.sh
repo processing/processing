9dbb120b6a2893a40c97ec0dd7ecac7e9ecda458
benfry <fry@processing.org>
2003-07-23 14:14:19 +0000

    tweaks to get $CLASSPATH back in there

--- processing/build/macosx/run14.sh
+++ processing/build/macosx/run14.sh
@@ -2,4 +2,5 @@
 
 CLASSPATH=/System/Library/Frameworks/JavaVM.framework/Versions/1.4/Classes/classes.jar:/System/Library/Frameworks/JavaVM.framework/Versions/1.4/Classes/ui.jar:/System/Library/Frameworks/JavaVM.framework/Versions/1.4/Home/lib/ext/comm.jar:/System/Library/Java/Extensions/QTJava.zip:lib:lib/build:lib/pde.jar:lib/kjc.jar:lib/oro.jar:../comm.jar
 
-cd work && /System/Library/Frameworks/JavaVM.framework/Versions/1.4/Commands/java -cp PdeBase
+cd work && /System/Library/Frameworks/JavaVM.framework/Versions/1.4/Commands/java -cp $CLASSPATH PdeBase
+
