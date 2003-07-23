326b6ef4ca187b15e7d2f2eb1f44153648c21d91
benfry <fry@processing.org>
2003-07-23 21:32:33 +0000

    adding licensing information to files, removing some tabs

--- app/PdeEditorHeader.java
+++ app/PdeEditorHeader.java
@@ -1,16 +1,32 @@
+/* -*- mode: jde; c-basic-offset: 2; indent-tabs-mode: nil -*- */
+
+/*
+  PdeEditorHeader - panel that containing the sketch title
+  Part of the Processing project - http://Proce55ing.net
+
+  Copyright (c) 2001-03 
+  Ben Fry, Massachusetts Institute of Technology and 
+  Casey Reas, Interaction Design Institute Ivrea
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License 
+  along with this program; if not, write to the Free Software Foundation, 
+  Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+
 import java.awt.*;
 import java.awt.event.*;
 import java.io.*;
 
-    /*
-    PdeEditorLabel sketchLabel = new PdeEditorLabel(1);
-    Color sketchBgColor = new Color(51, 51, 51);
-    Color sketchPrimaryTextColor = Color.white;
-    Color sketchSecondaryTextColor = new Color(153, 153, 153);
-    sketchLabel.setForeground(sketchPrimaryTextColor);
-    sketchLabel.setBackground(sketchBgColor);
-    rightPanel.add("North", sketchLabel);
-    */
 
 public class PdeEditorHeader extends Panel /* implements ActionListener*/ {
   static final String SKETCH_TITLER = "sketch";
