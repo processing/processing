70333bae745b84beb8b1646faa822eb05aebd60d
dmose <dmose@meer.net>
2003-07-23 09:57:08 +0000

    Changes to make OSX build, run, and dist with the new Jikes compiler

--- build/macosx/run.sh
+++ build/macosx/run.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 # -Dcom.apple.hwaccel=false 
-cd work && java -cp lib:lib/build:lib/pde.jar:lib/kjc.jar:lib/oro.jar:../comm.jar PdeBase
+
+CLASSPATH=/System/Library/Frameworks/JavaVM.framework/Classes/classes.jar:/System/Library/Frameworks/JavaVM.framework/Classes/ui.jar:/System/Library/Frameworks/JavaVM.framework/Home/lib/ext/comm.jar:/System/Library/Java/Extensions/QTJava.zip:lib:lib/build:lib/pde.jar:lib/kjc.jar:lib/oro.jar:../comm.jar
+
+cd work && java PdeBase
