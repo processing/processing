700fef8ed7a5b87e77cbcfdf5b11e07a3490912e
benfry <fry@processing.org>
2003-07-23 14:14:19 +0000

    tweaks to get $CLASSPATH back in there

--- build/macosx/run13.sh
+++ build/macosx/run13.sh
@@ -2,7 +2,7 @@
 
 CLASSPATH=/System/Library/Frameworks/JavaVM.framework/Versions/1.3.1/Classes/classes.jar:/System/Library/Frameworks/JavaVM.framework/Versions/1.3.1/Classes/ui.jar:/System/Library/Frameworks/JavaVM.framework/Versions/1.3.1/Home/lib/ext/comm.jar:/System/Library/Java/Extensions/QTJava.zip:lib:lib/build:lib/pde.jar:lib/kjc.jar:lib/oro.jar:../comm.jar
 
-cd work && /System/Library/Frameworks/JavaVM.framework/Versions/1.3.1/Commands/java -cp PdeBase
+cd work && /System/Library/Frameworks/JavaVM.framework/Versions/1.3.1/Commands/java -cp $CLASSPATH PdeBase
 
 # -Dcom.apple.hwaccel=false 
 #cd work && java -cp lib:lib/build:lib/pde.jar:lib/kjc.jar:lib/oro.jar:../comm.jar PdeBase
