73dea34e21c2f3e37029584614a0445081549cb3
benfry <fry@processing.org>
2003-07-23 21:32:33 +0000

    adding licensing information to files, removing some tabs

--- app/PdeBase.java
+++ app/PdeBase.java
@@ -1,8 +1,12 @@
 /* -*- mode: jde; c-basic-offset: 2; indent-tabs-mode: nil -*- */
 
 /*
-  Part of the Processing project
-  Copyright (c) 2001-03 Ben Fry and Casey Reas
+  PdeBase - base class for the main processing application
+  Part of the Processing project - http://Proce55ing.net
+
+  Copyright (c) 2001-03 
+  Ben Fry, Massachusetts Institute of Technology and 
+  Casey Reas, Interaction Design Institute Ivrea
 
   This program is free software; you can redistribute it and/or modify
   it under the terms of the GNU General Public License as published by
@@ -13,6 +17,10 @@
   but WITHOUT ANY WARRANTY; without even the implied warranty of
   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
   GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License 
+  along with this program; if not, write to the Free Software Foundation, 
+  Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 */
 
 import java.awt.*;
--- app/PdeEditorButtons.java
+++ app/PdeEditorButtons.java
@@ -1,8 +1,30 @@
+/* -*- mode: jde; c-basic-offset: 2; indent-tabs-mode: nil -*- */
+
+/*
+  PdeEditorButtons - run/stop/etc buttons for the ide
+  Part of the Processing project - http://Proce55ing.net
+
+  Copyright (c) 2001-03 
+  Ben Fry, Massachusetts Institute of Technology and 
+  Casey Reas, Interaction Design Institute Ivrea
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License 
+  along with this program; if not, write to the Free Software Foundation, 
+  Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+
 import java.awt.*;
 import java.awt.event.MouseEvent;
-
-//import java.awt.Image;
-
 import javax.swing.*;
 import javax.swing.event.*;
 
