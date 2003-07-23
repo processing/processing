2d56dd6d45c9cc0c2de937826c09d3f3b010d185
dmose <dmose@meer.net>
2003-07-23 15:19:30 +0000

    Removed extraneous -cp on Java commandline and added export CLASSPATH, just in case

--- build/macosx/run.sh
+++ build/macosx/run.sh
@@ -2,5 +2,6 @@
 # -Dcom.apple.hwaccel=false 
 
 CLASSPATH=/System/Library/Frameworks/JavaVM.framework/Classes/classes.jar:/System/Library/Frameworks/JavaVM.framework/Classes/ui.jar:/System/Library/Frameworks/JavaVM.framework/Home/lib/ext/comm.jar:/System/Library/Java/Extensions/QTJava.zip:lib:lib/build:lib/pde.jar:lib/kjc.jar:lib/oro.jar:../comm.jar
+export CLASSPATH
 
 cd work && java PdeBase
--- build/macosx/run13.sh
+++ build/macosx/run13.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
 CLASSPATH=/System/Library/Frameworks/JavaVM.framework/Versions/1.3.1/Classes/classes.jar:/System/Library/Frameworks/JavaVM.framework/Versions/1.3.1/Classes/ui.jar:/System/Library/Frameworks/JavaVM.framework/Versions/1.3.1/Home/lib/ext/comm.jar:/System/Library/Java/Extensions/QTJava.zip:lib:lib/build:lib/pde.jar:lib/kjc.jar:lib/oro.jar:../comm.jar
+export CLASSPATH
 
-cd work && /System/Library/Frameworks/JavaVM.framework/Versions/1.3.1/Commands/java -cp $CLASSPATH PdeBase
+cd work && /System/Library/Frameworks/JavaVM.framework/Versions/1.3.1/Commands/java PdeBase
 
 # -Dcom.apple.hwaccel=false 
 #cd work && java -cp lib:lib/build:lib/pde.jar:lib/kjc.jar:lib/oro.jar:../comm.jar PdeBase
