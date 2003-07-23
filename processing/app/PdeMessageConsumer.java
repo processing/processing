7900a08740f1d2b184cbb7e065ca7aa8bc6780a3
benfry <fry@processing.org>
2003-07-23 21:32:33 +0000

    adding licensing information to files, removing some tabs

--- processing/app/PdeMessageConsumer.java
+++ processing/app/PdeMessageConsumer.java
@@ -1,4 +1,27 @@
-// -*- Mode: JDE; tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 2 -*-
+/* -*- mode: jde; c-basic-offset: 2; indent-tabs-mode: nil -*- */
+
+/*
+  PdeMessageConsumer - interface for dealing with parser/compiler output
+  Part of the Processing project - http://Proce55ing.net
+
+  Copyright (c) 2001-03 
+  Ben Fry, Massachusetts Institute of Technology and 
+  Casey Reas, Interaction Design Institute Ivrea
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License 
+  along with this program; if not, write to the Free Software Foundation, 
+  Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
 
 // Different instances of PdeMessageStream need to do different things with
 // messages.  In particular, a stream instance used for parsing output from
