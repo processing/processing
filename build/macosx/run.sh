67641ec02caad1814494992b0456255b2dcfa0d7
benfry <fry@processing.org>
2003-07-30 04:32:53 +0000

    attempts to turn off console antialiasing, fix for cmd-key macosx issues

--- build/macosx/run.sh
+++ build/macosx/run.sh
@@ -4,4 +4,5 @@
 CLASSPATH=/System/Library/Frameworks/JavaVM.framework/Classes/classes.jar:/System/Library/Frameworks/JavaVM.framework/Classes/ui.jar:/System/Library/Frameworks/JavaVM.framework/Home/lib/ext/comm.jar:/System/Library/Java/Extensions/QTJava.zip:lib:lib/build:lib/pde.jar:lib/kjc.jar:lib/oro.jar:../comm.jar
 export CLASSPATH
 
+#cd work && java -Dcom.apple.awt.antialiasing=off PdeBase
 cd work && java PdeBase
