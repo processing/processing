de2a1b1ee5427c1508065890665fca0fc9525b36
benfry <fry@processing.org>
2003-08-03 20:28:45 +0000

    modifying scripts for newer serial setup

--- build/windows/make.sh
+++ build/windows/make.sh
@@ -35,9 +35,13 @@ else
   unzip -q -d work jre.zip
   # cygwin requires this because of unknown weirdness
   # it was not formerly this anal retentive
-  chmod +x work/java/bin/*.exe
-  chmod +x work/java/bin/*.dll
-  chmod +x work/java/bin/client/*.dll
+  cd work/java/bin/
+  chmod -R +x *.exe *.dll
+  cd ../../..
+  pwd
+  #chmod -R +x work/java/bin/*.exe
+  #chmod +x work/java/bin/*.dll
+  #chmod +x work/java/bin/client/*.dll
 
   mkdir work/lib/export
   mkdir work/lib/build
@@ -49,7 +53,16 @@ else
   echo Compiling processing.exe
   cd launcher && make && cp processing.exe ../work/ && cd ..
 
-  echo
+  # get the serial stuff
+  echo Copying serial support from bagel dir
+  cp ../../bagel/serial/comm.jar work/lib/
+  cp ../../bagel/serial/javax.comm.properties work/lib/
+  cp ../../bagel/serial/win32com.dll work/
+  chmod +x work/win32com.dll
+
+  # get jikes and depedencies
+  gunzip < jikes.exe.gz > work/jikes.exe
+  chmod +x work/jikes.exe
 fi
 
 
@@ -88,14 +101,21 @@ fi
 # another alternative
 #QT_JAVA_PATH=../build/shared/lib/qtjava.zip
 
-CLASSPATH="..\\build\\windows\\work\\java\\lib\\rt.jar;..\\build\\windows\\work\\java\\lib\\ext\\comm.jar;${QT_JAVA_PATH}"
+# regular version
+#CLASSPATH="..\\build\\windows\\work\\java\\lib\\rt.jar;..\\build\\windows\\work\\java\\lib\\ext\\comm.jar;${QT_JAVA_PATH}"
 
-### --- make version with serial for the application
-echo Building bagel with serial, sonic and video support
-perl make.pl SERIAL SONIC VIDEO
+# new regular version
+CLASSPATH="..\\build\\windows\\work\\java\\lib\\rt.jar;..\\build\\windows\\work\\lib\\comm.jar;${QT_JAVA_PATH}"
+
+# rxtx version
+#CLASSPATH="..\\build\\windows\\work\\java\\lib\\rt.jar;..\\build\\windows\\work\\java\\lib\\ext\\RXTXcomm.jar;${QT_JAVA_PATH}"
+
+# make version with serial for the application
+echo Building bagel with serial, sonic, video and jdk13 support
+perl make.pl SERIAL SONIC VIDEO JDK13
 cp classes/*.class ../build/windows/work/classes/
 
-### --- make simpler version for applet exporting
+# make simpler version for applet exporting, only 1.1 functions
 echo Building bagel for export with sonic support
 perl make.pl SONIC
 cp classes/*.class ../build/windows/work/lib/export/
@@ -109,8 +129,14 @@ echo Building PDE for JDK 1.4
 
 cd app
 
-CLASSPATH="..\\build\\windows\\work\\classes;..\\build\\windows\\work\\lib\\kjc.jar;..\\build\\windows\\work\\lib\\oro.jar;..\\build\\windows\\work\\java\\lib\\rt.jar;..\\build\\windows\\work\\java\\lib\\ext\\comm.jar;${QT_JAVA_PATH}"
+# rxtx version
+#CLASSPATH="..\\build\\windows\\work\\classes;..\\build\\windows\\work\\lib\\kjc.jar;..\\build\\windows\\work\\lib\\oro.jar;..\\build\\windows\\work\\java\\lib\\rt.jar;..\\build\\windows\\work\\java\\lib\\ext\\RXTXcomm.jar;${QT_JAVA_PATH}"
 
+# original version 
+#CLASSPATH="..\\build\\windows\\work\\classes;..\\build\\windows\\work\\lib\\kjc.jar;..\\build\\windows\\work\\lib\\oro.jar;..\\build\\windows\\work\\java\\lib\\rt.jar;..\\build\\windows\\work\\java\\lib\\ext\\comm.jar;${QT_JAVA_PATH}"
+
+# new javax.comm location
+CLASSPATH="..\\build\\windows\\work\\classes;..\\build\\windows\\work\\lib\\kjc.jar;..\\build\\windows\\work\\lib\\oro.jar;..\\build\\windows\\work\\java\\lib\\rt.jar;..\\build\\windows\\work\\lib\\comm.jar;${QT_JAVA_PATH}"
 
 # version for javac/1.1 testing
 #CLASSPATH="..\\build\\windows\\work\\classes;..\\build\\windows\\work\\lib\\kjc.jar;..\\build\\windows\\work\\lib\\oro.jar;..\\build\\windows\\work\\java\\lib\\rt.jar;..\\build\\windows\\work\\java\\lib\\ext\\comm.jar;..\\build\\macos9\\JDKClasses.zip;..\\build\\macos9\\JDKToolsClasses.zip"
--- build/windows/run.sh
+++ build/windows/run.sh
@@ -11,10 +11,18 @@ fi
 # another alternative
 #QT_JAVA_PATH=..\\build\\shared\\lib\\qtjava.zip
 
+# rxtx testing
+#CLASSPATH=java\\lib\\rt.jar\;lib\;lib\\build\;lib\\pde.jar\;lib\\kjc.jar\;lib\\oro.jar\;lib\\RXTXcomm.jar\;${QT_JAVA_PATH}
 
-CLASSPATH=java\\lib\\rt.jar\;lib\;lib\\build\;lib\\pde.jar\;lib\\kjc.jar\;lib\\oro.jar\;java\\lib\\ext\\comm.jar\;${QT_JAVA_PATH}
+# newer javax.comm version
+#CLASSPATH=java\\lib\\rt.jar\;lib\;lib\\build\;lib\\pde.jar\;lib\\kjc.jar\;lib\\oro.jar\;lib\\comm.jar\;${QT_JAVA_PATH}
+CLASSPATH=java\\lib\\rt.jar\;lib\;lib\\build\;lib\\pde.jar\;lib\\kjc.jar\;lib\\oro.jar\;lib\\comm.jar\;lib\\RXTXcomm.jar\;${QT_JAVA_PATH}
+
+# javax.comm version
+#CLASSPATH=java\\lib\\rt.jar\;lib\;lib\\build\;lib\\pde.jar\;lib\\kjc.jar\;lib\\oro.jar\;java\\lib\\ext\\comm.jar\;${QT_JAVA_PATH}
 
 # version for javac/1.1 testing
 #CLASSPATH=java\\lib\\rt.jar\;lib\;lib\\build\;lib\\pde.jar\;lib\\kjc.jar\;lib\\oro.jar\;java\\lib\\ext\\comm.jar\;${QT_JAVA_PATH}\;..\\..\\macos9\\JDKClasses.zip\;..\\..\\macos9\\JDKToolsClasses.zip
 
+#cd work && ./java/bin/java -cp ${CLASSPATH} PdeBase
 cd work && ./java/bin/java -cp ${CLASSPATH} PdeBase
