9642eb2a8ad2e8f657bde8fe3c6f2a50ee106ff7
benfry <fry@processing.org>
2003-08-04 06:30:21 +0000

    working on rev 57 for macosx

--- build/macosx/make.sh
+++ build/macosx/make.sh
@@ -22,21 +22,29 @@ else
   rm reference.zip
   cd ..
 
-  # copy gl4java libs and jar file
-  #cp ../../bagel/opengl/gl4java.jar work/lib/
-  #cp ../../bagel/opengl/macosx/libGL4JavaJauGljJNI13.jnilib work/
-
   mkdir work/lib/export
   mkdir work/lib/build
 
   mkdir work/classes
 
+  # get a copy of the mac-specific properties
   cp dist/lib/pde_macosx.properties work/lib/
 
   # grab serial goodies
   echo Copying serial support from bagel dir
   cp ../../bagel/serial/RXTXcomm.jar work/lib/
   cp ../../bagel/serial/libSerial.jnilib work/
+
+  # copy gl4java libs and jar file
+  # disabled till the next release when i can recompile for 1.4
+  #cp ../../bagel/opengl/gl4java.jar work/lib/
+  #cp ../../bagel/opengl/libGL4JavaJauGljJNI13.jnilib work/
+
+  # to have a copy of this guy around for messing with
+  cp -a dist/Processing.app work/
+  #cd work/Processing.app
+  #find . -name "CVS" -depth -exec rm {} \;
+  #cd ../..
 
   # get jikes and depedencies
   gunzip < dist/jikes.gz > work/jikes
@@ -98,9 +106,12 @@ echo Building PDE for JDK 1.4
 # new rxtx
 CLASSPATH=../build/macosx/work/classes:../build/macosx/work/lib/kjc.jar:../build/macosx/work/lib/oro.jar:../build/macosx/work/lib/RXTXcomm.jar:$MACOSX_CLASSPATH
 
-perl ../bagel/buzz.pl "jikes +D -classpath $CLASSPATH -d ../build/macosx/work/classes" -dJDK13 -dJDK14 -dMACOS *.java jeditsyntax/*.java
+perl ../bagel/buzz.pl "jikes +D -classpath $CLASSPATH -d ../build/macosx/work/classes" -dJDK13 -dJDK14 -dMACOS -dRXTX *.java jeditsyntax/*.java
 
 cd ../build/macosx/work/classes
 rm -f ../lib/pde.jar
 zip -0q ../lib/pde.jar *.class
 cd ../..
+
+# get the libs
+cp work/lib/*.jar work/Processing.app/Contents/Resources/Java/
