0e1b1f2f82ab6962a5f2403ff66788905f340226
dmose <dmose@meer.net>
2003-08-21 00:53:37 +0000

    ANTLR build changes

--- build/macosx/run.sh
+++ build/macosx/run.sh
@@ -8,7 +8,7 @@
 #CLASSPATH=/System/Library/Frameworks/JavaVM.framework/Classes/classes.jar:/System/Library/Frameworks/JavaVM.framework/Classes/ui.jar:/System/Library/Frameworks/JavaVM.framework/Home/lib/ext/comm.jar:/System/Library/Java/Extensions/QTJava.zip:lib:lib/build:lib/pde.jar:lib/kjc.jar:lib/oro.jar:lib/RXTXcomm.jar
 
 # is qt java already included tho?
-CLASSPATH=/System/Library/Java/Extensions/QTJava.zip:lib:lib/build:lib/pde.jar:lib/kjc.jar:lib/oro.jar:lib/RXTXcomm.jar
+CLASSPATH=/System/Library/Java/Extensions/QTJava.zip:lib:lib/build:lib/pde.jar:lib/kjc.jar:lib/antlr.jar:lib/oro.jar:lib/RXTXcomm.jar
 
 export CLASSPATH
 
