ef525acd6ad7423eb16f2faca37b0696907dfcf4
dmose <dmose@meer.net>
2003-08-21 00:45:37 +0000

    ANTLR build fixes

--- processing/build/linux/stub.sh
+++ processing/build/linux/stub.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-CLASSPATH=java/lib/rt.jar:lib:lib/build:lib/pde.jar:lib/kjc.jar:lib/oro.jar:lib/RXTXcomm.jar
+CLASSPATH=java/lib/rt.jar:lib:lib/build:lib/pde.jar:lib/kjc.jar:lib/antlr.jar:lib/oro.jar:lib/RXTXcomm.jar
 export CLASSPATH
 
 exec java/bin/java PdeBase
