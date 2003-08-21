5825394cbe402ae23181bf9d5f1242bebe9e2567
dmose <dmose@meer.net>
2003-08-21 00:14:24 +0000

    ANTLR build changes

--- build/windows/dist/run.bat
+++ build/windows/dist/run.bat
@@ -4,7 +4,7 @@ REM --- if you need more ram, change the 64m (which means
 REM --- 64 megabytes) to something higher. 
 
 set SAVEDCP=%CLASSPATH%
-set CLASSPATH=%CLASSPATH%;java\lib\rt.jar;lib;lib\build;lib\pde.jar;lib\kjc.jar;lib\oro.jar;lib\comm.jar;c:\winnt\system32\qtjava.zip;c:\windows\system32\qtjava.zip
+set CLASSPATH=%CLASSPATH%;java\lib\rt.jar;lib;lib\build;lib\pde.jar;lib\kjc.jar;lib\antlr.jar;lib\oro.jar;lib\comm.jar;c:\winnt\system32\qtjava.zip;c:\windows\system32\qtjava.zip
 
 java -ms64m -mx64m PdeBase
 
