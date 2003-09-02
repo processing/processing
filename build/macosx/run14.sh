8e10af5f80db05ce5d2fe92185860765f224b6ba
benfry <fry@processing.org>
2003-09-02 01:48:55 +0000

    building on the mac now..

--- build/macosx/run14.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-CLASSPATH=/System/Library/Frameworks/JavaVM.framework/Versions/1.4/Classes/classes.jar:/System/Library/Frameworks/JavaVM.framework/Versions/1.4/Classes/ui.jar:/System/Library/Frameworks/JavaVM.framework/Versions/1.4/Home/lib/ext/comm.jar:/System/Library/Java/Extensions/QTJava.zip:lib:lib/build:lib/pde.jar:lib/kjc.jar:lib/antlr.jar:lib/oro.jar:../comm.jar
-export CLASSPATH
-
-cd work && /System/Library/Frameworks/JavaVM.framework/Versions/1.4/Commands/java PdeBase
-
