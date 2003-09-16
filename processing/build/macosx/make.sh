12db2fc2de3fff50985b06755b426acc65ce35f2
benfry <fry@processing.org>
2003-09-16 23:28:12 +0000

    modification to properly rebuild grammar

--- processing/build/macosx/make.sh
+++ processing/build/macosx/make.sh
@@ -73,12 +73,12 @@ else
   chmod +x work/jikes
 
   # build classes/grammar for preprocessor
-  cd preprocessor
+  cd ../../app/preprocessor
   # first build the default java goop
   java -cp ../../build/macosx/work/lib/antlr.jar antlr.Tool java.g
   # now build the pde stuff that extends the java classes
   java -cp ../../build/macosx/work/lib/antlr.jar antlr.Tool -glib java.g pde.g
-  cd ..
+  cd ../../build/macosx
 
   #echo
 fi
