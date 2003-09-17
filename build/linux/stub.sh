460891201442399f1e0d4e749a14b0c704c42fd4
dmose <dmose@meer.net>
2003-09-17 01:19:36 +0000

    Allow processing to find jikes

--- build/linux/stub.sh
+++ build/linux/stub.sh
@@ -3,4 +3,10 @@
 CLASSPATH=java/lib/rt.jar:lib:lib/build:lib/pde.jar:lib/kjc.jar:lib/antlr.jar:lib/oro.jar:lib/RXTXcomm.jar
 export CLASSPATH
 
+# put the directory where this file lives in the front of the path, because
+# that directory also contains jikes, which we will need at runtime.
+#
+PATH=`pwd`/`dirname $0`:${PATH}
+export PATH
+ 
 exec java/bin/java PdeBase
