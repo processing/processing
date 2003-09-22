674faf7b8806d211589bc64834de6f580c279cef
benfry <fry@processing.org>
2003-09-22 06:08:05 +0000

    fixes for run.bat and build scripts

--- build/windows/dist/run.bat
+++ build/windows/dist/run.bat
@@ -4,9 +4,8 @@ REM --- if you need more ram, change the 64m (which means
 REM --- 64 megabytes) to something higher. 
 
 set SAVEDCP=%CLASSPATH%
-REM set CLASSPATH=%CLASSPATH%;java\lib\rt.jar;lib;lib\build;lib\pde.jar;lib\kjc.jar;lib\antlr.jar;lib\oro.jar;lib\comm.jar;c:\winnt\system32\qtjava.zip;c:\windows\system32\qtjava.zip
-set CLASSPATH=%CLASSPATH%;java\lib\rt.jar;lib;lib\build;lib\pde.jar;lib\kjc.jar;lib\antlr.jar;lib\oro.jar;lib\comm.jar;%QTJAVA%
+set CLASSPATH=%CLASSPATH%;java\lib\rt.jar;lib;lib\build;lib\pde.jar;lib\kjc.jar;lib\antlr.jar;lib\oro.jar;lib\comm.jar;%windir%\system32\qtjava.zip;%windir%\system\qtjava.zip
 
-java -ms64m -mx64m PdeBase
+start .\java\bin\javaw -ms64m -mx64m PdeBase
 
 set CLASSPATH=%SAVEDCP%
