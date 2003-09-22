0733de85067f3815c022b2f2b908ba1f5f1adae2
benfry <fry@processing.org>
2003-09-22 06:08:05 +0000

    fixes for run.bat and build scripts

--- processing/build/windows/run.sh
+++ processing/build/windows/run.sh
@@ -1,25 +1,25 @@
 #!/bin/sh
 
-#if test -d /cygdrive/c/WINNT
-#then
-  # windows 2000 or nt
-#  QT_JAVA_PATH=C:\\WINNT\\system32\\QTJava.zip
-#else
-  # other versions of windows, including xp
-#  QT_JAVA_PATH=C:\\WINDOWS\\system32\\QTJava.zip
-#fi
-# another alternative
-#QT_JAVA_PATH=..\\build\\shared\\lib\\qtjava.zip
-
-# #$(*&@(#*$& quicktime for java
-QT_JAVA_PATH=`perl -e '$qt = $ENV{'QTJAVA'}; $qt =~ s/\"//g; print $qt'`;
+QT_JAVA_PATH="$WINDIR\\system32\\QTJava.zip"
+if test -f "${QT_JAVA_PATH}"
+then
+  #echo "Found Quicktime at $QT_JAVA_PATH"
+else 
+  QT_JAVA_PATH="$WINDIR\\system\\QTJava.zip"
+  if test -f "${QT_JAVA_PATH}"
+    echo "could not find qtjava.zip in either"
+    echo "${WINDIR}\\system32\\qtjava.zip or"
+    echo "${WINDIR}\\system\\qtjava.zip"
+    echo "quicktime for java must be installed before building."
+    exit 1;
+  then
+    #echo "Found Quicktime at $QT_JAVA_PATH"
+  else
+  fi
+fi
 
 # rxtx testing
 #CLASSPATH=java\\lib\\rt.jar\;lib\;lib\\build\;lib\\pde.jar\;lib\\kjc.jar\;lib\\oro.jar\;lib\\RXTXcomm.jar\;${QT_JAVA_PATH}
-
-CLASSPATH=\"java\\lib\\rt.jar\;lib\;lib\\build\;lib\\pde.jar\;lib\\kjc.jar\;lib\\antlr.jar\;lib\\oro.jar\;lib\\comm.jar\;lib\\RXTXcomm.jar\;${QT_JAVA_PATH}\"
-
-#echo $CLASSPATH
 
 # will this one work? or do the quotes have to be chopped?
 #CLASSPATH=java\\lib\\rt.jar\;lib\;lib\\build\;lib\\pde.jar\;lib\\kjc.jar\;lib\\antlr.jar\;lib\\oro.jar\;lib\\comm.jar\;lib\\RXTXcomm.jar\;${QTJAVA}
@@ -27,6 +27,6 @@ CLASSPATH=\"java\\lib\\rt.jar\;lib\;lib\\build\;lib\\pde.jar\;lib\\kjc.jar\;lib\
 # version for javac/1.1 testing
 #CLASSPATH=java\\lib\\rt.jar\;lib\;lib\\build\;lib\\pde.jar\;lib\\kjc.jar\;lib\\oro.jar\;java\\lib\\ext\\comm.jar\;${QT_JAVA_PATH}\;..\\..\\macos9\\JDKClasses.zip\;..\\..\\macos9\\JDKToolsClasses.zip
 
-#cd work && ./java/bin/java -cp ${CLASSPATH} PdeBase
-#cd work && ./java/bin/java -cp ${CLASSPATH} PdeBase
+CLASSPATH=\"java\\lib\\rt.jar\;lib\;lib\\build\;lib\\pde.jar\;lib\\kjc.jar\;lib\\antlr.jar\;lib\\oro.jar\;lib\\comm.jar\;lib\\RXTXcomm.jar\;${QT_JAVA_PATH}\"
+
 cd work && ./java/bin/java PdeBase
