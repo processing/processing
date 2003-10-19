43305add4e5155382f9f848174b662dcb2fb705e
benfry <fry@processing.org>
2003-10-19 18:30:15 +0000

    including netscape.javascript stuff, and fixing ram alloc in run.bat

--- build/windows/dist/run-expert.bat
+++ build/windows/dist/run-expert.bat
@@ -1,11 +1,11 @@
 @echo off
 
-REM --- if you need more ram, change the 64m (which means 
-REM --- 64 megabytes) to something higher. 
+REM --- if you're running out of memory, change the 128m 
+REM --- (which means 128 megabytes) to something higher. 
 
 set SAVEDCP=%CLASSPATH%
-set CLASSPATH=%CLASSPATH%;java\lib\rt.jar;lib;lib\build;lib\pde.jar;lib\kjc.jar;lib\antlr.jar;lib\oro.jar;lib\comm.jar;%windir%\system32\qtjava.zip;%windir%\system\qtjava.zip
+set CLASSPATH=%CLASSPATH%;lib;lib\build;lib\pde.jar;lib\kjc.jar;lib\antlr.jar;lib\oro.jar;lib\comm.jar;%windir%\system32\qtjava.zip;%windir%\system\qtjava.zip
 
-start javaw -ms64m -mx64m PdeBase
+start javaw -ms128m -mx128m PdeBase
 
 set CLASSPATH=%SAVEDCP%
