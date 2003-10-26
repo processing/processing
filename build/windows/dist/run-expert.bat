fba3324f0d9f85f86f7f2d115576748a68250781
benfry <fry@processing.org>
2003-10-26 03:10:05 +0000

    removing CLASSPATH from .bat scripts since causing so much trouble

--- build/windows/dist/run-expert.bat
+++ build/windows/dist/run-expert.bat
@@ -4,7 +4,7 @@ REM --- if you're running out of memory, change the 128m
 REM --- (which means 128 megabytes) to something higher. 
 
 set SAVEDCP=%CLASSPATH%
-set CLASSPATH=%CLASSPATH%;lib;lib\build;lib\pde.jar;lib\kjc.jar;lib\antlr.jar;lib\oro.jar;lib\comm.jar;%windir%\system32\qtjava.zip;%windir%\system\qtjava.zip
+set CLASSPATH=lib;lib\build;lib\pde.jar;lib\kjc.jar;lib\antlr.jar;lib\oro.jar;lib\comm.jar;%windir%\system32\qtjava.zip;%windir%\system\qtjava.zip
 
 start javaw -ms128m -mx128m PdeBase
 
