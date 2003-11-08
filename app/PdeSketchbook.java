810ee0fbc6f8462d1858a33c2f9380a33dc9fe12
benfry <fry@processing.org>
2003-11-08 21:01:53 +0000

    further work on reorganizing things

---
+++ app/PdeSketchbook.java
@@ -0,0 +1,44 @@
+/* -*- mode: jde; c-basic-offset: 2; indent-tabs-mode: nil -*- */
+
+/*
+  PdeSketchbook - handles sketchbook mechanics for the sketch menu
+  Part of the Processing project - http://Proce55ing.net
+
+  Except where noted, code is written by Ben Fry 
+  Copyright (c) 2001-03 Massachusetts Institute of Technology
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License 
+  along with this program; if not, write to the Free Software Foundation, 
+  Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+
+import java.awt.*;
+import java.awt.event.*;
+import java.io.*;
+import java.net.*;
+import java.util.*;
+import java.util.zip.*;
+
+import javax.swing.*;
+import javax.swing.event.*;
+import javax.swing.text.*;
+import javax.swing.undo.*;
+
+
+public class PdeSketchbook {
+  JMenu sketchbookMenu;
+  File sketchbookFolder;
+  String sketchbookPath;
+
+    
+}
