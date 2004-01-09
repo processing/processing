8d8b07d5f5278579a290d254f87dd6e8932c919f
benfry <fry@processing.org>
2004-01-09 17:11:09 +0000

    more work on multi-file support

--- processing/app/PdeEditorHeader.java
+++ processing/app/PdeEditorHeader.java
@@ -39,6 +39,12 @@ public class PdeEditorHeader extends JComponent {
   PdeEditor editor;
   //PdeSketch sketch;
 
+  int tabLeft[];
+  int tabRight[];
+
+  int menuLeft;
+  int menuRight;
+
   //int sketchLeft;
   //int sketchRight;
   //int sketchTitleLeft;
@@ -47,6 +53,11 @@ public class PdeEditorHeader extends JComponent {
   Font font;
   FontMetrics metrics;
   int fontAscent;
+
+  //
+
+  boolean menuVisible;
+  JMenu menu;
 
   //
 
@@ -150,6 +161,12 @@ public class PdeEditorHeader extends JComponent {
       fontAscent = metrics.getAscent();
     }
 
+    if ((tabLeft == null) ||
+        (tabLeft.length < sketch.fileCount)) {
+      tabLeft = new int[sketch.fileCount];
+      tabRight = new int[sketch.fileCount];
+    }
+
     int x = PdePreferences.GUI_SMALL;
     for (int i = 0; i < sketch.fileCount; i++) {
       String text = sketch.modified[i] ? 
@@ -165,10 +182,12 @@ public class PdeEditorHeader extends JComponent {
       x += PIECE_WIDTH;
 
       int contentLeft = x;
+      tabLeft[i] = x;
       for (int j = 0; j < pieceCount; j++) {
         g.drawImage(pieces[state][MIDDLE], x, 0, null);
         x += PIECE_WIDTH;
       }
+      tabRight[i] = x;
       int textLeft = contentLeft + (pieceWidth - textWidth) / 2;
 
       g.setColor(textColor[STATUS]);
@@ -178,6 +197,12 @@ public class PdeEditorHeader extends JComponent {
       g.drawImage(pieces[state][RIGHT], x, 0, null);
       x += PIECE_WIDTH - 1;  // overlap by 1 pixel
     }
+
+    menuLeft = sizeW - (16 + pieces[0][MENU].getWidth());
+    menuRight = sizeW - 16;
+    // draw the dropdown menu target
+    g.drawImage(pieces[menuVisible ? SELECTED : UNSELECTED][MENU], 
+                menuLeft, 0);
 
     /*
     sketchTitleLeft = PdePreferences.GUI_SMALL;
@@ -200,6 +225,96 @@ public class PdeEditorHeader extends JComponent {
     */
 
     screen.drawImage(offscreen, 0, 0, null);
+  }
+
+
+  /*
+    New [code,tab,source] file...  Cmd-shift-N
+    Rename
+    Delete
+    Hide
+    Unhide  >
+    Reset file list (not needed?)
+  */
+  public JMenu rebuildMenu() {
+    if (menu != null) {
+      menu.removeAll();
+    } else {
+      menu = new JMenu();
+    }
+    JMenuItem item;
+
+    item = new JMenuItem("New");
+    item.addActionListener(new ActionListener() {
+        public void actionPerformed(ActionEvent e) { 
+        }
+      });
+    menu.add(item);
+
+    item = new JMenuItem("Rename");
+    item.addActionListener(new ActionListener() {
+        public void actionPerformed(ActionEvent e) { 
+        }
+      });
+    menu.add(item);
+
+    item = new JMenuItem("Delete");
+    item.addActionListener(new ActionListener() {
+        public void actionPerformed(ActionEvent e) { 
+        }
+      });
+    menu.add(item);
+
+    item = new JMenuItem("Hide");
+    item.addActionListener(new ActionListener() {
+        public void actionPerformed(ActionEvent e) { 
+          // don't let the user hide if only 1 file open
+        }
+      });
+    menu.add(item);
+
+    JMenu unhide = new JMenu("Unhide");
+    ActionListener unhideListener = new ActionListener() {
+        public void actionPerformed(ActionEvent e) { 
+          //System.out.println("unhide " + e);
+          File from = (File) e.getActionCommand();
+          String filename = from.getName();
+          if (!filename.endsWith(".x")) {
+            System.err.println("error while trying to unhide a file");
+          } else if (!filename.exists()) {
+            System.err.println("file no longer exists");
+          } else {
+            File to = new File(from.getPath(), 
+                               filename.substring(0, filename.length() - 2));
+            if (!from.renameTo(to)) {
+              System.err.println("problem while unhiding");
+            }
+          }
+          rebuildMenu();
+        }
+      };
+    for (int i = 0; i < hiddenCount; i++) {
+      item = new JMenuItem(hiddenNames[i]);
+      item.setActionCommand(hiddenFiles[i]);
+      item.addActionListener(unhideListener);
+      hidden.add(item);
+    }
+    menu.add(unhide);
+
+    menu.addSeparator();
+
+    ActionListener jumpListener = new ActionListener() {
+        public void actionPerformed(ActionEvent e) { 
+          //System.out.println("jump to " + e.getActionCommand());
+          System.out.println("jump to " + e);
+        }
+      };
+    for (int i = 0; i < fileCount; i++) {
+      item = new JMenuItem(names[i]);
+      //item.setActionCommand(files[i]);
+      item.addActionListener(jumpListener);
+      menu.add(item);
+    }
   }
 
 
