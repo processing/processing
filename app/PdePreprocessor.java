3f50bfd64a3a707191f9ae8b15d5e422b188334d
benfry <fry@processing.org>
2004-07-12 22:06:35 +0000

    fixed bug for empty sketches

--- app/PdePreprocessor.java
+++ app/PdePreprocessor.java
@@ -81,7 +81,8 @@ public class PdePreprocessor {
                       String extraImports[]) throws java.lang.Exception {
     // if the program ends with no CR or LF an OutOfMemoryError will happen.
     // not gonna track down the bug now, so here's a hack for it:
-    if (program.charAt(program.length()-1) != '\n') {
+    if ((program.length() > 0) &&
+        program.charAt(program.length()-1) != '\n') {
       program += "\n";
     }
 
