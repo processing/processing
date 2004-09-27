7d5e6c5490b1bd5e953ffd98ad6a1b84cb580174
benfry <fry@processing.org>
2004-09-27 20:42:45 +0000

    updating linux and removing the unused 'design' directory

--- build/linux/dist.sh
+++ build/linux/dist.sh
@@ -9,18 +9,12 @@ echo Creating linux distribution for revision $REVISION...
 # remove any old boogers
 rm -rf processing
 rm -rf processing-*
-#rm -f processing-*.tgz
 
 # use 'shared' files as starting point
 cp -r ../shared processing
-# something like the following might be better:
-# find / -name "*.mp3" -exec rm -f {}\;
-# and same for cvsignore
 rm -rf processing/CVS
 rm -rf processing/lib/CVS
-rm -rf processing/lib/netscape/CVS
-rm -rf processing/lib/netscape/javascript/CVS
-rm -rf processing/fonts/CVS
+
 rm -rf processing/reference/CVS
 rm -rf processing/reference/images/CVS
 rm -rf processing/sketchbook/CVS
@@ -47,41 +41,28 @@ mkdir processing/lib/build
 
 # grab pde.jar and export from the working dir
 cp work/lib/pde.jar processing/lib/
-cp -r work/lib/export processing/lib/
-rm -rf processing/lib/export/CVS
+cp work/lib/core.jar processing/lib/
 
 # get platform-specific goodies from the dist dir
-#cp work/Proce55ing processing/
-#cp work/processing processing/
-install -m 755 stub.sh processing/processing
-#cp dist/lib/pde_linux.properties processing/lib/
+install -m 755 dist/processing processing/processing
+cp dist/jikes processing/
+chmod +x processing/jikes
 
 # make sure notes.txt is unix LFs
 # the 2> is because the app is a little chatty
 dos2unix processing/readme.txt 2> /dev/null
 dos2unix processing/revisions.txt 2> /dev/null
 dos2unix processing/lib/pde.properties 2> /dev/null
-#dos2unix processing/lib/pde_linux.properties 2> /dev/null
 
-# get the serial stuff
-echo Copying serial support from bagel dir
-cp ../../bagel/serial/RXTXcomm.jar processing/lib/
-mkdir processing/lib/i386
-cp ../../bagel/serial/librxtxSerial.so processing/lib/i386/libSerial.so
-
-# get jikes and depedencies
-#gunzip < dist/jikes.gz > processing/jikes
-cp dist/jikes processing/
-chmod +x processing/jikes
+# remove boogers
+find processing -name "*~" -exec rm -f {} ';'
+find processing -name ".DS_Store" -exec rm -f {} ';'
+find processing -name "._*" -exec rm -f {} ';'
 
 # zip it all up for release
 echo Creating tarball and finishing...
 P5=processing-$REVISION
 mv processing $P5
-
-echo files to remove...
-cd $P5
-find . | grep .DS_Store
 
 #tar cfz $P5.tgz $P5
 # nah, keep the new directory around
