7b2d43c0e2d9f70e851c784ece6fc1e8583b6c7f
benfry <fry@processing.org>
2004-10-15 01:33:11 +0000

    mostly cleaning up the PLine.java file

--- core/PGraphics.java
+++ core/PGraphics.java
@@ -142,10 +142,10 @@ public class PGraphics extends PImage
    * Model transformation of the form m[row][column],
    * which is a "column vector" (as opposed to "row vector") matrix.
    */
-  float m00, m01, m02, m03;
-  float m10, m11, m12, m13;
-  float m20, m21, m22, m23;
-  float m30, m31, m32, m33;
+  public float m00, m01, m02, m03;
+  public float m10, m11, m12, m13;
+  public float m20, m21, m22, m23;
+  public float m30, m31, m32, m33;
 
   int angle_mode;
 
