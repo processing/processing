20294b178327abff3d254a10e47f77e1f9350a89
benfry <fry@processing.org>
2005-01-30 18:44:08 +0000

    windows registry work

--- build/macosx/run.sh
+++ build/macosx/run.sh
@@ -1,16 +1,7 @@
 #!/bin/sh
 
-# -Dcom.apple.hwaccel=false 
-#cd work && java -Dcom.apple.awt.antialiasing=off PdeBase
-
-# old rxtx
-#CLASSPATH=/System/Library/Frameworks/JavaVM.framework/Classes/classes.jar:/System/Library/Frameworks/JavaVM.framework/Classes/ui.jar:/System/Library/Frameworks/JavaVM.framework/Home/lib/ext/comm.jar:/System/Library/Java/Extensions/QTJava.zip:lib:lib/build:lib/pde.jar:lib/kjc.jar:lib/oro.jar:../comm.jar
-
-# rxtx 2.1.6
-#CLASSPATH=/System/Library/Frameworks/JavaVM.framework/Classes/classes.jar:/System/Library/Frameworks/JavaVM.framework/Classes/ui.jar:/System/Library/Frameworks/JavaVM.framework/Home/lib/ext/comm.jar:/System/Library/Java/Extensions/QTJava.zip:lib:lib/build:lib/pde.jar:lib/kjc.jar:lib/oro.jar:lib/RXTXcomm.jar
-
 # is qt java already included tho?
-CLASSPATH=/System/Library/Java/Extensions/QTJava.zip:lib:lib/build:lib/pde.jar:lib/core.jar:lib/antlr.jar:lib/oro.jar:lib/RXTXcomm.jar
+CLASSPATH=/System/Library/Java/Extensions/QTJava.zip:lib:lib/build:lib/pde.jar:lib/core.jar:lib/antlr.jar:lib/oro.jar:lib/registry.jar
 
 export CLASSPATH
 
