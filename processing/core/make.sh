40862fb69d2b82ce90c73ce81e4a3d7ee7056e62
benfry <fry@processing.org>
2005-04-17 18:58:57 +0000

    writing the faq, updating some code for beta

--- processing/core/make.sh
+++ processing/core/make.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 #C:\jdk-1.4.2_05\bin
-#/cygdrive/c/jdk-1.4.2_05/bin/javadoc -d doc *.java
+/cygdrive/c/jdk-1.4.2_05/bin/javadoc -d doc *.java
 #javadoc -public -d doc *.java
 ./preproc.pl
 jikes -d . +D *.java
