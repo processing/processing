e1d93595b5b4b7c87b7d7b77c3b6881632fa016b
benfry <fry@processing.org>
2005-04-17 23:39:04 +0000

    consolidation of bugs.txt and the rest into the faq

--- build/windows/dist/run.bat
+++ build/windows/dist/run.bat
@@ -9,7 +9,7 @@ set SAVEDPATH=%PATH%
 set CLASSPATH=java\lib\rt.jar;lib;lib\build;lib\pde.jar;lib\core.jar;lib\antlr.jar;lib\oro.jar;lib\registry.jar;lib\mrj.jar;%windir%\system32\qtjava.zip;%windir%\system\qtjava.zip
 set PATH=java\bin;%PATH%
 
-start javaw -ms128m -mx128m PdeBase
+start javaw -ms128m -mx128m processing.app.Base
 REM start .\java\bin\javaw -ms128m -mx128m PdeBase
 
 set CLASSPATH=%SAVEDCP%
