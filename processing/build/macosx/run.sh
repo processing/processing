0ce20ab905c7ef84d8c34a07040ae0bc1b80c84c
no_author <bugs@processing.org>
2005-04-17 06:08:39 +0000

    This commit was manufactured by cvs2svn to create tag 'mobile-0074'.

--- processing/build/macosx/run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# is qt java already included tho?
-CLASSPATH=/System/Library/Java/Extensions/QTJava.zip:lib:lib/build:lib/pde.jar:lib/core.jar:lib/antlr.jar:lib/oro.jar:lib/registry.jar
-
-export CLASSPATH
-
-#cd work && /System/Library/Frameworks/JavaVM.framework/Versions/1.3.1/Commands/java -Dcom.apple.macos.useScreenMenuBar=true PdeBase
-cd work && java -Dapple.laf.useScreenMenuBar=true -Dapple.awt.showGrowBox=false processing.app.Base
