9e963280e915633a834d4854d22b9c6029447283
francisli <mail@francisli.com>
2005-04-20 06:53:36 +0000

    Build modified to check out processing-0085

--- mobile/web/download/index.php
+++ mobile/web/download/index.php
@@ -5,13 +5,14 @@ require '../header.inc.php';
 ?>
 <h2>Processing Mobile pre-Alpha</h2>
 <ol>
-  <li>Download and install the Wireless Toolkit (WTK) from Sun. The latest version is 2.1.<br><br>
+  <li>Download and install the Wireless Toolkit (WTK) from Sun. The latest version is 2.2.<br><br>
 <a href="http://java.sun.com/products/j2mewtoolkit/">http://java.sun.com/products/j2mewtoolkit/</a><br>&nbsp;</li>
   <li>Download and install Processing Mobile.<br><br>
+0074 pre-ALPHA&nbsp;&nbsp;|&nbsp;&nbsp;31 01 2005&nbsp;&nbsp;Windows <a href="processing-0074-expert.zip">Expert</a><br>
 0071 pre-ALPHA&nbsp;&nbsp;|&nbsp;&nbsp;17 10 2004&nbsp;&nbsp;Windows <a href="processing-0071-expert.zip">Expert</a><br>&nbsp;</li>
   <li>Modify <b>preferences.txt</b> to include the following line, based on the installation location of the WTK:<br>
 <br>
-wtk.path=C:\WTK21<br>&nbsp;</li>
+wtk.path=C:\WTK22<br>&nbsp;</li>
 </ol>
 
 <?php
