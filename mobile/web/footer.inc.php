369a2d48a97f43a58cf24c9d4bfb7f18b12811ac
no_author <bugs@processing.org>
2005-06-07 05:14:53 +0000

    This commit was manufactured by cvs2svn to create tag 'processing-0091'.

--- mobile/web/footer.inc.php
+++
@@ -1,8 +0,0 @@
-</div>
-<div id="footer"> 
-    <a href="http://www.processing.org" target="_blank">Processing</a> is 
-        &copy; 2004-2001 Massachusetts Institute of Technology and Interaction 
-        Design Institute Ivrea </p>
-</div>
-</body>
-</html>
