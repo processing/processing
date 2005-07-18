d823124648de494f360cf6fc4a978ea0837fced9
benfry <fry@processing.org>
2005-07-18 22:36:43 +0000

    fixes for the new dev site

--- processing/build/windows/run.sh
+++ processing/build/windows/run.sh
@@ -3,18 +3,18 @@
 QT_JAVA_PATH="$WINDIR\\system32\\QTJava.zip"
 if test -f "${QT_JAVA_PATH}"
 then
-  #echo "Found Quicktime at $QT_JAVA_PATH"
+  echo "Found Quicktime at $QT_JAVA_PATH"
 else 
   QT_JAVA_PATH="$WINDIR\\system\\QTJava.zip"
   if test -f "${QT_JAVA_PATH}"
+  then
     echo "could not find qtjava.zip in either"
     echo "${WINDIR}\\system32\\qtjava.zip or"
     echo "${WINDIR}\\system\\qtjava.zip"
     echo "quicktime for java must be installed before building."
     exit 1;
-  then
-    #echo "Found Quicktime at $QT_JAVA_PATH"
   else
+    echo "Found Quicktime at $QT_JAVA_PATH"
   fi
 fi
 
