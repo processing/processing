a727533c226646bb1667aa2fe4b7b175764eae36
francisli <mail@francisli.com>
2005-08-22 07:00:29 +0000

    Adding an example midlet

---
+++ web/learning/examples/shapeprimitives/midlet/shapeprimitives.java
@@ -0,0 +1,22 @@
+import processing.core.*; public class shapeprimitives extends PMIDlet{public void setup() {// Shape Primitives 
+// by REAS <http://reas.com> 
+background(0); 
+noStroke(); 
+fill(226); 
+triangle(10, 10, 10, 200, 45, 200); 
+rect(45, 45, 35, 35); 
+quad(105, 10, 120, 10, 120, 200, 80, 200); 
+ellipse(140, 80, 40, 40); 
+triangle(160, 10, 195, 200, 160, 200); 
+ 
+
+ 
+
+
+
+
+
+
+
+	 
+noLoop(); }}
