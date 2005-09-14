0588d0fcad77a7175e74d636761423fcc58b3e12
lennyjpg <info@leanderherzog.ch>
2005-09-14 23:15:19 +0000

    Initial templates commit

---
+++ web/templates/template.nav.php
@@ -0,0 +1,159 @@
+<?
+
+$pages = array(
+    'Cover'         => array('/index.php', 0),
+    'Exhibition'    => array('/exhibition/index.html', 1),
+    'Learning'      => array('/learning/index.html', 1),
+    'Examples'      => array('/learning/examples/index.html', 2),
+    'Tutorials'     => array('/learning/tutorials/index.html', 2),
+    'Reference'     => array('/reference/index.html', 1),
+    'Download'      => array('/download/index.html', 1),
+    'Discourse'     => array('/discourse/index.html', 1),
+    'Contribute'    => array('/contribute/index.html', 1),
+    'FAQ'           => array('/faq/index.html', 1),
+    'Language'      => array('/reference/index.html', 1),
+    'Environment'   => array('/reference/environment/index.html', 2),
+    'Libraries'     => array('/reference/libraries/index.html', 2),
+    'Comparison'    => array('/reference/compare/index.html', 2)
+    
+    );
+
+function navigation($section = '')
+{  
+    $ref = array('Reference', 'Language', 'Environment', 'Libraries', 'Comparison');
+    $learn = array('Learning', 'Examples', 'Tutorials');
+    
+    $html = "\t\t\t".'<div id="navigation">'."\n";
+
+    $id = (in_array($section, $ref) || in_array($section, $learn)) ? 'mainnav' : 'mainnav_noSub';    
+    $html .= "\t\t\t\t".'<div class="navBar" id="'.$id.'">'."\n";
+    
+    $html .= "\t\t\t\t\t" . l('Cover', $section == 'Cover') . " \\\n";
+    $html .= "\t\t\t\t\t" . l('Exhibition', $section == 'Exhibition') . " \\\n";
+    $html .= "\t\t\t\t\t" . l('Learning', in_array($section, $learn)) . " \\\n";
+    $html .= "\t\t\t\t\t" . l('Reference', in_array($section, $ref)) . " \\\n";
+    $html .= "\t\t\t\t\t" . l('Download', $section == 'Download') . " \\\n";
+    $html .= "\t\t\t\t\t" . l('Discourse', $section == 'Discourse') . " \\\n";
+    $html .= "\t\t\t\t\t" . l('Contribute', $section == 'Contribute') . "\n";
+    $html .= "\t\t\t\t\t" . "<a href=\"/faq/index.html\"" . ($section == 'FAQ' ? ' class="active faq"' : 'class="faq"') . ">FAQ</a>\n";
+       
+    $html .= "\t\t\t\t</div>\n";
+    
+    if (in_array($section, $ref)) {
+        $html .= "\t\t\t\t" . '<div class="navBar" id="subNav">' . "\n";
+        
+        $html .= "\t\t\t\t\t" . l('Language', $section == 'Language') . " \\\n";
+        $html .= "\t\t\t\t\t" . l('Environment', $section == 'Environment') . " \\\n";
+        $html .= "\t\t\t\t\t" . l('Libraries', $section == 'Libraries') . " \\\n";
+        $html .= "\t\t\t\t\t" . l('Comparison', $section == 'Comparison') . "\n";
+        
+        $html .= "\t\t\t\t</div>\n";
+    } else if (in_array($section, $learn)) {
+        $html .= "\t\t\t\t" . '<div class="navBar learning" id="subNav">' . "\n";
+        
+        $html .= "\t\t\t\t\t" . l('Examples', $section == 'Examples') . " \\\n";
+        $html .= "\t\t\t\t\t" . l('Tutorials', $section == 'Tutorials') . "\n";
+        
+        $html .= "\t\t\t\t</div>\n";
+    }
+
+    
+    return $html . "\t\t\t</div>\n";
+}
+
+function l($s, $c)
+{
+    global $pages;
+    return "<a href=\"{$pages[$s][0]}\"" . ($c ? ' class="active"' : '') . "\">$s</a>";
+}
+
+function short_nav($section)
+{
+    $html  = "\t\t\t".'<div id="navigation">'."\n";
+    $html .= "\t\t\t\t".'<div class="navBar" id="mainnav_noSub">'."\n";
+    
+    $html .= "\t\t\t\t\t<a href=\"http://processing.org/index.php\"" . ($section == 'Cover' ? ' class="active"' : '') . ">Cover</a> \\ \n";
+    $html .= "\t\t\t\t\t<a href=\"/reference/index.html\"" . ($section == 'Language' ? ' class="active"' : '') . ">Langauge</a> \\ \n";
+    $html .= "\t\t\t\t\t<a href=\"/reference/environment/index.html\"" . ($section == 'Environment' ? ' class="active"' : '') . ">Environment</a> \\ \n";
+    $html .= "\t\t\t\t\t<a href=\"/reference/libraries/index.html\"" . ($section == 'Libraries' ? ' class="active"' : '') . ">Libraries</a> \\ \n";
+    $html .= "\t\t\t\t\t<a href=\"/reference/compare/index.html\"" . ($section == 'Comparison' ? 'class="active"' : '') . ">Comparison</a>\n";
+    
+    $html .= "\t\t\t\t</div>\n";
+    $html .= "\t\t\t</div>\n";
+    
+    return $html;
+}
+
+function navigation_tr($section)
+{
+    global $lang;
+    global $translation;
+    $tr = $translation->navigation;
+    
+    $html  = "\t\t\t".'<div id="navigation">'."\n";
+    $html .= "\t\t\t\t".'<div class="navBar" id="mainnav_noSub">'."\n";
+    
+    $html .= "\t\t\t\t\t<a href=\"/index.php\"" . ($section == 'Cover' ? ' class="active"' : '') . ">$tr[cover]</a> \\ \n";
+    $html .= "\t\t\t\t\t<a href=\"/reference/$lang/index.html\"" . ($section == 'Language' ? ' class="active"' : '') . ">$tr[language]</a> \\ \n";
+    $html .= "\t\t\t\t\t<a href=\"/reference/$lang/environment/index.html\"" . ($section == 'Environment' ? ' class="active"' : '') . ">$tr[environment]</a> \\ \n";
+    $html .= "\t\t\t\t\t<a href=\"/reference/$lang/libraries/index.html\"" . ($section == 'Libraries' ? ' class="active"' : '') . ">$tr[libraries]</a> \\ \n";
+    $html .= "\t\t\t\t\t<a href=\"/reference/$lang/compare/index.html\"" . ($section == 'Comparison' ? 'class="active"' : '') . ">$tr[comparison]</a>\n";
+    
+    $html .= "\t\t\t\t</div>\n";
+    $html .= "\t\t\t</div>\n";
+    
+    return $html;
+}
+
+function reference_nav($current = '')
+{
+    global $lang;
+    global $translation;
+    global $LANGUAGES;
+    $tr = $translation->navigation;
+    
+    $html = "<a href=\"index.html\">$tr[abridged]</a>";
+    if ($LANGUAGES[$lang][2]) {
+        $html .= " (<a href=\"index_alpha.html\">$tr[az]</a>)";
+    }
+    $html .= " \ <a href=\"index_ext.html\">$tr[complete]</a>";
+    if ($LANGUAGES[$lang][2]) {
+        $html .= " (<a href=\"index_alpha_ext.html\">$tr[az]</a>)";
+    }
+    return $html;
+}
+
+function language_nav($current)
+{
+    global $LANGUAGES;
+    
+    $html = "\t".'<select name="nav" size="1" class="refnav" onChange="javascript:gogo(this)">'."\n";
+    foreach ($LANGUAGES as $short => $array) {
+        if ($array[3] != '' ) {
+            $sel = ($current == $short) ? ' selected="selected"' : '';
+            $html .= "\t\t<option value=\"$array[3]\"$sel>$array[0]</option>\n";
+        }
+    }
+    $html .= "\t</select>\n";
+    return $html;
+}
+
+
+/** test **
+?>
+<style>
+
+#mainnav { background: #cfc; }
+#mainnav_noSub { background: #fcc; }
+#mainnav, #mainnav_noSub { width: 600px; position: relative;}
+#subNav { margin-left: 190px; }
+.active { font-weight: bold; }
+.faq { position: absolute; right: 10px; }
+
+</style>
+<?
+foreach ($navigation_pages as $page => $array) {
+    echo navigation($page);
+}
+**/
+?>
---
+++ web/templates/template.php
@@ -0,0 +1,189 @@
+<?
+
+require(TEMPLATEDIR.'template.nav.php');
+
+class Page
+{
+    var $xhtml;
+    var $lang;
+    var $subtemplate = false;
+    
+    function Page($title = '', $section = '', $bodyid = '')
+    {
+        $bodyid = ($bodyid == '') ? $section : $bodyid;
+        $this->xhtml = new xhtml_page(TEMPLATEDIR.'template.html');
+        if ($section == 'Cover') {
+            $this->xhtml->set('header', '<img src="/img/processing_beta_cover.gif" alt="Processing cover" />');
+        } else {
+            $this->xhtml->set('header', '<a href="http://processing.org/"><img src="/img/processing_beta.gif" alt="Processing cover" title="Back to the cover." /></a>');
+        }
+        $this->xhtml->set('bodyid', $bodyid);
+        $title = ($title == '') ? 'Processing 1.0 (BETA)' : $title . ' \ Processing 1.0 (BETA)';
+        $this->xhtml->set('title', $title);
+        $this->xhtml->set('navigation', navigation($section));
+    }
+    
+    function set($key, $value)
+    {
+        $this->xhtml->set($key, $value);
+    }
+    
+    function set_array($array)
+    {
+        foreach ($array as $key => $value) {
+            $this->xhtml->set($key, $value);
+        }
+    }
+    
+    function subtemplate($file)
+    {
+        $piece = new xhtml_piece(TEMPLATEDIR.$file);
+        $this->xhtml->set('content_for_layout', $piece->out());
+        $this->subtemplate = true;
+    }
+    
+    function content($content)
+    {
+        if (!$this->subtemplate) {
+            $this->xhtml->set('content_for_layout', $content);
+        } else {
+            $this->xhtml->set('content', $content);
+        }
+    }
+    
+    function language($lang)
+    {
+        global $LANGUAGES;
+        $this->lang = $lang;
+        $this->xhtml->set('charset', $LANGUAGES[$lang][1]);
+        $this->xhtml->set('lang', $lang);
+        if ($lang != 'en')
+            $this->xhtml->set('navigation', navigation_tr($section));
+    }
+    
+    function out()
+    {
+        if (!$this->lang) { $this->language('en'); }
+        return $this->xhtml->out();
+    }
+
+}
+
+class ReferencePage
+{
+    var $xhtml;
+    var $lang;
+    var $filepath;
+    
+    function ReferencePage(&$ref, $translation, $lang = 'en')
+    {
+        global $LANGUAGES;
+        
+        $this->filepath = 'reference/' . ($lang == 'en' ? '' : "$lang/") . $ref->name();
+        $title = $ref->title() . ($lang == 'en' ? '' : " \ {$LANGUAGES[$lang][0]}") .' \ Language (API) \ Processing 1.0 (BETA)';
+        
+        $xhtml = new xhtml_page(TEMPLATEDIR.'template.translation.html');
+        $xhtml->set('header', '<a href="http://processing.org/"><img src="/img/processing_beta.gif" alt="Processing cover" title="Back to the cover." /></a>');
+        $xhtml->set('title', $title);
+        $xhtml->set('bodyid', 'Langauge-'.$lang);
+        if ($lang == 'en') {
+            $xhtml->set('navigation', navigation('Language'));
+        } else {
+            $xhtml->set('navigation', navigation_tr('Language'));
+        }
+        
+        $piece = new xhtml_piece(TEMPLATEDIR.'template.reference.item.html');
+        $xhtml->set('content_for_layout', $piece->out());
+        
+        $xhtml->set('reference_nav', reference_nav());
+        $xhtml->set('language_nav', language_nav($lang));
+        
+        $xhtml->set('content', $ref->display());
+        foreach ($translation->attributes as $key => $value) {
+            $xhtml->set($key, $value);
+        }
+        
+        foreach ($translation->meta as $key => $value) {
+            $xhtml->set($key, $value);
+        }
+        
+        
+        $this->xhtml = $xhtml;
+        $this->language($lang);
+    }
+    
+    function language($lang)
+    {
+        global $LANGUAGES;
+        $this->lang = $lang;
+        $this->xhtml->set('charset', $LANGUAGES[$lang][1]);
+        $this->xhtml->set('lang', $lang);
+    }
+
+    function out()
+    {
+        return $this->xhtml->out();
+    }
+    
+    function write()
+    {
+        writeFile($this->filepath, $this->xhtml->out());
+    }   
+}
+
+class LocalPage extends Page
+{
+    var $xhtml;
+    var $lang = 'en';
+    var $subtemplate = false;
+    
+    function LocalPage($title = '', $section = '', $bodyid = '')
+    {
+        $this->xhtml = new xhtml_page(TEMPLATEDIR.'template.local.html');
+        $this->xhtml->set('header', '<a href="http://processing.org/"><img src="img/processing_beta.gif" alt="Processing cover" title="Back to the cover." /></a>');
+        $bodyid = ($bodyid == '') ? $section : $bodyid;
+        $this->xhtml->set('bodyid', $bodyid);
+        $title = ($title == '') ? 'Processing 1.0 (BETA)' : $title . ' \ Processing 1.0 (BETA)';
+        $this->xhtml->set('title', $title);
+        $this->xhtml->set('navigation', short_nav($section));
+    }
+}
+
+class LocalReferencePage extends ReferencePage
+{
+    var $xhtml;
+    var $lang = 'en';
+    var $filepath;
+    
+    function LocalReferencePage(&$ref, $translation, $lang = 'en')
+    {        
+        $this->filepath = 'distribution/' . $ref->name();
+        $title = $ref->title() .' \ Language (API) \ Processing 1.0 (BETA)';
+        
+        $xhtml = new xhtml_page(TEMPLATEDIR.'template.local.html');
+        $xhtml->set('header', '<a href="http://processing.org/"><img src="img/processing_beta.gif" alt="Processing cover" title="Back to the cover." /></a>');
+        $xhtml->set('title', $title);
+        $xhtml->set('bodyid', 'Langauge');
+        $xhtml->set('navigation', short_nav('Language'));
+        
+        $piece = new xhtml_piece(TEMPLATEDIR.'template.reference.item.html');
+        $xhtml->set('content_for_layout', $piece->out());
+        
+        $xhtml->set('reference_nav', reference_nav());
+        $xhtml->set('language_nav', language_nav($lang));
+        
+        $xhtml->set('content', $ref->display());
+        foreach ($translation->attributes as $key => $value) {
+            $xhtml->set($key, $value);
+        }
+        
+        foreach ($translation->meta as $key => $value) {
+            $xhtml->set($key, $value);
+        }
+        
+        $this->xhtml = $xhtml;
+        $this->language($lang);
+    }
+}
+
+?>
