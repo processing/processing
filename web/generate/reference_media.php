f17201022d971d063837f534c1ec62501865022b
lennyjpg <info@leanderherzog.ch>
2005-09-17 00:37:10 +0000

    permissions bug

--- web/generate/reference_media.php
+++ web/generate/reference_media.php
@@ -4,7 +4,7 @@ require('../config.php');
 $benchmark_start = microtime_float();
 
 if (!is_dir(REFERENCEDIR.'media')) { mkdir(REFERENCEDIR.'media', '0757'); }
-copydirr(CONTENTDIR.'api_media', REFERENCEDIR.'media', null, '0757', true);
+copydirr(CONTENTDIR.'api_media', REFERENCEDIR.'media', null, 0757, true);
 
 $benchmark_end = microtime_float();
 $execution_time = round($benchmark_end - $benchmark_start, 4);
