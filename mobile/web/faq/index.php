9948af44c35664952baa259da413c4a3e2e7f3f1
francisli <mail@francisli.com>
2005-09-19 20:53:48 +0000

    Added faq

---
+++ mobile/web/faq/index.php
@@ -0,0 +1,11 @@
+<?php
+
+$PAGE_TITLE = "Mobile Processing &raquo; FAQ";
+require '../header.inc.php';
+?>
+<img src="images/header.png"><br>
+<br>
+Coming soon...
+<?php
+ require '../footer.inc.php';
+?>
