9ab9cec901a4ffb6a7d1d9f570c56558167e95c0
francisli <mail@francisli.com>
2005-09-27 00:18:37 +0000

    Web updates

--- mobile/web/learning/subnavigation.inc.php
+++ mobile/web/learning/subnavigation.inc.php
@@ -1,13 +1,12 @@
 <?php
-$section = 0;
-if (stristr($_SERVER['PHP_SELF'], SITE_ROOT .'learning/tutorials') 
+$section = -1;
+if (stristr($_SERVER['PHP_SELF'], SITE_ROOT .'learning/index.php') 
     !== false) {
+    $section = 0;
+} else if (stristr($_SERVER['PHP_SELF'], 
+                   SITE_ROOT .'learning/tutorials/index.php') !== false) {
     $section = 1;
-} else if (stristr($_SERVER['PHP_SELF'], SITE_ROOT .'learning/example.php') 
-    !== false) {
-    $section = 2;
 }
-
 ?>
 
 <div id="subnavigation" style="padding-left: 80px">
