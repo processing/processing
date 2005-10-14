7bf96cb360b3b2c510e12378133d6115fdadf73d
benfry <fry@processing.org>
2005-10-14 17:03:02 +0000

    fixes to dist script to remove .svn folders

--- build/macosx/dist.sh
+++ build/macosx/dist.sh
@@ -83,6 +83,7 @@ find processing -name "Thumbs.db" -exec rm -f {} ';'
 # clean out the cvs entries
 find processing -name "CVS" -exec rm -rf {} ';' 2> /dev/null
 find processing -name ".cvsignore" -exec rm -rf {} ';'
+find processing -name ".svn" -exec rm -rf {} ';'
 
 mv processing/Processing.app "processing/Processing $SHORT_REVISION.app"
 mv processing processing-$REVISION
