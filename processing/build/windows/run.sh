fe07d05bd8217ee9b23e95e7cfba51215eef675d
benfry <fry@processing.org>
2005-10-14 16:55:31 +0000

    last bits for rev 93, mostly getting qt7 support working

--- processing/build/windows/run.sh
+++ processing/build/windows/run.sh
@@ -1,22 +1,37 @@
 #!/bin/sh
 
-QT_JAVA_PATH="$WINDIR\\system32\\QTJava.zip"
-if test -f "${QT_JAVA_PATH}"
-then
-  echo "Found Quicktime at $QT_JAVA_PATH"
-else 
-  QT_JAVA_PATH="$WINDIR\\system\\QTJava.zip"
-  if test -f "${QT_JAVA_PATH}"
+if test -f "${QTJAVA}"
+then 
+  echo "Found Quicktime 7 installation"
+else
+  QTJAVA="$WINDIR\\system32\\QTJava.zip"
+  if test -f "${QTJAVA}"
   then
-    echo "could not find qtjava.zip in either"
-    echo "${WINDIR}\\system32\\qtjava.zip or"
-    echo "${WINDIR}\\system\\qtjava.zip"
-    echo "quicktime for java must be installed before building."
+    echo "Found Quicktime 6 at $QTJAVA"
+  else 
+    echo "Could not find QuickTime for Java,"
+    echo "you'll need to install it before running."
     exit 1;
-  else
-    echo "Found Quicktime at $QT_JAVA_PATH"
   fi
 fi
+
+#QT_JAVA_PATH="$WINDIR\\system32\\QTJava.zip"
+#if test -f "${QT_JAVA_PATH}"
+#then
+#  echo "Found Quicktime at $QT_JAVA_PATH"
+#else 
+#  QT_JAVA_PATH="$WINDIR\\system\\QTJava.zip"
+#  if test -f "${QT_JAVA_PATH}"
+#  then
+#    echo "could not find qtjava.zip in either"
+#    echo "${WINDIR}\\system32\\qtjava.zip or"
+#    echo "${WINDIR}\\system\\qtjava.zip"
+#    echo "quicktime for java must be installed before running."
+#    exit 1;
+#  else
+#    echo "Found Quicktime at $QT_JAVA_PATH"
+#  fi
+#fi
 
 # rxtx testing
 #CLASSPATH=java\\lib\\rt.jar\;lib\;lib\\build\;lib\\pde.jar\;lib\\kjc.jar\;lib\\oro.jar\;lib\\RXTXcomm.jar\;${QT_JAVA_PATH}
@@ -30,7 +45,7 @@ fi
 # includes jaws.jar
 #CLASSPATH=\"java\\lib\\rt.jar\;java\\lib\\jaws.jar\;lib\;lib\\build\;lib\\pde.jar\;lib\\kjc.jar\;lib\\antlr.jar\;lib\\oro.jar\;lib\\comm.jar\;lib\\RXTXcomm.jar\;${QT_JAVA_PATH}\"
 
-CLASSPATH=\"java\\lib\\rt.jar\;lib\;lib\\build\;lib\\pde.jar\;lib\\core.jar\;lib\\mrj.jar\;lib\\antlr.jar\;lib\\oro.jar\;lib\\registry.jar\;${QT_JAVA_PATH}\"
+CLASSPATH=\"java\\lib\\rt.jar\;lib\;lib\\build\;lib\\pde.jar\;lib\\core.jar\;lib\\mrj.jar\;lib\\antlr.jar\;lib\\oro.jar\;lib\\registry.jar\;${QTJAVA}\"
 export CLASSPATH
 
 #cd work && ./java/bin/java -Xint PdeBase
