5f52089c807a93a0d751befd6ed658ecca5a11f5
benfry <fry@processing.org>
2005-11-02 19:38:11 +0000

    revving the version number, finalizing 0095

--- app/Base.java
+++ app/Base.java
@@ -50,8 +50,8 @@ import processing.core.*;
  * files and images, etc) that comes from that.
  */
 public class Base {
-  static final int VERSION = 94;
-  static final String VERSION_NAME = "0094 Beta";
+  static final int VERSION = 95;
+  static final String VERSION_NAME = "0095 Beta";
 
   /**
    * Path of filename opened on the command line,
