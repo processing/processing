eac759f2a26a084491aa23743f35708fb2ddc0d2
francisli <mail@francisli.com>
2005-11-07 19:36:59 +0000

    Video library docs

---
+++ web/reference/libraries/video/index.php
@@ -0,0 +1,27 @@
+<?php
+
+$PAGE_TITLE = "Video &raquo; Libraries &raquo; Mobile Processing";
+
+$PAGE_SHOWBACKINDEX = true;
+$PAGE_BACK_LINK = '../index.php';
+
+require '../../../header.inc.php';
+?>
+<div class="column">
+<h3>Video</h3><br>
+<br>
+The Video library allows Mobile Processing sketches to play and record sounds on supported mobile phones.
+</div>
+<div class="column" style="padding-left: 40px">
+<b>Capture</b><br>
+The capture class allows live video output and frame capture.<br>
+<br>
+<a href="reference.php?name=Capture">Capture</a><br>
+<a href="reference.php?name=Capture_close">close()</a><br>
+<a href="reference.php?name=Capture_hide">hide()</a><br>
+<a href="reference.php?name=Capture_read">read()</a><br>
+<a href="reference.php?name=Capture_show">show()</a><br>
+</div>
+<?php
+require '../../../footer.inc.php';
+?>
