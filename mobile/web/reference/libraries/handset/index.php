117ab722068be38b6818ef5f74bb3e47ed8c0d0e
francisli <mail@francisli.com>
2005-11-07 19:10:39 +0000

    Handset library docs

---
+++ mobile/web/reference/libraries/handset/index.php
@@ -0,0 +1,25 @@
+<?php
+
+$PAGE_TITLE = "Handset &raquo; Libraries &raquo; Mobile Processing";
+
+$PAGE_SHOWBACKINDEX = true;
+$PAGE_BACK_LINK = '../index.php';
+
+require '../../../header.inc.php';
+?>
+<div class="column">
+<h3>Handset</h3><br>
+<br>
+The Handset library allows Mobile Processing sketches to control the backlight and vibrator of supported mobile phones.
+</div>
+<div class="column" style="padding-left: 40px">
+<b>Handset</b><br>
+This class provides access to the handset methods.<br>
+<br>
+<a href="reference.php?name=Handset">Handset</a><br>
+<a href="reference.php?name=Handset_vibrate">vibrate()</a><br>
+<a href="reference.php?name=Handset_flash">flash()</a><br>
+</div>
+<?php
+require '../../../footer.inc.php';
+?>
