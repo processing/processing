fb0aa9f5febb47ce74d2f1853676c004cf3c48cd
francisli <mail@francisli.com>
2005-11-07 19:10:39 +0000

    Handset library docs

---
+++ web/reference/libraries/handset/reference.php
@@ -0,0 +1,12 @@
+<?php
+
+$PAGE_TITLE = "Handset &raquo; Libraries &raquo; Mobile Processing";
+$PAGE_SHOWBACKINDEX = true;
+
+require '../../../header.inc.php';
+
+$fullpath = dirname(__FILE__);
+require '../../reference.inc.php';
+
+require '../../../footer.inc.php';
+?>
