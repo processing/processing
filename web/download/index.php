bc850b09d73e6252b87b259e3e2a6e25e44e3552
francisli <mail@francisli.com>
2005-11-08 22:36:24 +0000

    Web updates for release

--- web/download/index.php
+++ web/download/index.php
@@ -6,15 +6,24 @@ require '../header.inc.php';
 <img src="images/header.png"><br>
 &nbsp;
 <ol>
-  <li>Download and install the Wireless Toolkit (WTK) from Sun. The latest version is 2.2.<br><br>
-<a href="http://java.sun.com/products/j2mewtoolkit/">http://java.sun.com/products/j2mewtoolkit/</a><br>&nbsp;</li>
+  <li>Download and install a Wireless Toolkit (WTK) for building mobile phone applications.<br><br>
+<b>Windows</b>: Sun WTK 2.2<br>
+<a href="http://java.sun.com/products/j2mewtoolkit/">http://java.sun.com/products/j2mewtoolkit/</a><br><br>
+<b>Mac OS X</b>: Mpowerplayer SDK<br>
+<a href="http://www.mpowerplayer.com/products-sdk.php">http://www.mpowerplayer.com/products-sdk.php</a><br>&nbsp;</li>
   <li>Download and install Mobile Processing.<br><br>
-0092 ALPHA&nbsp;&nbsp|&nbsp;&nbsp;19 09 2005&nbsp;&nbsp;Windows <a href="processing-0092.zip">Standard</a> or <a href="processing-0092-expert.zip">without Java</a><br><br></li>
-  <li>Modify <b>preferences.txt</b> to include the following line, based on your installation location of the WTK:<br>
+0001 ALPHA&nbsp;&nbsp|&nbsp;&nbsp;08 11 2005&nbsp;&nbsp;Windows <a href="mobile-0001.zip">Standard</a> or <a href="mobile-0001-expert.zip">without Java</a>&nbsp;&nbsp;|&nbsp;&nbsp;Mac OS X<br><br>
+  </li>
+  <li>Run Mobile Processing. Choose <b>Preferences</b> from the main drop-down menu. In the <b>Preferences</b> dialog box in the Mobile Processing IDE, go to the <b>Mobile</b> tab, and enter the location of the WTK.<br>
 <br>
-wtk.path=C:\WTK22<br>&nbsp;</li>
+<br>
+<br>
+<br>
+<br>
+Previous releases:<br>
+0000 ALPHA&nbsp;&nbsp|&nbsp;&nbsp;19 09 2005&nbsp;&nbsp;Windows <a href="processing-0092.zip">Standard</a> or <a href="processing-0092-expert.zip">without Java</a><br><br>
+  </li>
 </ol>
-
 <?php
 require '../footer.inc.php';
 ?>
