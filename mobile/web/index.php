096c76144b861770e2d884bb8b0e916358433879
francisli <mail@francisli.com>
2005-11-08 22:36:24 +0000

    Web updates for release

--- mobile/web/index.php
+++ mobile/web/index.php
@@ -12,7 +12,19 @@ require 'header.inc.php';
 <div class="column">
     <br>
     Mobile Processing allows software written using the Processing programming 
-    language and development environment to run on Java Powered mobile devices.
+    language and development environment to run on Java Powered mobile devices.<br>
+    <br>
+    <br>
+    <img src="images/updates.png"><br>
+    <br>
+    <i>November 8</i><br>
+    Mobile Processing 0001 is released.<br>
+    <br>
+    <br>
+    <img src="images/happenings.png"><br>
+    <br>
+    <i>November 7 - 8</i><br>
+Mobile Processing introduction and workshop at Universidad Aut&oacute;noma de Manizales, Colombia by Marlon J. Manrique. <a href="http://mjs.darkgreenmedia.com/">More info</a>
 </div>
 <?php
 require 'footer.inc.php';
