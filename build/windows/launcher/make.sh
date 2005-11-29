c30677a887064026b6df579b625ba90815d64c82
benfry <fry@processing.org>
2005-11-29 22:02:17 +0000

    changes to exe files for launcher and its placement, also notes in faq about export to app being implemented

--- build/windows/launcher/make.sh
+++ build/windows/launcher/make.sh
@@ -8,4 +8,4 @@ cp processing.exe ../work/
 g++ -mwindows -mno-cygwin -O2 -Wall -DEXPORT -c -o launcher.o launcher.cpp
 g++ -mwindows -mno-cygwin -O2 -Wall -mwindows -mno-cygwin -O2 -Wall -o export.exe launcher.o 
 cp export.exe ../work/lib/export/application.exe
-cp export.exe ../../shared/lib/export/
+cp export.exe ../../shared/lib/export/application.exe
