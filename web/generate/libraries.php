9e41a30635713deb9ae9b157e5c5f1482cd8f770
lennyjpg <info@leanderherzog.ch>
2005-12-14 19:14:20 +0000

    adding functioning libraries generation script (also changes to functions.inc.php and others)

--- web/generate/libraries.php
+++ web/generate/libraries.php
@@ -14,23 +14,43 @@ $translation = new Translation($lang);
 // each lib
 $libraries = array('net', 'serial', 'video', 'opengl');
 
+$lib_dir = 'reference/'.($lang != 'en' ? "$lang/" : '').'libraries';
+
+// get library index
+$index = CONTENTDIR."api_$lang/libraries.html";
+$page = new Page('Libraries \\ Processing 1.0 (BETA)', 'Libraries');
+$page->content(file_get_contents($index));
+make_necessary_directories(BASEDIR.$lib_dir.'/images/include.php');
+writeFile($lib_dir.'/index.html', $page->out());
+copydirr(CONTENTDIR."api_$lang/LIB_images", BASEDIR.$lib_dir.'/images');
+
 // foreach lib
 foreach ($libraries as $lib) {
+	$source = "api_$lang/LIB_$lib";
+	$destination = ($lang != 'en' ? "$lang/" : '')."libraries/$lib";
+	make_necessary_directories(REFERENCEDIR.$destination.'/images/include');
 
     // get xml files
-    $directory = "api_$lang/LIB_".$lib;
-    $files = getXMLFiles(CONTENTDIR.$directory);
-    // parse xml files and create pages
-    foreach ($files as $file) {
-        $page = new LibReferencePage(new Ref($directory.'/'.$file), $lib, $translation, $lang);
-        $page->write();
-    }
-    
-    // get index
-    // create page
-    // write page
+    if (!$files = getXMLFiles(CONTENTDIR.$source)) { 
+		echo "couldn't open files"; 
+	} else {
+	// parse xml files and create pages
+	    foreach ($files as $file) {
+	        echo $file.'<br/>';
+	        $page = new LibReferencePage(new Ref($source.'/'.$file), $lib, $translation, $lang);
+	        $page->write();
+	    }
+	}
+
+    // template and copy index
+    $index = CONTENTDIR.$source.'/index.html';
+    $page = new Page(ucfirst($lib) . ' \\ Libraries \\ Processing 1.0 (BETA)', 'Libraries', 'Library-index');
+    $page->content(file_get_contents($index));
+    writeFile('reference/'.$destination.'/index.html', $page->out());
     
     // copy images directory
+	copydirr(CONTENTDIR.$source.'/images', REFERENCEDIR.$destination.'/images');
+
 }
 
 ?>
