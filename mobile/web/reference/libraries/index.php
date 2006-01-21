d82be6350ccee7cb19d9bf359596f26413a2dd17
francisli <mail@francisli.com>
2006-01-21 05:30:00 +0000

    Updated docs to match 0002

--- mobile/web/reference/libraries/index.php
+++ mobile/web/reference/libraries/index.php
@@ -9,20 +9,16 @@ require '../../header.inc.php';
 <img src="images/core.png"><br>
 <br>
 <div class="column">
-<b><a href="net/index.php">Net</a></b><br>
-Sending and receiving data via the Internet.<br>
-<br>
-<br>
 <b><a href="phone/index.php">Phone</a></b><br>
 Controlling phone-specific features.
-</div>
-<div class="column">
-<b><a href="sound/index.php">Sound</a></b><br>
-Sound playback.<br>
 <br>
 <br>
 <b><a href="bluetooth/index.php">Bluetooth</a></b><br>
 Sending and receiving data via Bluetooth wireless connections.
+</div>
+<div class="column">
+<b><a href="sound/index.php">Sound</a></b><br>
+Sound playback.<br>
 </div>
 <div class="column">
 <b><a href="video/index.php">Video</a></b><br>
