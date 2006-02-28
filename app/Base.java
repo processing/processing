6a7c23022355274b47064f0e62aa2e8beb931c7c
benfry <fry@processing.org>
2006-02-28 02:32:53 +0000

    oops, this is 105

--- app/Base.java
+++ app/Base.java
@@ -50,8 +50,8 @@ import processing.core.*;
  * files and images, etc) that comes from that.
  */
 public class Base {
-  static final int VERSION = 104;
-  static final String VERSION_NAME = "0104 Beta";
+  static final int VERSION = 105;
+  static final String VERSION_NAME = "0105 Beta";
 
   /**
    * Path of filename opened on the command line,
