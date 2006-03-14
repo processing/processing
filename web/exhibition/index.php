0c27d2b59d6379382f589e02f78d4fb0a36c87c1
francisli <mail@francisli.com>
2006-03-14 08:36:37 +0000

    First pass, exhibition section of website

---
+++ web/exhibition/index.php
@@ -0,0 +1,58 @@
+<?php
+
+$PAGE_TITLE = "Mobile Processing &raquo; Exhibition";
+require '../header.inc.php';
+?>
+<div class="column2x">
+<img src="images/header.png"><br>
+<br>
+<br>
+<table border="0" cellspacing="0" cellpadding="0">
+  <tr>
+    <td valign="top" width="224">
+<img src="images/rosaly.png"><br>
+<br>
+<table width="200" border="0" cellspacing="0" cellpadding="0">
+  <tr>
+    <td>
+<b><a href="example.php?name=pointslines">Rosaly</a></b><br>
+<br>
+<b>by Oolong</b><br>
+<br>
+0 toggles lines/dots<br>
+9 changes colours<br>
+Use direction arrows for other controls<br>
+<br>
+<a href="http://oolong.co.uk/">Oolong</a>
+    </td>
+  </tr>
+</table>
+    </td>
+    <td valign="top" width="224">
+<img src="images/resonata.png"><br>
+<br>
+<b><a href="example.php?name=pointslines">Resonata</a></b><br>
+<br>
+<table width="200" border="0" cellspacing="0" cellpadding="0">
+  <tr>
+    <td>
+<b>by Oolong</b><br>
+<br>
+This is a spring system, designed to demonstrate many of the universal features of waves.<br>
+<br>
+<a href="http://oolong.co.uk/">Oolong</a>
+    </td>
+  </tr>
+</table>
+    </td>
+  </tr>
+</table>
+</div>
+<div class="column">
+<img src="images/network.png"><br>
+<br>
+<img src="images/addlink.png">
+</div>
+<?php
+ require '../footer.inc.php';
+?>
