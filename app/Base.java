57650049a0f40e096eb42b68f989fe937814b425
benfry <fry@processing.org>
2006-03-15 14:07:42 +0000

    starting rev 0109

--- app/Base.java
+++ app/Base.java
@@ -50,8 +50,8 @@ import processing.core.*;
  * files and images, etc) that comes from that.
  */
 public class Base {
-  static final int VERSION = 108;
-  static final String VERSION_NAME = "0108 Beta";
+  static final int VERSION = 109;
+  static final String VERSION_NAME = "0109 Beta";
 
   /**
    * Path of filename opened on the command line,
