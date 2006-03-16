f3135e70806cb6eef81da1d2a69310d63f4ed165
francisli <mail@francisli.com>
2006-03-16 19:12:30 +0000

    Temporary static exhibition/examples added for now

--- mobile/web/index.php
+++ mobile/web/index.php
@@ -3,25 +3,77 @@ $PAGE_TITLE = "Mobile Processing";
 
 require 'header.inc.php';
 ?>
-<div>
-<img align="left" src="images/mobile_cover.jpg" width="590" height="300">
+<div class="column2x" style="width: 300px; clear: left">
+    <img src="exhibition/images/header.png"><br>
+<br>
+<br>
+<a href="http://www.mimeomai.f2s.com/applet/rosalymob/applet/index.html" target="_new"><img src="exhibition/images/rosaly.png"></a><br>
+<br>
+<table width="200" border="0" cellspacing="0" cellpadding="0">
+  <tr>
+    <td>
+<b><a href="http://www.mimeomai.f2s.com/applet/rosalymob/applet/index.html" target="_new">Rosaly</a></b>
+    </td>
+  </tr>
+  <tr height="5">
+  </tr>
+  <tr>
+    <td>
+<b>by Oolong</b><br>
+    </td>
+  </tr>
+</table><br>
+<br>
+<a href="http://www.mimeomai.f2s.com/applet/resmob/applet/index.html" target="_new"><img src="exhibition/images/resonata.png"></a><br>
+<br>
+<table width="200" border="0" cellspacing="0" cellpadding="0">
+  <tr>
+    <td>
+<b><a href="http://www.mimeomai.f2s.com/applet/resmob/applet/index.html" target="_new">Resonata</a></b>
+    </td>
+  </tr>
+  <tr height="5">
+  </tr>
+  <tr>
+    <td>
+<b>by Oolong</b><br>
+    </td>
+  </tr>
+</table><br>
+<br>
+<br>
+<br>
+<a href="examples/"><img src="images/examples.png"></a><br>
+<br>
+<br>
+<table border="0" cellspacing="0" cellpadding="0">
+  <tr>
+    <td>
+<a href="learning/example.php?name=yahoosonar"><img src="images/examples/yahoosonar.png"></a>
+    </td>
+    <td width="1"></td>
+    <td>
+<a href="learning/example.php?name=bluescan"><img src="images/examples/bluescan.png"></a>
+    </td>
+  </tr>
+</table>
 </div>
-<div class="column" style="clear: left">
-&nbsp;
-</div>
-<div class="column">
+<div class="column" style="width: 400px">
     <br>
-    Mobile Processing allows software written using the Processing programming 
-    language and development environment to run on Java Powered mobile devices.<br>
+    Mobile Processing is an open source programming environment for people who want to design and prototype software for mobile phones. It is based on and shares the same design goals as the open source <a href="http://processing.org/">Processing</a> project. Sketches programmed using Mobile Processing run on Java Powered mobile devices.<br>
+    <br>
     <br>
     <br>
     <img src="images/updates.png"><br>
+    <br>
     <br>
     <i>January 29</i><br>
     Mobile Processing 0003 is released.<br>
     <br>
     <br>
+    <br>
     <img src="images/happenings.png"><br>
+    <br>
     <br>
     <i>March 18</i><br>
 Mobile Processing Workshop II, 8 Hours of Mobility at Universidad Autonoma
