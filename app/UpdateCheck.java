641833e96ba0202c7703a5bb4670d783a29d0b6d
francisli <mail@francisli.com>
2006-05-08 08:22:15 +0000

    Auto-update check and download implemented

--- app/UpdateCheck.java
+++ app/UpdateCheck.java
@@ -54,17 +54,79 @@ import processing.core.*;
  * people are using Processing, which helps us when writing grant
  * proposals and that kind of thing so that we can keep Processing free.
  */
-public class UpdateCheck implements Runnable {
+public class UpdateCheck extends JDialog implements ActionListener, Runnable {
   Editor editor;
-  String downloadURL = "http://mobile.processing.org/download/latest.txt";
+  public static final String downloadURL = "http://mobile.processing.org/download/latest.txt";
+  
+  public static final String libURL = "http://mobile.processing.org/download/libraries/";
+  public static final String libVersion = "version.properties";
+  String platform;
+  boolean cancelled = false;
+  boolean outOfDate = false;  
+  JLabel label;
+  JButton action;
 
   static final long ONE_DAY = 24 * 60 * 60 * 1000;
 
-
   public UpdateCheck(Editor editor) {
+    super(editor);
     this.editor = editor;
+    if (Base.isWindows()) {
+        platform = "windows/";
+    } else if (Base.isMacOS()) {
+        platform = "macosx/";
+    }
     Thread thread = new Thread(this);
     thread.start();
+  }
+  
+  public boolean isOutOfDate() {
+      return outOfDate;
+  }
+  
+  public void show() {
+      setTitle("Checking for updates...");
+      setModal(true);
+      
+      JPanel panel;
+      label = new JLabel("Connecting to server...");
+      action = new JButton("Cancel");
+      action.addActionListener(this);
+      
+      Container content = getContentPane();      
+      panel = new JPanel();
+      panel.add(label);
+      content.add(panel, BorderLayout.CENTER);      
+      panel = new JPanel(new FlowLayout(FlowLayout.CENTER));
+      panel.add(action);
+      content.add(panel, BorderLayout.SOUTH);      
+      pack();
+      
+      Rectangle bounds = editor.getBounds();
+      setLocation(bounds.x + ((bounds.width - 400) >> 1), bounds.y + ((bounds.height - 100) >> 1));
+      setSize(400, 100);
+      setResizable(false);
+      
+      setDefaultCloseOperation(WindowConstants.DO_NOTHING_ON_CLOSE);
+      addWindowListener(new WindowAdapter() {
+          public void windowClosing(WindowEvent we) {
+              actionPerformed(null);
+          }
+      });
+      
+      super.show();
+  }
+  
+  public void actionPerformed(ActionEvent ae) {
+      cancelled = true;
+      dispose();
+  }
+  
+  public void setMessage(String message) {
+      if (label != null) {
+          label.setText(message);
+          repaint();
+      }
   }
 
 
@@ -92,7 +154,11 @@ public class UpdateCheck implements Runnable {
                         System.getProperty("os.arch"));
 
     try {
+      setMessage("Checking latest version of PDE...");
       int latest = readInt(downloadURL + "?" + info);
+      if (cancelled) {
+          return;
+      }
 
       String lastString = Preferences.get("update.last");
       long now = System.currentTimeMillis();
@@ -110,6 +176,7 @@ public class UpdateCheck implements Runnable {
         "would you like to visit the Mobile Processing download page?";
 
       if (latest > Base.VERSION) {
+        outOfDate = true;
         Object[] options = { "Yes", "No" };
         int result = JOptionPane.showOptionDialog(editor,
                                                   prompt,
@@ -122,16 +189,172 @@ public class UpdateCheck implements Runnable {
 
         if (result == JOptionPane.YES_OPTION) {
           Base.openURL("http://mobile.processing.org/download/");
-
-        //} else if (result == JOptionPane.NO_OPTION) {
+          dispose();
+          return;
         }
       }
+      if (cancelled) {
+          return;
+      }
+      
+      //// now do library update check
+      setMessage("Checking latest version of libraries...");
+      HashMap versions = new HashMap();
+      readLibraryVersions(versions);
+      if (cancelled) {
+          return;
+      }
+      //// reconcile versions
+      compareLibraryVersions(versions);
+      if (cancelled) {
+          return;
+      }
+      //// download any updates
+      downloadLibraries(versions);
+      
+      if (action != null) {
+        action.setText("OK");
+      }
+      if (!outOfDate) {
+        setMessage("No updates found.");
+      } else {
+        setMessage("Update check complete. Please restart Mobile Processing.");
+      }
     } catch (Exception e) {
-      //e.printStackTrace();
-      //System.err.println("Error while trying to check for an update.");
+      if (action != null) {
+        action.setText("OK");
+      }
+      setMessage("An error occurred while checking for updates.  Please try again later.");
+      e.printStackTrace();
     }
   }
-
+  
+  protected void readLibraryVersions(HashMap versions) throws Exception {
+      URL url = new URL(libURL + platform + libVersion);
+      BufferedReader reader = new BufferedReader(new InputStreamReader(url.openStream()));
+      String line = reader.readLine();
+      String lib, version;
+      int pos;
+      while (line != null) {
+          pos = line.indexOf('=');
+          if (!line.startsWith("#") && (pos > 0)) {
+              lib = line.substring(0, pos).trim();
+              version = line.substring(pos + 1).trim();
+              versions.put(lib, version);
+          }
+          line = reader.readLine();
+      }
+      reader.close();
+  }
+  
+  protected void compareLibraryVersions(HashMap versions) throws Exception {
+      Iterator keys = versions.keySet().iterator();
+      String lib;
+      int localVersion, serverVersion;
+      while (keys.hasNext()) {
+          lib = (String) keys.next();
+          serverVersion = Integer.parseInt((String) versions.get(lib));
+          //// open and parse local library version
+          File f = new File(Sketchbook.librariesFolder, lib + File.separator + libVersion);
+          if (f.exists()) {
+              BufferedReader reader = new BufferedReader(new FileReader(f));
+              //// assume that first line MUST be build version
+              String version = reader.readLine();
+              reader.close();
+              if (version != null) {
+                  localVersion = Integer.parseInt(version.substring(version.indexOf('=') + 1).trim());
+              } else {
+                  localVersion = 0;
+              }
+              if (localVersion < serverVersion) {
+                  //// mark for download
+                  versions.put(lib, new Boolean(true));                  
+                  outOfDate = true;
+              } else {
+                  versions.put(lib, new Boolean(false));
+              }
+          } else {
+              //// mark for download
+              versions.put(lib, new Boolean(true));
+              outOfDate = true;
+          }
+      }
+  }
+  
+  protected void downloadLibraries(HashMap versions) throws Exception {
+      Iterator keys = versions.keySet().iterator();
+      String lib;
+      boolean updated;
+      while (keys.hasNext()) {
+          lib = (String) keys.next();
+          updated = ((Boolean) versions.get(lib)).booleanValue();
+          if (updated) {
+              setMessage("Downloading new " + lib + " library...");
+              if (cancelled) {
+                  return;
+              }
+              File f = new File(Sketchbook.librariesFolder, lib + ".zip");
+              //// check if any previous download exists, and delete if so
+              if (f.exists()) {
+                  f.delete();
+              }
+              URL url = new URL(libURL + platform + lib + ".zip");
+              writeStreamToFile(url.openStream(), f);
+              
+              //// rename old library folder as a backup
+              File folder = new File(Sketchbook.librariesFolder, lib);
+              File backup = new File(Sketchbook.librariesFolder, lib + "_bak");
+              if (folder.exists()) { 
+                  if (!folder.renameTo(backup)) {
+                      throw new Exception("Directory rename failed.");
+                  }
+              }
+              //// create library folder
+              if (!folder.mkdir()) {
+                  throw new Exception("Directory create failed.");
+              }
+              setMessage("Extracting " + lib + " library...");
+              if (cancelled) {
+                  return;
+              }
+              //// extract files into folder
+              ZipFile zip = new ZipFile(f);
+              Enumeration e = zip.entries();
+              ZipEntry ze;
+              while (e.hasMoreElements()) {
+                  ze = (ZipEntry) e.nextElement();
+                  File zf = new File(Sketchbook.librariesFolder, lib + File.separator + ze.getName());
+                  if (ze.isDirectory()) {
+                      zf.mkdirs();
+                  } else {
+                      writeStreamToFile(zip.getInputStream(ze), zf);
+                  }
+                  if (cancelled) {
+                      return;
+                  }
+              }
+              //// delete zip file
+              f.delete();
+              //// delete backup
+              if (backup.exists()) {
+                  Base.removeDir(backup);
+              }
+          }
+      }      
+  }
+  
+  protected static void writeStreamToFile(InputStream is, File f) throws Exception {
+      BufferedInputStream bis = new BufferedInputStream(is);
+      FileOutputStream os = new FileOutputStream(f);
+      byte[] buffer = new byte[4096];
+      int bytesRead = bis.read(buffer);
+      while (bytesRead >= 0) {
+          os.write(buffer, 0, bytesRead);
+          bytesRead = bis.read(buffer);
+      }
+      os.close();
+      bis.close();
+  }
 
   protected int readInt(String filename) throws Exception {
     URL url = new URL(filename);
