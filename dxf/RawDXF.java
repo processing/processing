3b7cf7d622e6471260242f0a2095da1496b690a9
benfry <fry@processing.org>
2006-05-09 12:01:19 +0000

    remove error with dxf and opengl

--- dxf/RawDXF.java
+++ dxf/RawDXF.java
@@ -315,7 +315,9 @@ public class RawDXF extends PGraphics3 {
   public void beginShape(int kind) {
     shape = kind;
 
-    if ((shape != LINES) && (shape != TRIANGLES)) {
+    if ((shape != LINES) &&
+        (shape != LINE_STRIP) &&
+        (shape != TRIANGLES)) {
       String err =
         "RawDXF should only be used with beginRaw(), " +
         "as it only supports lines and triangles";
