10ecb1a2071dcf991a8c8082a17fe01a26be0f16
francisli <mail@francisli.com>
2006-05-21 20:59:20 +0000

    Updated web download page

--- web/download/index.php
+++ web/download/index.php
@@ -6,15 +6,16 @@ require '../header.inc.php';
 <img src="images/header.png"><br>
 &nbsp;
 <ol>
-  <li>Install the latest version of Java on your computer.<br><br><a href="http://www.java.com/">http://www.java.com/</a><br><br>Choose <b>Download Now</b> to begin or <b>Verify Installation</b> to check if it is already installed on your computer.<br><br></li>
+  <li>Install the latest version of Java on your computer.<br><br>
+<a href="http://java.com/"><img border="0" src="images/java.png"></a><br>
+<a href="http://java.com/">http://java.com/</a><br><br>Choose <b>Download Now</b> to begin or <b>Verify Installation</b> to check if it is already installed on your computer.<br><br></li>
   <li>Download and install a Wireless Toolkit (WTK) for building mobile phone applications.<br><br>
 <b>Windows</b>: Sun WTK 2.2<br>
 <a href="http://java.sun.com/products/j2mewtoolkit/">http://java.sun.com/products/j2mewtoolkit/</a><br><br>
 <b>Mac OS X</b>: Mpowerplayer SDK<br>
 <a href="http://www.mpowerplayer.com/products-sdk.php">http://www.mpowerplayer.com/products-sdk.php</a><br>&nbsp;</li>
   <li>Download and install Mobile Processing.<br><br>
-0005 ALPHA&nbsp;&nbsp|&nbsp;&nbsp;18 05 2006&nbsp;&nbsp;<a href="mobile-0005-expert.zip">Windows</a>&nbsp;&nbsp;|&nbsp;&nbsp;Mac OS X (coming soon!)<br>
-0004 ALPHA&nbsp;&nbsp|&nbsp;&nbsp;10 05 2006&nbsp;&nbsp;<a href="mobile-0004.dmg">Mac OS X</a><br><br>
+0005 ALPHA&nbsp;&nbsp|&nbsp;&nbsp;18 05 2006&nbsp;&nbsp;<a href="mobile-0005-expert.zip">Windows</a>&nbsp;&nbsp;|&nbsp;&nbsp;<a href="mobile-0005.dmg">Mac OS X</a><br><br>
   </li>
   <li>Run Mobile Processing. Choose <b>Preferences</b> from the main drop-down menu. In the <b>Preferences</b> dialog box in the Mobile Processing IDE, go to the <b>Mobile</b> tab, and enter the location of the WTK.<br>
 <br>
