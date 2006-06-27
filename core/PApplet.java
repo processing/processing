4d68f1027e47e0b7dc7cc29121f1fe2437fe56ba
benfry <fry@processing.org>
2006-06-27 00:38:25 +0000

    adding notes about saveXxxx() methods

--- core/PApplet.java
+++ core/PApplet.java
@@ -4053,6 +4053,15 @@ public class PApplet extends Applet
    * Returns a path inside the applet folder to save to,
    * just like sketchPath(), but also creates any in-between
    * folders so that things save properly.
+   * <p/>
+   * All saveXxxx() functions use the path to the sketch folder, rather than
+   * its data folder. Once exported, the data folder will be found inside the
+   * jar file of the exported application or applet. In this case, it's not
+   * possible to save data into the jar file, because it will often be running
+   * from a server, or marked in-use if running from a local file system.
+   * With this in mind, saving to the data path doesn't make sense anyway.
+   * If you know you're running locally, and want to save to the data folder,
+   * use <TT>saveXxxx("data/blah.dat")</TT>.
    */
   public String savePath(String where) {
     String filename = sketchPath(where);
