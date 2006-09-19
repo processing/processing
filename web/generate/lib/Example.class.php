1955da1662da2ca6b0915e85c93e087f5d8631b1
REAS <reas@processing.org>
2006-09-19 23:20:00 +0000

    Mod for new comments style

--- web/generate/lib/Example.class.php
+++ web/generate/lib/Example.class.php
@@ -29,8 +29,9 @@ class Example
 			#	$doc = false;
 			#}
 			# Change for new comment style - cr
-			if (!preg_match("/^\ \*\//", $line) && doc) {
-			  $doc = false;  # End the documentation
+			if (!preg_match("/\*\//", $line) && doc) {
+			  $doc = false;  # End the documentation<br>
+              echo "match found";
 			}
 			if ($doc) {
 				#$doc_lines[] = htmlspecialchars(str_replace('// ', '', $line));<br>
