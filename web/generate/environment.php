c19ace537fa6e541096b43ea0499ca7b0641c348
REAS <reas@processing.org>
2006-09-23 02:53:30 +0000

    New environment pages

--- web/generate/environment.php
+++ web/generate/environment.php
@@ -7,6 +7,9 @@ $benchmark_start = microtime_float();
 // arguments
 $lang = isSet($_POST['lang']) ? $_POST['lang'] : 'en';
 
+// get translation file
+$translation = new Translation($lang);
+
 // make troubleshooting page
 $source = CONTENTDIR."/api_$lang/troubleshooting/";
 $path = REFERENCEDIR . ($lang == 'en' ? '' : "/$lang") . "/troubleshooting/";
@@ -17,9 +20,7 @@ $page->language($lang);
 writeFile('reference/'.($lang=='en'?'':"$lang/").'troubleshooting/index.html', $page->out());
 copydirr($source.'/images', $path.'/images');
 
-// get translation file
-$translation = new Translation($lang);
-
+// make overview page
 $source = CONTENTDIR."/api_$lang/environment/";
 $path = REFERENCEDIR . ($lang == 'en' ? '' : "/$lang") . "/environment/";
 make_necessary_directories($path."images/file");
@@ -28,6 +29,19 @@ $page->content(file_get_contents($source."index.html"));
 $page->language($lang);
 writeFile('reference/'.($lang=='en'?'':"$lang/").'environment/index.html', $page->out());
 copydirr($source.'/images', $path.'/images');
+
+// make export page
+$page = new Page("Environment (IDE)", "Environment", "Environment");
+$page->content(file_get_contents($source."export.html"));
+$page->language($lang);
+writeFile('reference/'.($lang=='en'?'':"$lang/").'environment/export.html', $page->out());
+
+// make platforms page
+$page = new Page("Environment (IDE)", "Environment", "Environment");
+$page->content(file_get_contents($source."platforms.html"));
+$page->language($lang);
+writeFile('reference/'.($lang=='en'?'':"$lang/").'platforms/export.html', $page->out());
+
 
 $benchmark_end = microtime_float();
 $execution_time = round($benchmark_end - $benchmark_start, 4);
