93f00d009c921db6ea127e32f72f4e1a70e64af8
REAS <reas@processing.org>
2006-09-23 03:36:43 +0000

    Making the new FAQ generator

---
+++ web/generate/staticpages.php
@@ -0,0 +1,24 @@
+<?
+
+require('../config.php');
+$benchmark_start = microtime_float();
+
+
+// make troubleshooting page
+$source = CONTENTDIR."/static/";
+$path = BASEDIR;
+
+$page = new Page("FAQ", "FAQ", "FAQ");
+$page->content(file_get_contents($source."faq.html"));
+$page->language($lang);
+writeFile($path.'/faq.html', $page->out());
+#copydirr($source.'/images', $path.'/images');
+
+
+$benchmark_end = microtime_float();
+$execution_time = round($benchmark_end - $benchmark_start, 4);
+
+?>
+
+<h2>Environment page generation Successful</h2>
+<p>Generated files in <?=$execution_time?> seconds.</p>
