c62ec35981d1a94bbbba9c0485e8706c1483b4fb
REAS <reas@processing.org>
2006-09-23 00:26:07 +0000

    Add generation for Troubleshooting page

--- web/generate/environment_local.php
+++ web/generate/environment_local.php
@@ -7,13 +7,22 @@ $benchmark_start = microtime_float();
 // arguments
 $lang = isSet($_POST['lang']) ? $_POST['lang'] : 'en';
 
+// make troubleshooting file
+$source = CONTENTDIR."/api_$lang/troubleshooting/";
+$path = DISTDIR."/troubleshooting/";<br>
+make_necessary_directories($path."images/file");
+$page = new LocalPage('Troubleshooting \\ Processing 1.0 (BETA)', 'Troubleshooting', 'Troubleshooting', '../');
+$page->content(file_get_contents($source."index.html"));
+$page->language($lang);
+writeFile('distribution/troubleshooting/index.html', $page->out());
+copydirr($source.'/images', $path.'/images');
+
 // get translation file
 $translation = new Translation($lang);
 
 $source = CONTENTDIR."/api_$lang/environment/";
 $path = DISTDIR."/environment/";
 make_necessary_directories($path."images/file");
-
 $page = new LocalPage("Environment (IDE)", "Environment", "Environment", '../');
 $page->content(file_get_contents($source."index.html"));
 $page->language($lang);
