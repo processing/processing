6833cd4f1d81dc21ddd9551601e446246e14cea4
francisli <mail@francisli.com>
2006-10-05 15:53:36 +0000

    Exhibition updates

--- web/exhibition/index.php
+++ web/exhibition/index.php
@@ -10,6 +10,52 @@ require '../header.inc.php';
 <table border="0" cellspacing="0" cellpadding="0">
   <tr>
     <td valign="top" width="224">
+<a href="http://todbot.com/blog/2006/09/12/roombactrl-drive-your-roomba-with-your-cell-phone/" target="_new"><img src="images/roombactrl.png"></a><br>
+<br>
+<table width="200" border="0" cellspacing="0" cellpadding="0">
+  <tr>
+    <td>
+<b><a href="http://todbot.com/blog/2006/09/12/roombactrl-drive-your-roomba-with-your-cell-phone/" target="_new">RoombaCtrl</a></b>
+    </td>
+  </tr>
+  <tr height="5">
+  </tr>
+  <tr>
+    <td>
+<b>by todbot</b><br />
+<br />
+RoombaCtrl works with the build-your-own Bluetooth adapter shown in the book "Hacking Roomba" or the pre-built RooTooth so you can drive your Roomba with your cellphone.
+    </td>
+  </tr>
+</table>
+    </td>
+    <td valign="top" width="224">
+<a href="http://i.document.m05.de/?p=189" target="_new"><img src="images/pixiebubble.png"></a><br>
+<br>
+<table width="200" border="0" cellspacing="0" cellpadding="0">
+  <tr>
+    <td>
+<b><a href="http://i.document.m05.de/?p=189" target="_new">pixiebubble</a></b>
+    </td>
+  </tr>
+  <tr height="5">
+  </tr>
+  <tr>
+    <td>
+<b>by michael</b><br />
+<br />
+pixiebubble is an application for mobile phones where images are processed into sound sequences. pixels of a photo shot with the phones camera are translated into bubble sounds.<br />
+<br />
+    </td>
+  </tr>
+</table>
+    </td>
+  </tr>
+  <tr>
+    <td colspan="2">&nbsp;</td>
+  </tr>
+  <tr>
+    <td valign="top" width="224">
 <a href="mandelbrotMobile/applet/index.html" target="_new"><img src="images/mandelbrotMobile.png"></a><br>
 <br>
 <table width="200" border="0" cellspacing="0" cellpadding="0">
@@ -46,7 +92,8 @@ require '../header.inc.php';
 9 changes colours<br>
 Use direction arrows for other controls<br>
 <br>
-<a href="http://oolong.co.uk/">Oolong</a>
+<a href="http://oolong.co.uk/">Oolong</a><br />
+<br />
     </td>
   </tr>
 </table>
