1d5753900bf0621fdf1243570c5e488738323744
benfry <fry@processing.org>
2006-11-06 20:42:32 +0000

    starting release 0122

--- app/Base.java
+++ app/Base.java
@@ -50,8 +50,8 @@ import processing.core.*;
  * files and images, etc) that comes from that.
  */
 public class Base {
-  static final int VERSION = 121;
-  static final String VERSION_NAME = "0121 Beta";
+  static final int VERSION = 122;
+  static final String VERSION_NAME = "0122 Beta";
 
   /**
    * Path of filename opened on the command line,
