2e7de6434bd7756bcb20c15cc6bf1044c630ab0b
benfry <fry@processing.org>
2006-11-11 14:52:57 +0000

    cleaning up deprecated methods, altering javadoc, clarifying copyright

--- xml/src/processing/xml/XMLParseException.java
+++ xml/src/processing/xml/XMLParseException.java
@@ -1,31 +1,30 @@
-/* XMLParseException.java
- *
- * $Revision: 1.4 $
- * $Date: 2002/03/24 10:27:59 $
- * $Name: RELEASE_2_2_1 $
- *
- * This file is part of NanoXML 2 Lite.
- * Copyright (C) 2000-2002 Marc De Scheemaecker, All Rights Reserved.
- *
- * This software is provided 'as-is', without any express or implied warranty.
- * In no event will the authors be held liable for any damages arising from the
- * use of this software.
- *
- * Permission is granted to anyone to use this software for any purpose,
- * including commercial applications, and to alter it and redistribute it
- * freely, subject to the following restrictions:
- *
- *  1. The origin of this software must not be misrepresented; you must not
- *     claim that you wrote the original software. If you use this software in
- *     a product, an acknowledgment in the product documentation would be
- *     appreciated but is not required.
- *
- *  2. Altered source versions must be plainly marked as such, and must not be
- *     misrepresented as being the original software.
- *
- *  3. This notice may not be removed or altered from any source distribution.
- *****************************************************************************/
+/*
+  This code is from NanoXML 2.2.3 Lite
+  Copyright (C) 2000-2002 Marc De Scheemaecker, All Rights Reserved.
 
+  Additional modifications Copyright (c) 2006 Ben Fry and Casey Reas 
+  to make the code better-suited for use with Processing.
+
+  Original license notice from Marc De Scheemaecker:
+  
+  This software is provided 'as-is', without any express or implied warranty.
+  In no event will the authors be held liable for any damages arising from the
+  use of this software.
+ 
+  Permission is granted to anyone to use this software for any purpose,
+  including commercial applications, and to alter it and redistribute it
+  freely, subject to the following restrictions:
+ 
+  1. The origin of this software must not be misrepresented; you must not
+     claim that you wrote the original software. If you use this software in
+     a product, an acknowledgment in the product documentation would be
+     appreciated but is not required.
+ 
+  2. Altered source versions must be plainly marked as such, and must not be
+     misrepresented as being the original software.
+ 
+  3. This notice may not be removed or altered from any source distribution.
+*/
 
 package processing.xml;
 
@@ -33,14 +32,10 @@ package processing.xml;
 /**
  * An XMLParseException is thrown when an error occures while parsing an XML
  * string.
- * <P>
- * $Revision: 1.4 $<BR>
- * $Date: 2002/03/24 10:27:59 $<P>
  *
- * @see nanoxml.XMLElement
+ * @see processing.xml.XMLElement
  *
  * @author Marc De Scheemaecker
- * @version $Name: RELEASE_2_2_1 $, $Revision: 1.4 $
  */
 public class XMLParseException
     extends RuntimeException
@@ -57,10 +52,10 @@ public class XMLParseException
      * <code>NO_LINE</code> if the line number is unknown.
      *
      * <dl><dt><b>Invariants:</b></dt><dd>
-     * <ul><li><code>lineNr &gt 0 || lineNr == NO_LINE</code>
+     * <ul><li><code>lineNumber &gt 0 || lineNumber == NO_LINE</code>
      * </ul></dd></dl>
      */
-    private int lineNr;
+    private int lineNumber;
 
 
     /**
@@ -74,7 +69,7 @@ public class XMLParseException
      * </ul></dd></dl>
      *
      * <dl><dt><b>Postconditions:</b></dt><dd>
-     * <ul><li>getLineNr() => NO_LINE
+     * <ul><li>getLineNumber() => NO_LINE
      * </ul></dd></dl><dl>
      */
     public XMLParseException(String name,
@@ -84,7 +79,7 @@ public class XMLParseException
               + ((name == null) ? "the XML definition"
                                 : ("a " + name + " element"))
               + ": " + message);
-        this.lineNr = XMLParseException.NO_LINE;
+        this.lineNumber = XMLParseException.NO_LINE;
     }
 
 
@@ -92,27 +87,27 @@ public class XMLParseException
      * Creates an exception.
      *
      * @param name    The name of the element where the error is located.
-     * @param lineNr  The number of the line in the input.
+     * @param lineNumber  The number of the line in the input.
      * @param message A message describing what went wrong.
      *
      * </dl><dl><dt><b>Preconditions:</b></dt><dd>
      * <ul><li><code>message != null</code>
-     *     <li><code>lineNr &gt; 0</code>
+     *     <li><code>lineNumber &gt; 0</code>
      * </ul></dd></dl>
      *
      * <dl><dt><b>Postconditions:</b></dt><dd>
-     * <ul><li>getLineNr() => lineNr
+     * <ul><li>getLineNumber() => lineNumber
      * </ul></dd></dl><dl>
      */
     public XMLParseException(String name,
-                             int    lineNr,
+                             int    lineNumber,
                              String message)
     {
         super("XML Parse Exception during parsing of "
               + ((name == null) ? "the XML definition"
                                 : ("a " + name + " element"))
-              + " at line " + lineNr + ": " + message);
-        this.lineNr = lineNr;
+              + " at line " + lineNumber + ": " + message);
+        this.lineNumber = lineNumber;
     }
 
 
@@ -120,11 +115,11 @@ public class XMLParseException
      * Where the error occurred, or <code>NO_LINE</code> if the line number is
      * unknown.
      *
-     * @see nanoxml.XMLParseException#NO_LINE
+     * @see processing.xml.XMLParseException#NO_LINE
      */
-    public int getLineNr()
+    public int getLineNumber()
     {
-        return this.lineNr;
+        return this.lineNumber;
     }
 
 }
