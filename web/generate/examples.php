0936637b6704fe0f5bf9a195696c217a1fecfc16
REAS <reas@processing.org>
2006-11-25 23:20:34 +0000

    Upper case mistake

--- web/generate/examples.php
+++ web/generate/examples.php
@@ -15,7 +15,8 @@ foreach ($categories as $cat => $array) {
 	if ($dp = opendir($dir.$cat)) {
 		while ($fp = readdir($dp)) {
 			if (substr($fp, 0, 1) != '.') {
-				$ex = new Example($fp, $subdir.'/'.$cat);
+				#$ex = new Example($fp, $subdir.'/'.$cat);
+				$ex = new Example($fp, $cat);
 				$ex->output_file($categories);
 				$count++;
 			}
