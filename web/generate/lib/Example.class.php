df13d402452f7e757d018a55f1e2b37cbfadc03b
REAS <reas@processing.org>
2006-11-26 01:32:31 +0000

    Changes to Examples class

--- web/generate/lib/Example.class.php
+++ web/generate/lib/Example.class.php
@@ -15,11 +15,12 @@ class Example
 		$this->name = $name;
 		$this->cat = $cat;
 		$this->sub = $sub;
+		
 		#$this->file = file_get_contents(CONTENTDIR.'examples/'.$cat.'/'.$name.'/'.$name.'.pde');
 		#$this->applet = CONTENTDIR.'examples/'.$cat.'/'.$name.'/applet/'.$name.'.jar';
 		$this->file = file_get_contents(CONTENTDIR.'examples/'.$cat.'/'.$name.'/'.$name.'.pde');
 		$this->applet = CONTENTDIR.'examples/'.$cat.'/'.$name.'/applet/'.$name.'.jar';
-		echo CONTENTDIR.'examples/'.$cat.'/'.$name.'/'.$name.'.pde';
+		#echo CONTENTDIR.'examples/'.$cat.'/'.$name.'/'.$name.'.pde';
 		
 		$this->split_file();
 	}
@@ -87,7 +88,7 @@ class Example
 		$page->subtemplate('template.example.html');
 		$page->content($this->display());
 		$page->set('examples_nav', $this->make_nav($menu_array));
-		writeFile("learning/examples/".strtolower($this->sub)."/".strtolower($this->name).".html", $page->out());
+		writeFile("learning/".strtolower($this->sub)."/".strtolower($this->name).".html", $page->out());
 		$this->copy_media();
 		#echo "learning/examples/".strtolower($this->sub)."/".strtolower($this->name).".html\n";
 	}
