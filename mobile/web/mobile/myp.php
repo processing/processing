72708bbbbfcea02b748083a5171d49dbc318253d
francisli <mail@francisli.com>
2006-12-04 17:02:01 +0000

    First successful test of downloading with params

--- mobile/web/mobile/myp.php
+++ mobile/web/mobile/myp.php
@@ -11,10 +11,10 @@ require_once 'header.inc.php';
 <b>Your browser is:</b><br />
 <?php echo $_SERVER['HTTP_USER_AGENT'] ?><br />
 <br />
-To test your phone, choose a link below to download a <b>Profiler</b> application. If you have a MIDP 2.0 phone, choose <b>Advanced</b>. If you don't know, are unsure, or have a MIDP 1.0 phone, choose <b>Basic</b>.<br />
+To test your phone, choose a link below to download a <b>Profiler</b> application. If you have a MIDP 2.0 phone, choose <b>Advanced.</b>  If you don't know, are unsure, or have a MIDP 1.0 phone, choose <b>Basic.</b><br />
 <br />
-<span class="accesskey">1</span> <a href="dl.php?n=pbasic" accesskey="1">Profiler Basic</a><br />
-<span class="accesskey">2</span> <a href="dl.php?n=padvanced" accesskey="2">Profiler Advanced</a><br />
+<span class="accesskey">1</span> <a href="pbasic/proguard/pbasic.jad" accesskey="1">Profiler Basic</a><br />
+<span class="accesskey">2</span> <a href="padv/proguard/padv.jad" accesskey="2">Profiler Advanced</a><br />
 <?php
 require_once 'footer.inc.php';
 ?>
