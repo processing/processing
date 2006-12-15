cf056e6be396936d63ab87f1a044921fb78c5295
francisli <mail@francisli.com>
2006-12-15 17:40:48 +0000

    Moving wap folder

--- wap/dl.php
+++ wap/dl.php
@@ -3,7 +3,7 @@
 //// get device profile, if any
 
 //// read jad file into array
-$lines = file($_GET['n'] .'.jad');
+$lines = file($_GET['n'] .'/proguard/'. $_GET['n'] .'.jad');
 
 //// compare profile and configuration compatibility
 $compatible = true;
@@ -15,8 +15,11 @@ if ($compatible || isset($_GET['f'])) {
     //// set content type for jad file
     header("Content-Type: text/vnd.sun.j2me.app-descriptor");
 
-    //// write out jad with additional params
+    //// write out jad with additional params, full url to jar file
     foreach ($lines as $l) {
+        if (stristr($l, "MIDlet-Jar-URL:") !== false) {        
+            $l = 'MIDlet-JAR-URL: http://'. $_SERVER['SERVER_NAME'] . substr($_SERVER['PHP_SELF'], 0, strrpos($_SERVER['PHP_SELF'], "/") + 1) . $_GET['n'] .'/proguard/'. ltrim(substr(strrchr($l, ":"), 1));
+        }
         echo $l;
     }
     echo "MP-Id: {$id}\n";
