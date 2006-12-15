6155125de764ac2d23b618d49f47b998a68415f5
francisli <mail@francisli.com>
2006-12-15 18:43:15 +0000

    Mods to download special handling

--- mobile/wap/dl.php
+++ mobile/wap/dl.php
@@ -3,7 +3,7 @@
 //// get device profile, if any
 
 //// read jad file into array
-$lines = file($_GET['n'] .'/proguard/'. $_GET['n'] .'.jad');
+$lines = file($_GET['n'] .'.jad');
 
 //// compare profile and configuration compatibility
 $compatible = true;
@@ -18,7 +18,7 @@ if ($compatible || isset($_GET['f'])) {
     //// write out jad with additional params, full url to jar file
     foreach ($lines as $l) {
         if (stristr($l, "MIDlet-Jar-URL:") !== false) {        
-            $l = 'MIDlet-JAR-URL: http://'. $_SERVER['SERVER_NAME'] . substr($_SERVER['PHP_SELF'], 0, strrpos($_SERVER['PHP_SELF'], "/") + 1) . $_GET['n'] .'/proguard/'. ltrim(substr(strrchr($l, ":"), 1));
+            $l = 'MIDlet-Jar-URL: http://'. $_SERVER['SERVER_NAME'] .'/'. $_GET['n'] .".jar\n";
         }
         echo $l;
     }
--- mobile/wap/myp.php
+++ mobile/wap/myp.php
@@ -13,8 +13,8 @@ require_once 'header.inc.php';
 <br />
 To test your phone, choose a link below to download a <b>Profiler</b> application. If you have a MIDP 2.0 phone, choose <b>Advanced</b>. If you don't know, are unsure, or have a MIDP 1.0 phone, choose <b>Basic</b>.<br />
 <br />
-<span class="accesskey">1</span> <a href="dl.php?n=pbasic" accesskey="1">Profiler Basic</a><br />
-<span class="accesskey">2</span> <a href="dl.php?n=padvanced" accesskey="2">Profiler Advanced</a><br />
+<span class="accesskey">1</span> <a href="pbasic/proguard/pbasic.jad" accesskey="1">Profiler Basic</a><br />
+<span class="accesskey">2</span> <a href="" accesskey="2">Profiler Advanced</a><br />
 <?php
 require_once 'footer.inc.php';
 ?>
