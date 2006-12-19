a4998fcde4e7f0dd9f3e0141f23c5fbef82c51d2
francisli <mail@francisli.com>
2006-12-19 07:26:51 +0000

    Fetch example

---
+++ mobile/web/learning/examples/fetch/midlet/fetch.java
@@ -0,0 +1,55 @@
+import processing.core.*; public class fetch extends PMIDlet{// Fetch
+// by Francis Li <http://www.francisli.com>
+//
+// This example demonstrates the basic usage of the PClient class for
+// network communication.  It uses the GET() method to fetch an image
+// from the Mobile Processing website and then displays it on the
+// screen.
+//
+final int STATE_START       = 0;
+final int STATE_DONE        = 1;
+final int STATE_ERROR       = 2;
+
+PClient client;
+PFont font;
+int state;
+PImage img;
+
+public void setup() {
+  client = new PClient("mobile.processing.org");
+  font = loadFont();
+  textFont(font);
+  state = STATE_START;  
+  softkey("GET");
+  noLoop();
+}
+
+public void softkeyPressed(String label) {
+  if (label.equals("GET")) {
+    if (client.GET("/images/mobile.png")) {
+      byte[] data = client.readBytes();
+      img = loadImage(data);      
+      state = STATE_DONE;
+    } else {
+      state = STATE_ERROR;
+    }
+    redraw();
+  }
+}
+
+public void draw() {
+  background(255);
+  switch (state) {
+    case STATE_START:
+      fill(0);
+      text("Press GET to start.", 4, 4, width - 8, height - 8);
+      break;
+    case STATE_DONE:
+      image(img, 4, 4);
+      break;
+    case STATE_ERROR:
+      text("An error has occurred.", 4, 4, width - 8, height - 8);
+      break;
+  }
+}
+}
