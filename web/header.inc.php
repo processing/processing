c3505b062ba0eb4c9899875631e5f89ecadce00d
francisli <mail@francisli.com>
2007-01-14 19:22:33 +0000

    Adding support for favicon.ico

--- web/header.inc.php
+++ web/header.inc.php
@@ -42,6 +42,7 @@ if (is_null($argv)) {
                       "http://www.w3.org/TR/html4/strict.dtd">
 <html>
 <head>
+<link rel="shortcut icon" href="http://mobile.processing.org/favicon.ico" type="image/x-icon" />
 <title><?php echo $PAGE_TITLE ?></title>
 <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
 <script language="javascript">
