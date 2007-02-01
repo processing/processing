a5599d6ec1b4d300ce6e347ddaed45439e37ee3e
francisli <mail@francisli.com>
2007-02-01 04:14:44 +0000

    Placeholder exhibition and learning links

---
+++ mobile/wap/exh.php
@@ -0,0 +1,14 @@
+<?php
+
+$PAGE_TITLE = "Exhibition";
+$PAGE_HEADER = <<<EOF
+<a href="index.php">Cover</a> \ Exhibition
+EOF;
+$PAGE_FOOTER = $PAGE_HEADER;
+
+require_once 'header.inc.php';
+?>
+Coming soon.
+<?php
+require_once 'footer.inc.php';
+?>
---
+++ mobile/wap/lrn.php
@@ -0,0 +1,14 @@
+<?php
+
+$PAGE_TITLE = "Learning";
+$PAGE_HEADER = <<<EOF
+<a href="index.php">Cover</a> \ Learning
+EOF;
+$PAGE_FOOTER = $PAGE_HEADER;
+
+require_once 'header.inc.php';
+?>
+Coming soon.
+<?php
+require_once 'footer.inc.php';
+?>
