0b590daf0211a1b2155f4d071a98f202a150f8a8
benfry <fry@processing.org>
2007-03-04 21:21:51 +0000

    fix up split/splitTokens mess

--- candy/src/processing/candy/SVG.java
+++ candy/src/processing/candy/SVG.java
@@ -563,7 +563,7 @@ public class SVG {
                 this.hasTransform = true;
                 transform = properties.getStringAttribute("transform");
                 transform = transform.substring(7, transform.length() - 2);
-                String tf[] = PApplet.split(transform);
+                String tf[] = PApplet.splitTokens(transform);
 
                 this.transformation = new float[tf.length];
                 for (int i = 0; i < transformation.length; i++)
@@ -1091,8 +1091,7 @@ public class SVG {
             this.closed = closed;
 
             if (properties.hasAttribute("points")) {
-                //pointsBuffer = PApplet.split(properties.getStringAttribute("points"), ' ');
-                pointsBuffer = PApplet.split(properties.getStringAttribute("points"));
+                pointsBuffer = PApplet.splitTokens(properties.getStringAttribute("points"));
             }
 
             points = new float[pointsBuffer.length][2];
@@ -1186,7 +1185,7 @@ public class SVG {
             //String pathDataKeys[] = PApplet.split(pathDataBuffer, '|');
             // use whitespace constant to get rid of extra spaces and CR or LF
             String pathDataKeys[] =
-                PApplet.split(pathDataBuffer, "|" + PConstants.WHITESPACE);
+                PApplet.splitTokens(pathDataBuffer, "|" + PConstants.WHITESPACE);
             //for (int j = 0; j < pathDataKeys.length; j++) {
             //    PApplet.println(j + "\t" + pathDataKeys[j]);
             //}
