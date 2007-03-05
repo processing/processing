2135c5cd41c8deda6b24bfbdb1678e4561495cc1
francisli <mail@francisli.com>
2007-03-05 07:05:45 +0000

    Home page finished for phones

--- web/phones/index.php
+++ web/phones/index.php
@@ -13,47 +13,57 @@ require '../header.inc.php';
 <div class="columnpadded">
 Mobile Processing sketches run on most mobile phones that support Java games and applications.<br />
 <br />
-To test your phone:<br />
+<b>To test your phone:</b><br />
+<br />
 <ol>
 <li>Go to <b>wapmp.at</b> in your phone's mobile web browser.<br /><br /></li>
 <li>Choose <b>My Phone</b> from the main menu.<br /><br /></li>
-<li>Download the <b>Profiler</b> sketch.<br /><br /></li>
+<li>Download a <b>Profiler</b> sketch.<br /><br /></li>
 <li>Run the Profiler and choose <b>Share Results</b> from the menu to upload the results to this website.<br /><br /></li>
 </div>
 <div class="column2x">
-<table width="100%" border="0" cellspacing="0" cellpadding="0">
+<table width="480" cellspacing="0" cellpadding="0" border="0">
+  <tr>
 <?php
 //// get the latest 20 submissions
 $link = db_connect();
-$query = "SELECT useragent, stamp, bluetooth, image2, messaging, phone, sound, videoplayback, videosnapshot, xml, configuration, profiles FROM profile_summary, profile_libraries, profile_microedition WHERE profile_summary.id=profile_libraries.id AND profile_microedition.id=profile_summary.id ORDER BY stamp DESC LIMIT 20";
+$query = "SELECT id, useragent, stamp, DATE_FORMAT(stamp, '%e %b %Y') as date FROM profile_summary ORDER BY stamp DESC LIMIT 20";
 $result = mysql_query($query);
 $wurfl = new tera_wurfl();
 $devices = array();
+$count = 0;
 while ($data = mysql_fetch_assoc($result)) {
   //// look up each one in wurfl
   if ($wurfl->getDeviceCapabilitiesFromAgent($data['useragent'])) {
     $name = $wurfl->brand .' '. $wurfl->model;
     if (is_null($devices[$name])) {
 ?>
-  <tr>
-    <td width="100" align="right">
-      <image src="../lib/tera-wurfl/<?php echo $wurfl->device_image ?>"><br />
+  <td width="160" align="center">
+      <a href="profile.php?id=<?php echo $data['id'] ?>"><image border="0" src="../lib/tera-wurfl/<?php echo $wurfl->device_image ?>"></a><br />
+      <b><a href="profile.php?id=<?php echo $data['id'] ?>"><?php echo $name ?></a></b><br />
       <br />
       <br />
-    </td>
-    <td align="left" valign="top">
-<?php
-      echo '<b>'. $name .'</b><br />';
-      echo $data['configuration'] .', '. $data['profiles'] .'<br />';
-?>
-    </td>
-  </tr> 
+      <br />
+      <br />
+  </td>
 <?php
       $devices[$name] = $data['useragent'];
+      $count++;
+      if (($count % 3) == 0) {
+         echo '</tr><tr>';
+      } else if ($count == 15) {
+          break;
+      }
     }
   }
 }
+for ($i = $count % 3; $i >= 0; $i--) {
+    echo '<td width="160">&nbsp;</td>';
+}
 ?>
+  </tr>
+</table>
+<p align="right"><b><a href="list.php">View complete list...</a></b></p>
 </div>
 </table>
 <?php
