e502c18e66ac787b73b1a59f5b70e60f3f84a27a
REAS <reas@processing.org>
2007-05-24 20:51:19 +0000

    Changes to the exhibition

--- web/generate/lib/Curated.class.php
+++ web/generate/lib/Curated.class.php
@@ -18,6 +18,7 @@ class Curated
 		$this->name 	= getAttribute($xml, 'name');
 		$this->by		= getAttribute($xml, 'by');
 		$this->scroll	= getAttribute($xml, 'scroll');
+		$this->resize	= getAttribute($xml, 'resize');
 		$this->width	= getAttribute($xml, 'width');
 		$this->height	= getAttribute($xml, 'height');
         
@@ -52,7 +53,7 @@ class Curated
     
     function display_piece()
     {
-        $link = sprintf("<a href=\"%s\" onclick=\"javascript:MM_openBrWindow(this.href,'%s','status=yes,scrollbars=%s,resizable=%s,width=%d,height=%d');return false;\">",
+        $link = sprintf("<a href=\"%s\" onclick=\"javascript:MM_openBrWindow(this.href,'%s','menubar=yes,status=yes,scrollbars=%s,resizable=%s,width=%d,height=%d');return false;\">",
                         $this->location, $this->name, $this->scroll, $this->resize, $this->width, $this->height);
         $html  = "<div class=\"curated-item\">\n";
         $html .= "\t$link<img src=\"/exhibition/{$this->image}\" width=\"223\" height=\"72\" alt=\"preview image\" title=\"{$this->name} by {$this->by}\" /></a>\n";
