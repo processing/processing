ba9f8450762a5943b682635d442361977db71c1f
REAS <reas@processing.org>
2007-05-28 15:06:42 +0000

    Changes to example web template

--- web/templates/template.nav.php
+++ web/templates/template.nav.php
@@ -7,10 +7,11 @@ $pages = array(
 	'Learning'      => array('/learning/index.html', 1),
     #'Examples'      => array('/learning/index.html', 2),
     #'Tutorials'     => array('/learning/tutorials/index.html', 2),
-	''      => array('/learning/index.html', 2),
-    'Basics'     => array('/learning/basics/index.html', 2),
-	'3D & OpenGL'     => array('/learning/3d/index.html', 2),
-	'Core Libraries'     => array('/learning/libraries/index.html', 2),
+	'Publications'      => array('/learning/index.html', 2),
+    'Basics'     		=> array('/learning/basics/index.html', 2),
+	'3D & OpenGL'		=> array('/learning/3d/index.html', 2),
+	'Core Libraries'	=> array('/learning/libraries/index.html', 2),
+	'Hacks'     		=> array('/learning/libraries/hacks/index.html', 2),
 	
     'Reference'     => array('/reference/index.html', 1),
     'Download'      => array('/download/index.html', 1),
@@ -18,10 +19,10 @@ $pages = array(
     'Contribute'    => array('/contribute/index.html', 1),
     'FAQ'           => array('/faq.html', 1),
     'Language'      => array('/reference/index.html', 1),
-    'Environment'   => array('/reference/environment/index.html', 2),
-    'Libraries'     => array('/reference/libraries/index.html', 2),
-    'Comparison'    => array('/reference/compare/index.html', 2),
-	'Troubleshooting'      => array('/reference/troubleshooting/index.html', 2)
+    'Environment'   	=> array('/reference/environment/index.html', 2),
+    'Libraries'     	=> array('/reference/libraries/index.html', 2),
+    'Comparison'    	=> array('/reference/compare/index.html', 2),
+	'Troubleshooting'	=> array('/reference/troubleshooting/index.html', 2)
     
     );
 
@@ -29,7 +30,7 @@ function navigation($section = '')
 {  
     $ref = array('Reference', 'Language', 'Environment', 'Libraries', 'Comparison', 'Troubleshooting');
     #$learn = array('Learning', 'Examples', 'Tutorials');
-	$learn = array('Learning', 'Publications', 'Basics', '3D & OpenGL', 'Core Libraries');
+	$learn = array('Learning', 'Publications', 'Basics', '3D & OpenGL', 'Core Libraries', 'Hacks');
     
     $html = "\t\t\t".'<div id="navigation">'."\n";
 
@@ -65,6 +66,7 @@ function navigation($section = '')
 		$html .= "\t\t\t\t\t" . l('Basics', $section == 'Basics') . " \\\n";;
 		$html .= "\t\t\t\t\t" . l('3D & OpenGL', $section == '3D & OpenGL') . " \\\n";;
 		$html .= "\t\t\t\t\t" . l('Core Libraries', $section == 'Core Libraries') . "\n";
+		$html .= "\t\t\t\t\t" . l('Hacks', $section == 'Hacks') . "\n";
         $html .= "\t\t\t\t</div>\n";
     }
 
