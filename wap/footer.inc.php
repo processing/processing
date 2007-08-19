953ba61464fa4a44db06611062ec3eb17ed881dd
francisli <mail@francisli.com>
2007-08-19 20:53:29 +0000

    Moving font size styling to stylesheet

--- wap/footer.inc.php
+++ wap/footer.inc.php
@@ -3,18 +3,14 @@
 if (isset($PAGE_FOOTER)) {
 ?>
     <div id="footer">
-      <small>
         <?php echo $PAGE_FOOTER ?><br />
-      </small>
     </div>
 <?php
 }
 ?>
     <div id="attribution">
-      <small>
         Mobile Processing<br />
         <a href="http://wapmp.at">http://wapmp.at/</a>
-      </small>
     </div>
   </body>
 </html>
