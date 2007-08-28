7b1e93f848f809acc37c86525fae45aee934f737
francisli <mail@francisli.com>
2007-08-28 04:06:05 +0000

    New examples generation scripts

---
+++ web/learning/regenerate.php
@@ -0,0 +1,14 @@
+<?php
+
+//// needs the database, so include db header
+require_once '../db.inc.php';
+
+//// needs the generation code
+require_once 'generate.inc.php';
+
+//// regenerate!
+links_generate();
+
+//// go back to index page
+header('Location: index.php');
+?>
