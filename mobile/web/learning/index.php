34cce0f48b2a2c228cdf9546e2dbdf29df265735
francisli <mail@francisli.com>
2007-08-28 06:05:20 +0000

    Using generated example links

--- mobile/web/learning/index.php
+++ mobile/web/learning/index.php
@@ -6,43 +6,14 @@ require '../header.inc.php';
 <img src="images/header.png"><br />
 <br />
 <br />
-<div class="column">
-<img src="images/form.png"><br />
-<br />
-<a href="example.php?name=pointslines">Points and Lines</a><br />
-<a href="example.php?name=shapeprimitives">Shape Primitives</a><br />
-<br />
-<br />
-<img src="../reference/images/net.gif" /><br />
-<br />
-<a href="example.php?name=fetch">Fetch</a><br />
-<br />
-</div>
-<div class="column">
-<img src="images/input.png"><br />
-<br />
-<a href="example.php?name=keypad">Keypad</a><br />
-<br />
-<br />
-<br />
-<img src="images/core.png"><br />
-<br />
-<i>Bluetooth</i><br />
-<a href="example.php?name=bluescan">Bluescan</a><br />
-<a href="example.php?name=tictactoe">Tic-Tac-Toe</a><br />
-<br />
-<i>Video</i><br />
-<a href="example.php?name=photoslider">Photoslider</a><br />
-<br />
-<i>XML</i><br />
-<a href="example.php?name=yahoosonar">Yahoo! Sonar</a><br />
-<br />
-</div>
-<div class="column">
-<img src="images/color.png"><br />
-<br />
-<a href="example.php?name=relativity">Relativity</a><br />
-</div>
+<?php 
+$lockfp = fopen('lockfile', 'r');
+if ($lockfp !== FALSE) {
+    if (flock($lockfp, LOCK_SH)) {
+        require 'generated/links.inc.php';
+    }
+}
+?>
 <?php
  require '../footer.inc.php';
 ?>
