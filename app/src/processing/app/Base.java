815bb420343b21509415ad55c3ed8243247dfdf4
benfry <fry@processing.org>
2007-10-20 13:18:51 +0000

    starting release 0131

--- app/src/processing/app/Base.java
+++ app/src/processing/app/Base.java
@@ -44,8 +44,8 @@ import processing.core.*;
  * files and images, etc) that comes from that.
  */
 public class Base {
-  static final int VERSION = 130;
-  static final String VERSION_NAME = "0130 Beta";
+  static final int VERSION = 131;
+  static final String VERSION_NAME = "0131 Beta";
 
   // set to true after the first time it's built.
   // so that the errors while building don't show up again.
