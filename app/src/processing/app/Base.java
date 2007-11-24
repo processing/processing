a25e331bc2bab52bf5962a891f26e61e5b4dee4a
benfry <fry@processing.org>
2007-11-24 15:11:30 +0000

    starting rev 0136

--- app/src/processing/app/Base.java
+++ app/src/processing/app/Base.java
@@ -44,8 +44,8 @@ import processing.core.*;
  * files and images, etc) that comes from that.
  */
 public class Base {
-  static final int VERSION = 135;
-  static final String VERSION_NAME = "0135 Beta";
+  static final int VERSION = 136;
+  static final String VERSION_NAME = "0136 Beta";
 
   // set to true after the first time it's built.
   // so that the errors while building don't show up again.
