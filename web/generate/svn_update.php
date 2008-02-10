8f2a740d8b730759c7cde0391b3b35e89280ac8a
REAS <reas@processing.org>
2008-02-10 01:03:13 +0000

    Adding the SVN updates from the generate page

---
+++ web/generate/svn_update.php
@@ -0,0 +1,28 @@
+<?
+
+require('../config.php');
+$benchmark_start = microtime_float();
+
+// arguments
+$lang = isSet($_POST['lang']) ? $_POST['lang'] : 'en';
+
+// get translation file
+$where = CONTENTDIR . 'api_' . $lang;
+
+// look for the .subversion folder somewhere else
+// otherwise will go looking for /home/root/.subversion or some other user
+putenv('HOME=' . CONTENTDIR);
+
+// do the initial checkout
+//`cd /var/www/processing && /usr/local/bin/svn co svn://processing.org/trunk/web/content`;
+
+// do things here
+`cd $where && /usr/local/bin/svn update`;
+
+$benchmark_end = microtime_float();
+$execution_time = round($benchmark_end - $benchmark_start, 4);
+
+?>
+
+<h2>Updated <?=$where?> </h2>
+<p>And it only took me <?=$execution_time?> seconds.</p>
