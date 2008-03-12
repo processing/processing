86da13fec0c79b0eac7ea192cd305c346eaabf5c
benfry <fry@processing.org>
2008-03-12 06:17:48 +0000

    last of the changes for the new debug setup

--- build/windows/run.sh
+++ build/windows/run.sh
@@ -45,7 +45,7 @@ fi
 # includes jaws.jar
 #CLASSPATH=\"java\\lib\\rt.jar\;java\\lib\\jaws.jar\;lib\;lib\\build\;lib\\pde.jar\;lib\\kjc.jar\;lib\\antlr.jar\;lib\\oro.jar\;lib\\comm.jar\;lib\\RXTXcomm.jar\;${QT_JAVA_PATH}\"
 
-CLASSPATH=\"java\\lib\\rt.jar\;lib\;lib\\build\;lib\\pde.jar\;lib\\core.jar\;lib\\mrj.jar\;lib\\antlr.jar\;lib\\oro.jar\;lib\\registry.jar\;${QTJAVA}\"
+CLASSPATH=\"java\\lib\\rt.jar\;lib\;lib\\build\;lib\\pde.jar\;lib\\core.jar\;lib\\mrj.jar\;lib\\antlr.jar\;lib\\oro.jar\;lib\\registry.jar\;lib\\tools.jar\;${QTJAVA}\"
 export CLASSPATH
 
 #cd work && ./java/bin/java -Xint PdeBase
