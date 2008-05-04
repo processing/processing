78c4d2d7eca4e16f01cedb80de20ad25c1d31ae4
benfry <fry@processing.org>
2008-05-04 23:31:43 +0000

    upgrading to nanoxml instead of nanoxml-lite

--- xml/src/processing/xml/XMLParseException.java
+++ xml/src/processing/xml/XMLParseException.java
@@ -1,125 +1,69 @@
-/*
-  This code is from NanoXML 2.2.3 Lite
-  Copyright (C) 2000-2002 Marc De Scheemaecker, All Rights Reserved.
-
-  Additional modifications Copyright (c) 2006 Ben Fry and Casey Reas 
-  to make the code better-suited for use with Processing.
-
-  Original license notice from Marc De Scheemaecker:
-  
-  This software is provided 'as-is', without any express or implied warranty.
-  In no event will the authors be held liable for any damages arising from the
-  use of this software.
- 
-  Permission is granted to anyone to use this software for any purpose,
-  including commercial applications, and to alter it and redistribute it
-  freely, subject to the following restrictions:
- 
-  1. The origin of this software must not be misrepresented; you must not
-     claim that you wrote the original software. If you use this software in
-     a product, an acknowledgment in the product documentation would be
-     appreciated but is not required.
- 
-  2. Altered source versions must be plainly marked as such, and must not be
-     misrepresented as being the original software.
- 
-  3. This notice may not be removed or altered from any source distribution.
-*/
+/* XMLParseException.java                                          NanoXML/Java
+ *
+ * $Revision: 1.3 $
+ * $Date: 2002/01/04 21:03:29 $
+ * $Name: RELEASE_2_2_1 $
+ *
+ * This file is part of NanoXML 2 for Java.
+ * Copyright (C) 2000-2002 Marc De Scheemaecker, All Rights Reserved.
+ *
+ * This software is provided 'as-is', without any express or implied warranty.
+ * In no event will the authors be held liable for any damages arising from the
+ * use of this software.
+ *
+ * Permission is granted to anyone to use this software for any purpose,
+ * including commercial applications, and to alter it and redistribute it
+ * freely, subject to the following restrictions:
+ *
+ *  1. The origin of this software must not be misrepresented; you must not
+ *     claim that you wrote the original software. If you use this software in
+ *     a product, an acknowledgment in the product documentation would be
+ *     appreciated but is not required.
+ *
+ *  2. Altered source versions must be plainly marked as such, and must not be
+ *     misrepresented as being the original software.
+ *
+ *  3. This notice may not be removed or altered from any source distribution.
+ */
 
 package processing.xml;
 
 
 /**
- * An XMLParseException is thrown when an error occures while parsing an XML
- * string.
- *
- * @see processing.xml.XMLElement
+ * An XMLParseException is thrown when the XML passed to the XML parser is not
+ * well-formed.
  *
  * @author Marc De Scheemaecker
+ * @version $Name: RELEASE_2_2_1 $, $Revision: 1.3 $
  */
 public class XMLParseException
-    extends RuntimeException
+   extends XMLException
 {
 
-    /**
-     * Indicates that no line number has been associated with this exception.
-     */
-    public static final int NO_LINE = -1;
+   /**
+    * Creates a new exception.
+    *
+    * @param msg the message of the exception.
+    */
+   public XMLParseException(String msg)
+   {
+      super(msg);
+   }
 
 
-    /**
-     * The line number in the source code where the error occurred, or
-     * <code>NO_LINE</code> if the line number is unknown.
-     *
-     * <dl><dt><b>Invariants:</b></dt><dd>
-     * <ul><li><code>lineNumber &gt 0 || lineNumber == NO_LINE</code>
-     * </ul></dd></dl>
-     */
-    private int lineNumber;
-
-
-    /**
-     * Creates an exception.
-     *
-     * @param name    The name of the element where the error is located.
-     * @param message A message describing what went wrong.
-     *
-     * </dl><dl><dt><b>Preconditions:</b></dt><dd>
-     * <ul><li><code>message != null</code>
-     * </ul></dd></dl>
-     *
-     * <dl><dt><b>Postconditions:</b></dt><dd>
-     * <ul><li>getLineNumber() => NO_LINE
-     * </ul></dd></dl><dl>
-     */
-    public XMLParseException(String name,
-                             String message)
-    {
-        super("XML Parse Exception during parsing of "
-              + ((name == null) ? "the XML definition"
-                                : ("a " + name + " element"))
-              + ": " + message);
-        this.lineNumber = XMLParseException.NO_LINE;
-    }
-
-
-    /**
-     * Creates an exception.
-     *
-     * @param name    The name of the element where the error is located.
-     * @param lineNumber  The number of the line in the input.
-     * @param message A message describing what went wrong.
-     *
-     * </dl><dl><dt><b>Preconditions:</b></dt><dd>
-     * <ul><li><code>message != null</code>
-     *     <li><code>lineNumber &gt; 0</code>
-     * </ul></dd></dl>
-     *
-     * <dl><dt><b>Postconditions:</b></dt><dd>
-     * <ul><li>getLineNumber() => lineNumber
-     * </ul></dd></dl><dl>
-     */
-    public XMLParseException(String name,
-                             int    lineNumber,
-                             String message)
-    {
-        super("XML Parse Exception during parsing of "
-              + ((name == null) ? "the XML definition"
-                                : ("a " + name + " element"))
-              + " at line " + lineNumber + ": " + message);
-        this.lineNumber = lineNumber;
-    }
-
-
-    /**
-     * Where the error occurred, or <code>NO_LINE</code> if the line number is
-     * unknown.
-     *
-     * @see processing.xml.XMLParseException#NO_LINE
-     */
-    public int getLineNumber()
-    {
-        return this.lineNumber;
-    }
+   /**
+    * Creates a new exception.
+    *
+    * @param systemID the system ID from where the data came
+    * @param lineNr   the line number in the XML data where the exception
+    *                 occurred.
+    * @param msg      the message of the exception.
+    */
+   public XMLParseException(String systemID,
+                            int    lineNr,
+                            String msg)
+   {
+      super(systemID, lineNr, null, msg, true);
+   }
 
 }
