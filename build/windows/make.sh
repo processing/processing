cf548e782a8391ad18ad45576fb45c5f04509dbf
benfry <fry@processing.org>
2008-06-10 02:09:07 +0000

    working on windows build/dist script

--- build/windows/make.sh
+++ build/windows/make.sh
@@ -9,15 +9,19 @@ then
 else
   echo Setting up directories to build P5...
   BUILD_PREPROC=true
-  cp -r ../shared work
-  rm -rf work/.svn
-  rm -f work/.DS_Store 
-  # in case one of those little mac poopers show up
 
-  # needs to make the dir because of packaging goofiness
-  mkdir -p work/classes/processing/app/preproc
-  mkdir -p work/classes/processing/app/syntax
-  mkdir -p work/classes/processing/app/tools
+  mkdir work
+  cp -r ../shared/lib work/
+  cp -r ../shared/libraries work/
+
+  cp ../../app/lib/antlr.jar work/lib/
+  cp ../../app/lib/jna.jar work/lib/
+
+  echo Extracting examples...
+  unzip -q -d work/ ../shared/examples.zip
+
+  echo Extracting reference...
+  unzip -q -d work/ ../shared/reference.zip
 
   cp -r ../../net work/libraries/
   cp -r ../../opengl work/libraries/
@@ -28,55 +32,21 @@ else
   cp -r ../../xml work/libraries/
   cp -r ../../candy work/libraries/
 
-  echo Extracting examples...
-  cd work
-  unzip -q examples.zip
-  rm examples.zip
-  cd ..
-
-  echo Extracting reference...
-  cd work
-  unzip -q reference.zip
-  # necessary for launching reference from shell/command prompt
-  # which is done internally to view reference
-  chmod +x reference/*.html
-  # needed by 'help' menu
-  chmod +x reference/environment/*.html
-  # chmod -R +x *.html doesn't seem to work
-
-  rm reference.zip
-  cd ..
-
   echo Extracting enormous JRE...
   unzip -q -d work jre.zip
-  # cygwin requires this because of unknown weirdness
-  # it was not formerly this anal retentive
-  cd work/java/bin/
-  #chmod +x *.exe *.dll 
-  #chmod +x client/*.dll
-  cd ../../..
-
-  mkdir work/lib/build
-  #mkdir work/classes
 
   echo Compiling processing.exe
   cd launcher
-  #chmod +x make.sh
-  #./make.sh # && cp processing.exe ../work/
   make 
   make application.exe
   cd ..
 
-  # get windows version of tools.jar
-  cp dist/tools.jar work/lib/
-
-  # get jikes and depedencies
-  cp dist/jikes.exe work/
-  #chmod +x work/jikes.exe
-
-  cp dist/ICE_JNIRegistry.dll work/
-
   # chmod +x the crew
+  # cygwin requires this because of unknown weirdness
+  # it was not formerly this anal retentive
+  # with the html, it's necessary on windows for launching reference 
+  # from shell/command prompt, which is done internally to view reference
+  find work -name "*.html" -exec chmod +x {} ';'
   find work -name "*.dll" -exec chmod +x {} ';'
   find work -name "*.exe" -exec chmod +x {} ';'
   find work -name "*.html" -exec chmod +x {} ';'
@@ -87,21 +57,18 @@ cd ../..
 
 ### -- BUILD CORE ----------------------------------------------
 
-echo Building processing.core
+echo Building processing.core...
 
-# move to core inside base 'processing' directory
 cd core
-#rm -f processing/core/*.class
 
-CLASSPATH="..\\build\\windows\\work\\java\\lib\\rt.jar"
+CLASSPATH="..\\build\\windows\\work\\java\\lib\\rt.jar;..\\build\\windows\\work\\java\\lib\\tools.jar"
 export CLASSPATH
 
 perl preproc.pl
 
 mkdir -p bin
-../build/windows/work/jikes -d bin +D -target 1.1 src/processing/core/*.java
-# use this from time to time to test 1.1 savviness
-#/cygdrive/c/msjdk-4.0/bin/jvc /d . src/processing/core/*.java
+../build/windows/work/java/bin/java sun.tools.javac.Main \
+    -d bin -source 1.5 -target 1.5 src/processing/core/*.java
 
 rm -f ../build/windows/work/lib/core.jar
 find bin -name "*~" -exec rm -f {} ';'
@@ -115,16 +82,10 @@ cd ..
 
 ### -- BUILD PREPROC ---------------------------------------------
 
-# i suck at shell scripting
-#if [ $1 = "preproc" ] 
-#then 
-#BUILD_PREPROC=true
-#fi
-
 if $BUILD_PREPROC
 then
 
-echo Building PDE for JDK 1.4
+echo Building PDE for JDK 1.5
 
 cd app
 
@@ -149,7 +110,7 @@ cd app
 # why this is required on windows and not the others is beyond me
 cp src/antlr/java/JavaTokenTypes.txt src/processing/app/preproc/
 
-# this is a total disaster...fix for next release
+# this is a total disaster...fix me soon!
 cd src/processing/app/preproc
 ../../../../../build/windows/work/java/bin/java \
   -cp "..\\..\\..\\..\\..\\build\\windows\\work\\lib\\antlr.jar" antlr.Tool \
@@ -168,19 +129,24 @@ fi
 
 cd app
 
-CLASSPATH="..\\build\\windows\\work\\lib\\core.jar;..\\build\\windows\\work\\lib\\apple.jar;..\\build\\windows\\work\\lib\antlr.jar;..\\build\\windows\\work\\lib\\registry.jar;..\\build\\windows\\work\\lib\\tools.jar;..\\build\\windows\\work\\java\\lib\\rt.jar"
+CLASSPATH=
 
-# compile the code as java 1.3, so that the application will run and
-# show the user an error, rather than crapping out with some strange
-# "class not found" crap
-../build/windows/work/jikes -target 1.3 +D -classpath "$CLASSPATH;..\\build\\windows\\work/classes" -d ..\\build\\windows\\work/classes src/processing/app/*.java src/processing/app/debug/*.java src/processing/app/preproc/*.java src/processing/app/syntax/*.java src/processing/app/tools/*.java src/antlr/*.java src/antlr/java/*.java
-#/cygdrive/c/jdk-1.4.2_05/bin/javac.exe -classpath $CLASSPATH -d ..\\build\\windows\\work/classes *.java jeditsyntax/*.java preprocessor/*.java
+../build/windows/work/java/bin/java sun.tools.javac.Main 
+    -source 1.5 -target 1.5 \
+    -classpath "..\\build\\windows\\work\\lib\\core.jar;..\\build\\windows\\work\\lib\\apple.jar;..\\build\\windows\\work\\lib\antlr.jar;..\\build\\windows\\work\\lib\\registry.jar;..\\build\\windows\\work\\lib\\tools.jar;..\\build\\windows\\work\\java\\lib\\rt.jar" \
+    -d ..\\build\\windows\\work/classes \
+    src/processing/app/*.java \
+    src/processing/app/debug/*.java \
+    src/processing/app/syntax/*.java \
+    src/processing/app/preproc/*.java \
+    src/processing/app/tools/*.java \
+    src/processing/app/windows/*.java \
+    src/antlr/*.java \
+    src/antlr/java/*.java 
 
 cd ../build/windows/work/classes
 rm -f ../lib/pde.jar
 zip -0rq ../lib/pde.jar .
-
-# back to build/windows
 cd ../..
 
 
@@ -190,7 +156,7 @@ PLATFORM=windows
 
 
 CLASSPATH="..\\build\\$PLATFORM\\work\\lib\\core.jar;..\\build\\$PLATFORM\\work\\java\\lib\\rt.jar"
-JIKES=../build/$PLATFORM/work/jikes
+JAVAC="../build/windows/work/java/bin/java sun.tools.javac.Main -source 1.5 -target 1.5"
 CORE="..\\build\\$PLATFORM\\work\\lib\\core.jar"
 LIBRARIES="..\\build\\$PLATFORM\\work\\libraries"
 
@@ -202,7 +168,7 @@ cd ..
 echo Building serial library...
 cd ../serial
 mkdir -p bin
-$JIKES -target 1.1 +D \
+$JAVAC \
     -classpath "library\\RXTXcomm.jar;$CORE;$CLASSPATH" \
     -d bin src/processing/serial/*.java 
 rm -f library/serial.jar
@@ -216,7 +182,7 @@ cp library/serial.jar $LIBRARIES/serial/library/
 echo Building net library...
 cd ../net
 mkdir -p bin
-$JIKES -target 1.1 +D -d bin src/processing/net/*.java 
+$JAVAC -d bin src/processing/net/*.java 
 rm -f library/net.jar
 find bin -name "*~" -exec rm -f {} ';'
 cd bin && zip -r0q ../library/net.jar processing/net/*.class && cd ..
@@ -242,7 +208,7 @@ else
 fi
 cd ../video
 mkdir -p bin
-$JIKES -target 1.1 +D \
+$JAVAC \
     -classpath "$QTJAVA;$CLASSPATH" \
     -d bin src/processing/video/*.java 
 rm -f library/video.jar
@@ -256,7 +222,7 @@ cp library/video.jar $LIBRARIES/video/library/
 echo Building OpenGL library...
 cd ../opengl
 mkdir -p bin
-$JIKES -target 1.1 +D \
+$JAVAC \
     -classpath "library\\jogl.jar;$CLASSPATH" \
     -d bin src/processing/opengl/*.java 
 rm -f library/opengl.jar
@@ -270,7 +236,7 @@ cp library/opengl.jar $LIBRARIES/opengl/library/
 echo Building PDF library...
 cd ../pdf
 mkdir -p bin
-$JIKES -target 1.1 +D \
+$JAVAC \
     -classpath "library\\itext.jar;$CLASSPATH" \
     -d bin src/processing/pdf/*.java 
 rm -f library/pdf.jar
@@ -284,7 +250,7 @@ cp library/pdf.jar $LIBRARIES/pdf/library/
 echo Building DXF library...
 cd ../dxf
 mkdir -p bin
-$JIKES -target 1.1 +D -d bin src/processing/dxf/*.java 
+$JAVAC -d bin src/processing/dxf/*.java 
 rm -f library/dxf.jar
 find bin -name "*~" -exec rm -f {} ';'
 cd bin && zip -r0q ../library/dxf.jar processing/dxf/*.class && cd ..
@@ -296,7 +262,7 @@ cp library/dxf.jar $LIBRARIES/dxf/library/
 echo Building XML library...
 cd ../xml
 mkdir -p bin
-$JIKES -target 1.1 +D -d bin src/processing/xml/*.java 
+$JAVAC -d bin src/processing/xml/*.java 
 rm -f library/xml.jar
 find bin -name "*~" -exec rm -f {} ';'
 cd bin && zip -r0q ../library/xml.jar processing/xml/*.class && cd ..
@@ -309,7 +275,7 @@ cp library/xml.jar $LIBRARIES/xml/library/
 echo Building Candy SVG library...
 cd ../candy
 mkdir -p bin
-$JIKES -target 1.1 +D \
+$JAVAC \
     -classpath "..\\xml\\library\\xml.jar;$CLASSPATH" \
     -d bin src/processing/candy/*.java 
 rm -f library/candy.jar
