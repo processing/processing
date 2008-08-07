cc87db523596a341cc27da2010ea6b99e7e18f74
REAS <reas@processing.org>
2008-08-07 20:43:44 +0000

    Update for exhibition

--- web/generate/exhibition.php
+++ web/generate/exhibition.php
@@ -1,7 +1,7 @@
 <?
 
 define('CURATED_PER_PAGE', 12);
-define('NETWORK_FIRST_PAGE', 30);
+define('NETWORK_FIRST_PAGE', 25);
 define('NETWORK_PER_PAGE', 90);
 
 if (!defined('SUBMIT')) {
@@ -27,6 +27,42 @@ function get_curated($curated, $start = 0, $num = 12)
     if ($j % 2 != 0) $html .= '<td>&nbsp;</td></tr>';
     return $html . '</table>';
 }
+
+function get_curated_one($curated, $start = 0, $num = 12)
+{
+    // output html
+    $html = '<table width="224" cellspacing="0" cellpadding="0" border="0">';
+    $j = 0;
+    for ($i = $start; $i < $start+$num; $i++) {
+      if ($curated[$i]) {
+        $html .= '<tr>';
+	$html .= '<td>' . $curated[$i]->display() . '</td>';
+        $html .= '</tr>';
+        $j++;
+      }
+    }
+    // if ($j % 2 != 0) $html .= '<td>&nbsp;</td></tr>';
+   return $html . '</table>';
+}
+
+function get_curated_three($curated, $start = 0, $num = 12)
+{
+  // output html                                                                                                          
+  $html = '<table width="672" cellspacing="0" cellpadding="0" border="0">';
+  $j = 0;
+  for ($i = $start; $i < $start+$num; $i++) {
+    if ($curated[$i]) {
+      if ($j % 3 == 0) $html .= '<tr>';
+      $html .= '<td>' . $curated[$i]->display() . '</td>';
+      if ($j % 3 == 2) $html .= '</tr>';
+      $j++;
+    }
+  }
+  if ($j % 3 == 1) $html .= '<td>&nbsp;</td></tr>';                                                                    
+  if ($j % 3 == 2) $html .= '<td>&nbsp;</td><td>&nbsp;</td></tr>';
+  return $html . '</table>';
+}
+
 
 function get_curated_short()
 {
@@ -118,30 +154,32 @@ if (!defined('COVER')) {
     $ntotal = count($network);
     // count number of pages needed
     $cnum_pages = ceil($ctotal / CURATED_PER_PAGE);
-    $nnum_pages = ceil(($ntotal - NETWORK_FIRST_PAGE) / NETWORK_PER_PAGE)+1;
+    $nnum_pages = ceil($ntotal / NETWORK_PER_PAGE)+1;
     
     // create and write the first page
-    $page = new Page('Exhibition', 'Exhibition');
+    $page = new Page('Exhibition', 'Index');
     $page->subtemplate('template.exhibition.html');
-    $page->set('exhibition', get_curated($curated, 0, CURATED_PER_PAGE));
+    $page->set('exhibition', get_curated_one($curated, 0, CURATED_PER_PAGE/2));
     $page->set('network', get_network_list($network, NETWORK_FIRST_PAGE));
     writeFile("exhibition/index.html", $page->out());
     
     // create and write the other pages
-    for ($i = 1; $i <= $cnum_pages; $i++) {
-        $page = new Page('Exhibition Archives', 'Exhibition');
+    for ($i = 0; $i <= $cnum_pages; $i++) {
+        $page = new Page('Exhibition Archives', 'Collection');
         $page->subtemplate('template.curated.archive.html');
         $page->set('curated_nav', curated_nav($cnum_pages, $i+1));
-        $page->set('exhibition', get_curated($curated, CURATED_PER_PAGE*$i, CURATED_PER_PAGE));
-        $pagename = sprintf("curated_page_%d.html", $i+1);
+        $page->set('exhibition', get_curated_three($curated, CURATED_PER_PAGE*$i, CURATED_PER_PAGE));
+        //$pagename = sprintf("curated_page_%d.html", $i+1);
+        $pagename = sprintf("curated_page_%d.html", $cnum_pages-$i);
         writeFile("exhibition/".$pagename, $page->out());
     }
     
-    for ($i = 2; $i <= $nnum_pages; $i++) {
-        $page = new Page('Network Archives', 'Exhibition', 'Network');
+    for ($i = 0; $i <= $nnum_pages; $i++) {
+        $page = new Page('Network Archives', 'Network Links');
         $page->subtemplate('template.network.archive.html');
         $page->set('network_nav', network_nav($nnum_pages, $i));
-        $page->set('network', get_network_table($network, NETWORK_PER_PAGE*($i-2)+NETWORK_FIRST_PAGE, NETWORK_PER_PAGE));
+        //$page->set('network', get_network_table($network, NETWORK_PER_PAGE*($i-2)+NETWORK_FIRST_PAGE, NETWORK_PER_PAGE));
+        $page->set('network', get_network_table($network, NETWORK_PER_PAGE*($i-1), NETWORK_PER_PAGE));
         $pagename = sprintf("network_page_%d.html", $i);
         writeFile("exhibition/".$pagename, $page->out());
     }
@@ -160,24 +198,42 @@ EOC;
 function curated_nav($num, $current)
 {
     $html = '<p class="exhibition-nav">';
-    $links[] = '<a href="index.html">1</a>';
-    for ($i = 2; $i <= $num; $i++) {
-        $links[] = ($i == $current) ? $i : sprintf("<a href=\"curated_page_%d.html\">%d</a>", $i, $i);
+    //if ($num == current) {
+    //  $links[] = sprintf("%d", $num);
+    //} else {
+    //  $links[] = sprintf("<a href=\"curated_page_%d.html\">%d</a>", $num, $num);
+    //}
+    for ($i = $num; $i > 0; $i--) {
+        $links[] = (($num-$i+1) == $current) ? $i : sprintf("<a href=\"curated_page_%d.html\">%d</a>", $i, $i);
     }
     $html .= implode(' \\ ', $links);
-    $html .= '</p>';
+    $html .= '</p>&nbsp;';
     return $html;
 }
+
+/*
+function curated_nav($num, $current)
+{
+  $html = '<p class="exhibition-nav">';
+  $links[] = '<a href="curated_page_1.html">1</a>';
+  for ($i = 2; $i <= $num; $i++) {
+    $links[] = ($i == $current) ? $i : sprintf("<a href=\"curated_page_%d.html\">%d</a>", $i, $i);
+  }
+  $html .= implode(' \\ ', $links);
+  $html .= '</p>&nbsp;';
+  return $html;
+}
+*/
 
 function network_nav($num, $current)
 {
     $html = '<p class="exhibition-nav">';
-    $links[] = '<a href="index.html">1</a>';
+    $links[] = '<a href="network_page_1.html">1</a>';
     for ($i = 2; $i <= $num; $i++) {
         $links[] = ($i == $current) ? $i : sprintf("<a href=\"network_page_%d.html\">%d</a>", $i, $i);
     }
     $html .= implode(' \\ ', $links);
-    $html .= '</p>';
+    $html .= '</p>&nbsp;';
     return $html;
 }
 
