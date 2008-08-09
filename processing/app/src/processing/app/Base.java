7527d6dda4f06e3e11bfa33d957fda13f705a000
benfry <fry@processing.org>
2008-08-09 16:04:45 +0000

    upping the release number

--- processing/app/src/processing/app/Base.java
+++ processing/app/src/processing/app/Base.java
@@ -42,8 +42,8 @@ import processing.core.*;
  * files and images, etc) that comes from that.
  */
 public class Base {
-  static final int VERSION = 143;
-  static final String VERSION_NAME = "0143 Beta";
+  static final int VERSION = 144;
+  static final String VERSION_NAME = "0144 Beta";
 
   static Platform platform;
 
