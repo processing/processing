e155d7cbce8a159ea917ab26a2224bc570ebf4e3
REAS <reas@processing.org>
2008-09-10 20:53:07 +0000

    Reverting to previous

--- web/templates/template.nav.php
+++ web/templates/template.nav.php
@@ -77,7 +77,6 @@ $pages = array(
     #'Examples'     => array('/learning/index.html', 2),
     #'Tutorials'    => array('/learning/tutorials/index.html', 2),
     'Overview'	    => array('/learning/index.html', 2),
-    'Getting Started'	     => array('/learning/gettingstarted/index.html', 2),
     'Basics'        => array('/learning/basics/index.html', 2),
     'Topics'        => array('/learning/topics/index.html', 2),
     '3D & OpenGL'   => array('/learning/3d/index.html', 2),
@@ -108,7 +107,7 @@ function navigation($section = '')
 
     $ref = array('Reference', 'Language', 'Environment', 'Libraries', 'Compare', 'Troubleshooting');
     #$learn = array('Learning', 'Examples', 'Tutorials');
-    $learn = array('Learning', 'Overview', 'Getting Started', 'Basics', 'Topics', '3D & OpenGL', 'Library Examples', 'Books');
+    $learn = array('Learning', 'Overview', 'Basics', 'Topics', '3D & OpenGL', 'Library Examples', 'Books', 'Hacks');
     #$exhib = array('Exhibition', 'Index', 'Collection', 'Network Links', 'Features');    
     $exhib = array('Exhibition', 'Index', 'Collection', 'Network Links');    
 
@@ -166,13 +165,12 @@ function navigation($section = '')
         $html .= "\t\t\t\t" . '<div class="navBar learning" id="subNav">' . "\n";
 		
         $html .= "\t\t\t\t\t" . l('Overview', $section == 'Overview') . " \\\n";
-        $html .= "\t\t\t\t\t" . l('Getting Started', $section == 'Getting Started') . " \\\n";
 		$html .= "\t\t\t\t\t" . l('Basics', $section == 'Basics') . " \\\n";
 		$html .= "\t\t\t\t\t" . l('Topics', $section == 'Topics') . " \\\n";
 		$html .= "\t\t\t\t\t" . l('3D & OpenGL', $section == '3D & OpenGL') . " \\\n";
 		$html .= "\t\t\t\t\t" . l('Library Examples', $section == 'Library Examples') . " \\\n";
-		$html .= "\t\t\t\t\t" . l('Books', $section == 'Books') . " \n";
-		//$html .= "\t\t\t\t\t" . l('Hacks', $section == 'Hacks') . " \n";
+		$html .= "\t\t\t\t\t" . l('Books', $section == 'Books') . " \\\n";
+		$html .= "\t\t\t\t\t" . l('Hacks', $section == 'Hacks') . " \n";
         $html .= "\t\t\t\t</div>\n";
     }
 
