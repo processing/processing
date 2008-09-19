572cd311efd0557812ac2889c9f3f149f5a168a9
benfry <fry@processing.org>
2008-09-19 15:07:37 +0000

    fix X/Y/Z and TX/TY/TZ mixup because of new indices

--- core/src/processing/core/PImage.java
+++ core/src/processing/core/PImage.java
@@ -193,7 +193,12 @@ public class PImage implements PConstants, Cloneable {
    * Returns a BufferedImage from this PImage.
    */
   public java.awt.Image getImage() {
-    return null;
+    int type = (format == RGB) ? 
+      BufferedImage.TYPE_INT_ARGB : BufferedImage.TYPE_INT_RGB; 
+    BufferedImage image = new BufferedImage(width, height, type);
+    WritableRaster wr = image.getRaster();
+    wr.setDataElements(0, 0, width, height, pixels);
+    return image;
   }
 
 
