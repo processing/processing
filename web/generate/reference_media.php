7476833d9e948d20ed4ba92ef33af7a8688d71f9
REAS <reas@processing.org>
2008-10-09 05:23:04 +0000

    Mod to update new images

--- web/generate/reference_media.php
+++ web/generate/reference_media.php
@@ -3,6 +3,24 @@
 require('../config.php');
 $benchmark_start = microtime_float();
 
+
+// make troubleshooting page
+$source = CONTENTDIR."static/tutorials/";
+$path = BASEDIR;
+
+// update the files on the server via SVN
+
+// look for the .subversion folder somewhere else
+// otherwise will go looking for /home/root/.subversion or some other user
+$where = CONTENTDIR . 'api_media';
+putenv('HOME=' . CONTENTDIR);
+
+// do the initial checkout
+//`cd /var/www/processing && /usr/local/bin/svn co svn://processing.org/trunk/web/content/`;
+
+`cd $where && /usr/local/bin/svn update`;
+
+
 if (!is_dir(REFERENCEDIR.'media')) { mkdir(REFERENCEDIR.'media', '0757'); }
 copydirr(CONTENTDIR.'api_media', REFERENCEDIR.'media', null, 0757, true);
 
