f7a3b6944e26a6a00f6d43ac552d47d24350f0fe
benfry <fry@processing.org>
2008-10-11 23:55:09 +0000

    fix osx build script

--- processing/build/macosx/dist.sh
+++ processing/build/macosx/dist.sh
@@ -13,78 +13,81 @@ then
   exit
 fi
 
-./make.sh
-
 echo Creating P5 distribution for revision $REVISION...
 
 # remove any old boogers
-rm -rf processing 
-rm -rf Processing*
-rm -rf processing-*
+mv processing ~/.Trash/
+mv Processing* ~/.Trash/
+mv processing-* ~/.Trash/
+mv work ~/.Trash/
 
-mkdir processing
-cp -r ../shared/lib processing/
-cp -r ../shared/libraries processing/
-cp -r ../shared/tools processing/
-cp ../../app/lib/antlr.jar processing/lib/
-cp ../../app/lib/ecj.jar processing/lib/
-cp ../../app/lib/jna.jar processing/lib/
-cp ../shared/revisions.txt processing/
+./make.sh
 
-echo Extracting examples...
-unzip -q -d processing/ ../shared/examples.zip
+#mkdir processing
+#cp -r ../shared/lib processing/
+#cp -r ../shared/libraries processing/
+#cp -r ../shared/tools processing/
+#cp ../../app/lib/antlr.jar processing/lib/
+#cp ../../app/lib/ecj.jar processing/lib/
+#cp ../../app/lib/jna.jar processing/lib/
+#cp ../shared/revisions.txt processing/
 
-echo Extracting reference...
-unzip -q -d processing/ ../shared/reference.zip
+#echo Extracting examples...
+#unzip -q -d processing/ ../shared/examples.zip
+
+#echo Extracting reference...
+#unzip -q -d processing/ ../shared/reference.zip
 
 # add the libraries folder with source
-cp -r ../../net processing/libraries/
-cp -r ../../opengl processing/libraries/
-cp -r ../../serial processing/libraries/
-cp -r ../../video processing/libraries/
-cp -r ../../pdf processing/libraries/
-cp -r ../../dxf processing/libraries/
+#cp -r ../../net processing/libraries/
+#cp -r ../../opengl processing/libraries/
+#cp -r ../../serial processing/libraries/
+#cp -r ../../video processing/libraries/
+#cp -r ../../pdf processing/libraries/
+#cp -r ../../dxf processing/libraries/
 
 # get ds_store file (!)
-cp dist/DS_Store processing/.DS_Store
+#cp dist/DS_Store processing/.DS_Store
 
 # get package from the dist dir
-cp -pR dist/Processing.app processing/
-chmod +x processing/Processing.app/Contents/MacOS/JavaApplicationStub
+#cp -pR dist/Processing.app processing/
+#chmod +x processing/Processing.app/Contents/MacOS/JavaApplicationStub
 
 # put jar files into the resource dir, leave the rest in lib
-RES=processing/Processing.app/Contents/Resources/Java
-mkdir -p $RES
-mv processing/lib/*.jar $RES/
+#RES=processing/Processing.app/Contents/Resources/Java
+#mkdir -p $RES
+#mv processing/lib/*.jar $RES/
 
 # grab pde.jar and export from the working dir
-cp work/lib/pde.jar $RES/
-cp work/lib/core.jar processing/lib/
+#cp work/lib/pde.jar $RES/
+#cp work/lib/core.jar processing/lib/
 
 # get platform-specific goodies from the dist dir
-chmod a+x processing/Processing.app/Contents/MacOS/JavaApplicationStub
+#chmod a+x processing/Processing.app/Contents/MacOS/JavaApplicationStub
 
 # remove boogers
-find processing -name "*~" -exec rm -f {} ';'
+find work -name "*~" -exec rm -f {} ';'
 # need to leave ds store stuff cuz one of those is important
 #find processing -name ".DS_Store" -exec rm -f {} ';'
-find processing -name "._*" -exec rm -f {} ';'
-find processing -name "Thumbs.db" -exec rm -f {} ';'
+find work -name "._*" -exec rm -f {} ';'
+find work -name "Thumbs.db" -exec rm -f {} ';'
 
 # clean out the cvs entries
-find processing -name "CVS" -exec rm -rf {} ';' 2> /dev/null
-find processing -name ".cvsignore" -exec rm -rf {} ';'
-find processing -name ".svn" -exec rm -rf {} 2> /dev/null ';'
+find work -name "CVS" -exec rm -rf {} ';' 2> /dev/null
+find work -name ".cvsignore" -exec rm -rf {} ';'
+find work -name ".svn" -exec rm -rf {} 2> /dev/null ';'
 
-mv processing/Processing.app "processing/Processing $SHORT_REVISION.app"
+NICE_APP="work/Processing $SHORT_REVISION.app"
+mv work/Processing.app "$NICE_APP"
 
-NICE_FOLDER="Processing $SHORT_REVISION"
-mv processing "$NICE_FOLDER"
+#NICE_FOLDER="Processing $SHORT_REVISION"
+#mv processing "$NICE_FOLDER"
 mkdir processing-$REVISION
-mv "$NICE_FOLDER" processing-$REVISION/
+#mv "$NICE_FOLDER" processing-$REVISION/
+mv "$NICE_APP" processing-$REVISION/
 
 chmod +x mkdmg
 ./mkdmg processing-$REVISION
-rm -rf processing-$REVISION
+mv processing-$REVISION ~/.Trash/
 
 echo Done.
