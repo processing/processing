6addf8ea49984b4f2d36a70fb2f420568ff24ee7
benfry <fry@processing.org>
2008-10-21 00:24:44 +0000

    remove quicktime stuff from run.sh, now smarter about getting qtjava.zip

--- build/windows/run.sh
+++ build/windows/run.sh
@@ -1,21 +1,6 @@
 #!/bin/sh
 
-if test -f "${QTJAVA}"
-then 
-  echo "Found Quicktime 7 installation"
-else
-  QTJAVA="$WINDIR\\system32\\QTJava.zip"
-  if test -f "${QTJAVA}"
-  then
-    echo "Found Quicktime 6 at $QTJAVA"
-  else 
-    echo "Could not find QuickTime for Java,"
-    echo "you'll need to install it before running."
-    exit 1;
-  fi
-fi
-
-CLASSPATH=lib\\pde.jar\;lib\\core.jar\;lib\\jna.jar\;lib\\ecj.jar\;lib\\antlr.jar\;java\\lib\\tools.jar\;${QTJAVA}
+CLASSPATH=lib\\pde.jar\;lib\\core.jar\;lib\\jna.jar\;lib\\ecj.jar\;lib\\antlr.jar\;java\\lib\\tools.jar
 export CLASSPATH
 
 cd work && ./java/bin/java processing.app.Base
