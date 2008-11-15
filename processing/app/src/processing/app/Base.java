b907cb3e63a4d2af25009e170f2fd180d364ea12
benfry <fry@processing.org>
2008-11-15 17:16:06 +0000

    working on bug #1010, show sketch folder problems on macosx

--- processing/app/src/processing/app/Base.java
+++ processing/app/src/processing/app/Base.java
@@ -1402,8 +1402,8 @@ public class Base {
   static public void showReference(String filename) {
     File referenceFolder = Base.getContentFile("reference");
     File referenceFile = new File(referenceFolder, filename);
-    openURL(referenceFile.getAbsolutePath());
-    //openURL(Base.getContentsPath("reference" + File.separator + referenceFile));
+    //openURL(referenceFile.getAbsolutePath());
+    PApplet.open(referenceFile.getAbsolutePath());
   }
 
 
