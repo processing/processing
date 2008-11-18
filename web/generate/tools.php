186a0dfca6c00671e6a15cc5378cb3ed53aab6a8
REAS <reas@processing.org>
2008-11-18 19:22:40 +0000

    Change to tool building

--- web/generate/tools.php
+++ web/generate/tools.php
@@ -11,15 +11,15 @@ $page = new Page('Tools \\ Processing 1.0', 'Tools');
 $page->content(file_get_contents($index));
 //make_necessary_directories(BASEDIR.$tools_dir.'/images/include.php');
 writeFile($tools_dir.'/index.html', $page->out());
-//copydirr(CONTENTDIR."api_$lang/LIB_images", BASEDIR.$tools_dir.'/images');
+copydirr(CONTENTDIR."api_$lang/LIB_images", BASEDIR.$tools_dir.'/images');
 
 // copy over the files for the contributed libraries
-//copy(CONTENTDIR."static/tools.html", BASEDIR.$tools_dir.'/tools.html');
+copy(CONTENTDIR."static/tools.html", BASEDIR.$tools_dir.'/tools.html');
 
 $benchmark_end = microtime_float();
 $execution_time = round($benchmark_end - $benchmark_start, 4);
 
 ?>
 
-<h2>Library Generation Successful</h2>
+<h2>Tool Generation Successful</h2>
 <p>Generated files in <?=$execution_time?> seconds.</p>
