6d9e101f0bbd338affa7bc2a5338947dcc26fa56
benfry <fry@processing.org>
2008-11-23 15:47:33 +0000

    cleaning up layout of export application dialog

--- app/src/processing/app/Sketch.java
+++ app/src/processing/app/Sketch.java
@@ -1981,7 +1981,7 @@ public class Sketch {
         Preferences.setBoolean("export.application.stop", showStopButton.isSelected());
       }
     });
-    showStopButton.setBorder(new EmptyBorder(50, 50, 50, 50));
+    showStopButton.setBorder(new EmptyBorder(6, 13, 6, 13));
 //  indentPanel.add(showStopButton);
 //  indentPanel.setAlignmentX(Component.LEFT_ALIGNMENT);
 
@@ -1995,6 +1995,7 @@ public class Sketch {
         showStopButton.setEnabled(sal);
       }
     });
+    fullScreenButton.setBorder(new EmptyBorder(6, 13, 0, 13));
 
     JPanel optionPanel = new JPanel();
     optionPanel.setLayout(new BoxLayout(optionPanel, BoxLayout.Y_AXIS));
