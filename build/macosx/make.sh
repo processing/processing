63e872731ff3c4cf33931c0a0310b836b4232804
benfry <fry@processing.org>
2009-05-31 21:09:18 +0000

    check os version before building (bug #1179)

--- build/macosx/make.sh
+++ build/macosx/make.sh
@@ -1,5 +1,21 @@
 #!/bin/sh
 
+# http://dev.processing.org/bugs/show_bug.cgi?id=1179
+OSX_VERSION=`sw_vers | grep ProductVersion | awk '{print $2}' | awk '{print substr($0,1,4)}'`
+if [ "$OSX_VERSION" = "10.4" ]
+then
+  echo "This script uses the -X option for cp (to disable resource forks),"
+  echo "which is not supported on OS X 10.4. Please either upgrade to 10.5,"
+  echo "or modify this script to remove use of the -X switch to continue."
+  # and you will also need to remove this error message
+  exit
+else
+  if [ "$OSX_VERSION" != "10.5" ] 
+    echo "Note: This script has not been tested on this "
+    echo "release of Mac OS and may cause errors."
+  fi
+fi
+
 
 ### -- SETUP WORK DIR -------------------------------------------
 
