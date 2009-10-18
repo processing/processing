34612e14ef0ca1a3d03a1a6262a07d49f36fdae3
benfry <fry@processing.org>
2009-10-18 19:39:46 +0000

    add upper/lowercase check when naming/saving tabs/code (bug #1102)

--- app/src/processing/app/Sketch.java
+++ app/src/processing/app/Sketch.java
@@ -387,7 +387,7 @@ public class Sketch {
     // Make sure no .pde *and* no .java files with the same name already exist
     // http://dev.processing.org/bugs/show_bug.cgi?id=543
     for (SketchCode c : code) {
-      if (sanitaryName.equals(c.getPrettyName())) {
+      if (sanitaryName.equalsIgnoreCase(c.getPrettyName())) {
         Base.showMessage("Nope",
                          "A file named \"" + c.getFileName() + "\" already exists\n" +
                          "in \"" + folder.getAbsolutePath() + "\"");
@@ -757,7 +757,7 @@ public class Sketch {
     // but ignore this situation for the first tab, since it's probably being
     // resaved (with the same name) to another location/folder.
     for (int i = 1; i < codeCount; i++) {
-      if (newName.equals(code[i].getPrettyName())) {
+      if (newName.equalsIgnoreCase(code[i].getPrettyName())) {
         Base.showMessage("Nope",
                          "You can't save the sketch as \"" + newName + "\"\n" +
                          "because the sketch already has a tab with that name.");
