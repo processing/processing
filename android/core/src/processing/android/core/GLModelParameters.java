301d7ba35536f80b11bae33c795bfdcae0a709e7
codeanticode <andres.colubri@gmail.com>
2010-01-26 20:58:27 +0000

    Finished new API of GLModel

--- android/core/src/processing/android/core/GLModelParameters.java
+++ android/core/src/processing/android/core/GLModelParameters.java
@@ -4,19 +4,13 @@ public class GLModelParameters implements GLConstants, PConstants {
   GLModelParameters() {
     updateMode = STATIC;    
     drawMode= POINTS;
-    pointSize = 1;
-    lineWidth = 1;
   }
 
    GLModelParameters(GLModelParameters src) {
     updateMode = src.updateMode;    
     drawMode= src.drawMode;
-    pointSize = src.pointSize;
-    lineWidth = src.lineWidth;
   }
 
   public int updateMode;  
   public int drawMode;
-  public int pointSize;
-  public int lineWidth;
 }
