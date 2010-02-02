d83facfeb613f921f32699e2521a9acb2909afb0
codeanticode <andres.colubri@gmail.com>
2010-02-02 05:18:49 +0000

    fixed texturing bug in glmodel

--- android/core/src/processing/android/core/GLModel.java
+++ android/core/src/processing/android/core/GLModel.java
@@ -239,7 +239,7 @@ public class GLModel implements GLConstants, PConstants {
       int offset = firstUpdateIdx * 2;
       int size = (lastUpdateIdx - firstUpdateIdx + 1) * 2;
       
-      texCoords.put(updateNormalArray, offset, size);
+      texCoords.put(updateTexCoordArray, offset, size);
       texCoords.position(0);
       
       gl.glBufferSubData(GL11.GL_ARRAY_BUFFER, offset * SIZEOF_FLOAT, size * SIZEOF_FLOAT, texCoords);
