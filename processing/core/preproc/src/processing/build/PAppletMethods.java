c1c4048dd98b2c75b63a435f105a2dbb7b3609fc
benfry <fry@processing.org>
2010-02-07 15:08:34 +0000

    more work on build scripts

--- processing/core/preproc/src/processing/build/PAppletMethods.java
+++ processing/core/preproc/src/processing/build/PAppletMethods.java
@@ -1,4 +1,4 @@
-package processing.build;  
+package processing.build;
 
 import java.io.BufferedReader;
 import java.io.ByteArrayOutputStream;
@@ -16,221 +16,222 @@ import org.apache.tools.ant.Task;
 
 
 /**
- * Ant Task for copying the PImage and PGraphics methods into PApplet. 
+ * Ant Task for copying the PImage and PGraphics methods into PApplet.
  */
 public class PAppletMethods extends Task {
-  
-  private File baseDir; 
-  
-  
+
+  private File baseDir;
+
+
   public PAppletMethods() {
   }
-  
-  
+
+
   public void setDir(String dir) {
-    baseDir = new File(dir);  
+    baseDir = new File(dir);
   }
-  
-  
+
+
   public void execute() throws BuildException {
-    // Do a bunch of checks... 
+    // Do a bunch of checks...
     if (baseDir == null) {
-      throw new BuildException("dir parameter must be set!"); 
+      throw new BuildException("dir parameter must be set!");
     }
-    
-    File graphicsFile = new File(baseDir, "PGraphics.java"); 
-    File appletFile = new File(baseDir, "PApplet.java"); 
-    File imageFile = new File(baseDir, "PImage.java"); 
-    
+
+    File graphicsFile = new File(baseDir, "PGraphics.java");
+    File appletFile = new File(baseDir, "PApplet.java");
+    File imageFile = new File(baseDir, "PImage.java");
+
     if (!graphicsFile.exists() || !graphicsFile.canRead()) {
-      throw new BuildException("PGraphics file not readable: " + 
-    		  graphicsFile.getAbsolutePath()); 
+      throw new BuildException("PGraphics file not readable: " +
+                  graphicsFile.getAbsolutePath());
     }
-    
+
     if (!appletFile.exists() || !appletFile.canRead() || !appletFile.canWrite()) {
-      throw new BuildException("PApplet file not read/writeable: " + 
-    		  appletFile.getAbsolutePath()); 
+      throw new BuildException("PApplet file not read/writeable: " +
+                  appletFile.getAbsolutePath());
     }
-    
+
     if (!imageFile.exists() || !imageFile.canRead()) {
-      throw new BuildException("PImage file not readable: " + 
-    		  imageFile.getAbsolutePath()); 
+      throw new BuildException("PImage file not readable: " +
+                  imageFile.getAbsolutePath());
     }
-    
+
     // Looking good, let's do this!
-    ByteArrayOutputStream outBytes = new ByteArrayOutputStream(); 
+    ByteArrayOutputStream outBytes = new ByteArrayOutputStream();
     PrintStream out = new PrintStream(outBytes);
     StringBuffer content = new StringBuffer();
-    
+
     try{
       BufferedReader applet = createReader(appletFile);
-      String line; 
+      String line;
       while ((line = applet.readLine()) != null) {
         out.println(line);
-        content.append(line + "\n"); 
-        
+        content.append(line + "\n");
+
         if (line.indexOf("public functions for processing.core") >= 0) {
-          break; 
+          break;
         }
       }
-      
+
       // read the rest of the file and append it to the
       while ((line = applet.readLine()) != null) {
-        content.append(line + "\n"); 
+        content.append(line + "\n");
       }
-      
+
       applet.close();
       process(out, graphicsFile);
       process(out, imageFile);
-      
+
       out.println("}");
-    
+
     } catch (IOException e) {
       e.printStackTrace();
-    
+
     } catch(Exception ex) {
-      ex.printStackTrace(); 
+      ex.printStackTrace();
     }
-    out.flush(); 
-    
+    out.flush();
+
     if (content.toString().equals(outBytes.toString())) {
       System.out.println("No changes to PApplet API.");
     } else {
       System.out.println("Updating PApplet with API changes from PImage or PGraphics.");
       try {
-        PrintStream temp = new PrintStream(appletFile);  
+        PrintStream temp = new PrintStream(appletFile);
         temp.print(outBytes.toString());
-        temp.flush(); 
-        temp.close(); 
+        temp.flush();
+        temp.close();
       } catch (FileNotFoundException e) {
         e.printStackTrace();
-      }  
-    }    
+      }
+    }
   }
-  
-  
+
+
   private void process(PrintStream out, File input) throws IOException {
-    BufferedReader in = createReader(input); 
+    BufferedReader in = createReader(input);
     int comments = 0;
     String line = null;
-    
+    StringBuffer comment = new StringBuffer();
+
     while ((line = in.readLine()) != null) {
-      String decl = ""; 
-      
+      String decl = "";
+
       // Keep track of comments
       if (line.matches(Pattern.quote("/*"))) {
-        comments ++; 
+        comments++;
       }
-      
+
       if (line.matches(Pattern.quote("*/"))) {
-        comments --; 
+        comments--;
       }
-      
+
       // Ignore everything inside comments
       if (comments > 0) {
-        continue;  
+        continue;
       }
-      
-      boolean gotSomething = false; 
+
+      boolean gotSomething = false;
       boolean gotStatic = false;
-      
-      Matcher result;  
-      
+
+      Matcher result;
+
       if ((result = Pattern.compile("^\\s*public ([\\w\\[\\]]+) [a-zA-z_]+\\(.*$").matcher(line)).matches()) {
-        gotSomething = true; 
+        gotSomething = true;
       }
       else if ((result = Pattern.compile("^\\s*abstract public ([\\w\\[\\]]+) [a-zA-z_]+\\(.*$").matcher(line)).matches()) {
-        gotSomething = true; 
+        gotSomething = true;
       }
       else if ((result = Pattern.compile("^\\s*public final ([\\w\\[\\]]+) [a-zA-z_]+\\(.*$").matcher(line)).matches()) {
         gotSomething = true;
       }
       else if ((result = Pattern.compile("^\\s*static public ([\\w\\[\\]]+) [a-zA-z_]+\\(.*$").matcher(line)).matches()) {
-        gotSomething = true; 
-        gotStatic = true; 
+        gotSomething = true;
+        gotStatic = true;
       }
 
       // if function is marked "// ignore" then, uh, ignore it.
       if (gotSomething && line.indexOf("// ignore") >= 0) {
-        gotSomething = false; 
+        gotSomething = false;
       }
 
-      String returns = ""; 
+      String returns = "";
       if (gotSomething) {
         if (result.group(1).equals("void")) {
-          returns = ""; 
+          returns = "";
         } else {
-          returns = "return "; 
+          returns = "return ";
         }
 
         // remove the abstract modifier
-        line = line.replaceFirst(Pattern.quote("abstract"), " "); 
-        
+        line = line.replaceFirst(Pattern.quote("abstract"), " ");
+
         // replace semicolons with a start def
-        line = line.replaceAll(Pattern.quote(";"), " {\n"); 
-        
+        line = line.replaceAll(Pattern.quote(";"), " {\n");
+
         out.println("\n\n" + line);
-        
-        decl += line; 
+
+        decl += line;
         while(line.indexOf(')') == -1) {
-          line = in.readLine(); 
-          decl += line; 
-          line = line.replaceAll("\\;\\s*$", " {\n"); 
-          out.println(line); 
+          line = in.readLine();
+          decl += line;
+          line = line.replaceAll("\\;\\s*$", " {\n");
+          out.println(line);
         }
-        
+
         result = Pattern.compile(".*?\\s(\\S+)\\(.*?").matcher(decl);
-        result.matches(); // try to match. DON't remove this or things will stop working! 
+        result.matches(); // try to match. DON't remove this or things will stop working!
         String declName = result.group(1);
-        String gline = ""; 
-        String rline = ""; 
+        String gline = "";
+        String rline = "";
         if (gotStatic) {
-          gline = "    " + returns + "PGraphics." + declName + "("; 
+          gline = "    " + returns + "PGraphics." + declName + "(";
         } else {
           rline = "    if (recorder != null) recorder." + declName + "(";
-          gline = "    " + returns + "g." + declName + "("; 
+          gline = "    " + returns + "g." + declName + "(";
         }
-        
+
         decl = decl.replaceAll("\\s+", " "); // smush onto a single line
-        decl = decl.replaceFirst("^.*\\(", ""); 
-        decl = decl.replaceFirst("\\).*$", ""); 
-        
-        int prev = 0; 
+        decl = decl.replaceFirst("^.*\\(", "");
+        decl = decl.replaceFirst("\\).*$", "");
+
+        int prev = 0;
         String parts[] = decl.split("\\, ");
-        
+
         for (String part : parts) {
           if (!part.trim().equals("")) {
             String blargh[] = part.split(" ");
             String theArg = blargh[1].replaceAll("[\\[\\]]", "");
-            
+
             if (prev != 0) {
-              gline += ", "; 
-              rline += ", "; 
+              gline += ", ";
+              rline += ", ";
             }
-            
-            gline += theArg; 
-            rline += theArg; 
+
+            gline += theArg;
+            rline += theArg;
             prev = 1;
           }
         }
-        
-        gline += ");"; 
-        rline += ");"; 
-        
+
+        gline += ");";
+        rline += ");";
+
         if (!gotStatic && returns.equals("")) {
-          out.println(rline); 
+          out.println(rline);
         }
-        
-        out.println(gline); 
-        out.println("  }"); 
+
+        out.println(gline);
+        out.println("  }");
       }
     }
-    
-    in.close(); 
+
+    in.close();
   }
 
-  
+
   private static BufferedReader createReader(File f) throws FileNotFoundException {
-    return new BufferedReader(new InputStreamReader(new FileInputStream(f))); 
+    return new BufferedReader(new InputStreamReader(new FileInputStream(f)));
   }
 }
