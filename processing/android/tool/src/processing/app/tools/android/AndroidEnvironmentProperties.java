7589d99f8b6869e97eefe6f28c9fbe85293075c0
jdf <jdf@pobox.com>
2010-03-01 21:01:18 +0000

    Clean up informational messages.

--- processing/android/tool/src/processing/app/tools/android/AndroidEnvironmentProperties.java
+++
@@ -1,6 +0,0 @@
-package processing.app.tools.android;
-
-public interface AndroidEnvironmentProperties {
-  public static final String DEVICE_REMOVED = "android.device.removed";
-  public static final String DEVICE_ADDED = "android.device.added";
-}
