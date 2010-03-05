a3f2860ad5cb6c45a16be8a55553ec929920c2d2
jdf <jdf@pobox.com>
2010-03-05 17:48:24 +0000

    Nice thingy for watching and killing unruly processes

---
+++ android/tool/src/processing/app/tools/android/ProcessRegistry.java
@@ -0,0 +1,40 @@
+package processing.app.tools.android;
+
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
+public class ProcessRegistry {
+  private static final Set<Process> REGISTRY = Collections
+      .synchronizedSet(new HashSet<Process>());
+
+  static {
+    Runtime.getRuntime().addShutdownHook(new Thread() {
+      @Override
+      public void run() {
+        synchronized (REGISTRY) {
+          for (final Process p : REGISTRY) {
+            try {
+              System.err.println("Cleaning up rogue process " + p);
+              p.destroy();
+            } catch (final Exception drop) {
+            }
+          }
+        }
+      }
+    });
+  }
+
+  /**
+  * When starting up a process
+  * @param p
+  */
+  public static void watch(final Process p) {
+    REGISTRY.add(p);
+  }
+
+  public static void unwatch(final Process p) {
+    REGISTRY.remove(p);
+  }
+
+}
