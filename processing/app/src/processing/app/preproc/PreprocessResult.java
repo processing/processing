c6b6cfa3507ee6391669c80732757e1dbddbffa6
jdf <jdf@pobox.com>
2010-03-16 19:40:09 +0000

    Many changes to improve the decoupling and testability of the preprocessor. For example, you no longer have to set up anything on the filesystem in order to preprocess.

---
+++ processing/app/src/processing/app/preproc/PreprocessResult.java
@@ -0,0 +1,24 @@
+package processing.app.preproc;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import processing.app.debug.RunnerException;
+
+public class PreprocessResult {
+  public final int headerOffset;
+  public final String className;
+  public final List<String> extraImports;
+
+  public PreprocessResult(int headerOffset, String className,
+                          final List<String> extraImports)
+      throws RunnerException {
+    if (className == null)
+      throw new RunnerException("Could not find main class");
+    this.headerOffset = headerOffset;
+    this.className = className;
+    this.extraImports = Collections
+        .unmodifiableList(new ArrayList<String>(extraImports));
+  }
+
+}
--- processing/app/test/src/test/processing/parsing/ParserTests.java
+++ processing/app/test/src/test/processing/parsing/ParserTests.java
@@ -3,9 +3,13 @@ package test.processing.parsing;
 import org.junit.Test;
 
 public class ParserTests {
-  
+
+  static String preprocess(final String resource) {
+    return null;
+  }
+
   @Test
   public void bug5() {
-    
+
   }
 }
