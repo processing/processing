e187e1ea1ec3545856298ba1186e1dc9eca4d823
benfry <fry@processing.org>
2010-03-31 14:41:22 +0000

    starting revision 0184

--- app/src/processing/app/Base.java
+++ app/src/processing/app/Base.java
@@ -40,9 +40,9 @@ import processing.core.*;
  * files and images, etc) that comes from that.
  */
 public class Base {
-  static final int REVISION = 183;
+  static final int REVISION = 184;
   /** This might be replaced by main() if there's a lib/version.txt file. */
-  static public String VERSION_NAME = "0183";
+  static public String VERSION_NAME = "0184";
   /** Set true if this a proper release rather than a numbered revision. */
   static public boolean RELEASE = false;
 
