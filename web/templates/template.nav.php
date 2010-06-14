d99bcdad0609bbae4a6291fc0d6397b6e02c1969
REAS <reas@processing.org>
2010-06-14 16:41:41 +0000

    New exhibition nav

--- web/templates/template.nav.php
+++ web/templates/template.nav.php
@@ -54,7 +54,7 @@ function navigation($section = '')
 
     $html = "\t\t\t".'<div id="navigation">'."\n";
 
-    $id = (in_array($section, $ref) || in_array($section, $learn) || in_array($section, $exhib) || 
+    $id = (in_array($section, $ref) || in_array($section, $learn) || 
     	   in_array($section, $abo)) ? 'mainnav' : 'mainnav_noSub';   
     	    
     $html .= "\t\t\t\t".'<div class="navBar" id="'.$id.'">'."\n";
