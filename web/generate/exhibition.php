b0c8e6a4550186e6b2d38f57d97bfd6e3f5b4aaa
REAS <reas@processing.org>
2010-06-15 22:14:59 +0000

    New generator script for exhibition

--- web/generate/exhibition.php
+++ web/generate/exhibition.php
@@ -8,7 +8,6 @@ if (!defined('SUBMIT')) {
     require('../config.php');
 }
 require(GENERATEDIR.'lib/Curated.class.php');
-require(GENERATEDIR.'lib/Network.class.php');
 
 // update the files on the server via SVN
 
@@ -24,7 +23,6 @@ putenv('HOME=' . CONTENTDIR);
 //`cd /var/www/processing && /usr/local/bin/svn co svn://processing.org/trunk/web/content/`;
 
 `cd $there && /usr/local/bin/svn update curated.xml`;
-`cd $there && /usr/local/bin/svn update updates.xml`;
 
 
 /******************************************** CURATED ***/
@@ -113,66 +111,15 @@ function curated_xml($num)
     return $curated;
 }
 
-/******************************************** NETWORK ***/
-function get_network_list(&$network, $num = 30)
-{    
-    // output html
-    $html = '<dl class="network">'."\n";
-    for ($i = 0; $i < $num; $i++) {
-        $html .= $network[$i]->display();
-    }
-    $html .= "</dl>\n\n";
-    return $html;
-}
-
-function get_network_table(&$network, $start = NETWORK_FIRST_PAGE, $num = 90)
-{
-    $html = "\n<table border=\"0\" width=\"100%\">\n\t<tr>\n";
-    $j = 0;
-    for ($i = $start; $i < min(count($network), $start+$num); $i++) {
-        if ($j % 3 == 0 && $j != 0) { $html .= "\t</tr>\n\t<tr>\n"; }
-        if ($network[$i]) {
-            $html .= "\t\t<td>" . $network[$i]->display_cell() . "</td>\n";
-        } else {
-            $html .= "\t\t<td>&nbsp;</td>\n";
-        }
-        $j++;
-    }
-    return $html . "\t</tr>\n</table>\n";
-}
-
-function network_xml($num)
-{
-    // open and parse network.xml
-    $xml =& openXML('network.xml');
-    
-    // get software nodes
-    $softwares = $xml->getElementsByTagName('software');
-    $softwares = $softwares->toArray();
-    
-    // create network objects
-    $i = 1;
-    foreach ($softwares as $software) {
-        $network[] = new Network($software);
-        if ($i >= $num && $num != 'all') { break; }
-        $i++;
-    }
-    
-    return $network;   
-}
-
 if (!defined('COVER')) {
     $benchmark_start = microtime_float();
     
     // get xml
     $curated = curated_xml('all');
-    $network = network_xml('all');
     // count number of items
     $ctotal = count($curated);
-    $ntotal = count($network);
     // count number of pages needed
     $cnum_pages = ceil($ctotal / CURATED_PER_PAGE);
-    $nnum_pages = ceil($ntotal / NETWORK_PER_PAGE);
     
     // create and write the first page
     #$page = new Page('Exhibition', 'Index');
@@ -195,22 +142,7 @@ if (!defined('COVER')) {
         }
         writeFile("exhibition/".$pagename, $page->out());
     }
-    
-    for ($i = 0; $i <= $nnum_pages; $i++) {
-        $page = new Page('Network Archives', 'Network Links');
-        $page->subtemplate('template.network.archive.html');
-        $page->set('network_nav', network_nav($nnum_pages, $i+1));
-        //$page->set('network', get_network_table($network, NETWORK_PER_PAGE*($i-2)+NETWORK_FIRST_PAGE, NETWORK_PER_PAGE));
-        $page->set('network', get_network_table($network, NETWORK_PER_PAGE*$i, NETWORK_PER_PAGE));
-        //$pagename = sprintf("network_page_%d.html", $i);
-        if ($i == 0 ) {
-          $pagename = sprintf("network/index.html");
-        } else {
-          $pagename = sprintf("network/network_page_%d.html", $nnum_pages-$i);
-        }
-        writeFile("exhibition/".$pagename, $page->out());
-    }
-    
+        
     $benchmark_end = microtime_float();
     $execution_time = round($benchmark_end - $benchmark_start, 4);
     
@@ -235,36 +167,6 @@ function curated_nav($num, $current)
     	  $links[] = sprintf("<a href=\"./\">%d</a>", $i);
     	} else {
           $links[] = (($num-$i+1) == $current) ? $i : sprintf("<a href=\"curated_page_%d.html\">%d</a>", $i, $i);
-    	}
-    }
-    $html .= implode(' \\ ', $links);
-    $html .= '</p>&nbsp;';
-    return $html;
-}
-
-/*
-function curated_nav($num, $current)
-{
-  $html = '<p class="exhibition-nav">';
-  $links[] = '<a href="curated_page_1.html">1</a>';
-  for ($i = 2; $i <= $num; $i++) {
-    $links[] = ($i == $current) ? $i : sprintf("<a href=\"curated_page_%d.html\">%d</a>", $i, $i);
-  }
-  $html .= implode(' \\ ', $links);
-  $html .= '</p>&nbsp;';
-  return $html;
-}
-*/
-
-function network_nav($num, $current)
-{
-    $html = '<p class="exhibition-nav">';
-    //$links[] = '<a href="network_page_1.html">1</a>';
-    for ($i = $num; $i > 0; $i--) {
-    	if ($i == $num) {
-    	  $links[] = sprintf("<a href=\"./\">%d</a>", $i);
-    	} else {
-          $links[] = (($num-$i+1) == $current) ? $i : sprintf("<a href=\"network_page_%d.html\">%d</a>", $i, $i);
     	}
     }
     $html .= implode(' \\ ', $links);
