2084f00f3a8b58ad8fdc9da8e0294d4988a306e3
REAS <reas@processing.org>
2010-06-16 03:58:05 +0000

    Fix Exhibition navigation bug

--- web/generate/exhibition.php
+++ web/generate/exhibition.php
@@ -164,7 +164,7 @@ function curated_nav($num, $current)
     //}
     for ($i = $num; $i > 0; $i--) {
     	if ($i == $num) {
-    	  if ($i == $current+1) {
+    	  if (($num-$i+1) == $current) {
     	    $links[] = sprintf("Page: %d", $i);
     	  } else {
     	    $links[] = sprintf("Page: <a href=\"./\">%d</a>", $i);
