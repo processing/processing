46677107d1c2c77ad1a80dbe4a9d93ba790354ce
REAS <reas@processing.org>
2010-06-16 01:09:38 +0000

    Removing old language translation options

--- web/config.php
+++ web/config.php
@@ -22,19 +22,19 @@ require_once(TEMPLATEDIR.'template.php');
 // Name, Encoding, Alphabetize, URL
 $domain = "http://processing.org/reference/";
 $LANGUAGES = array(
-    'en' => array('English', 'utf-8', true, $domain),
-    'zh' => array('Chinese Traditional', 'big5', false, $domain."zh/"),
-    'zh-cn' => array('Chinese Simplified', 'GB2312', false, $domain."zh-cn/"),
-    'fr' => array('French', 'utf-8', true, $domain."fr/"),
-    'id' => array('Indonesian', 'utf-8', false, $domain."id/"),
-    'it' => array('Italian', 'utf-8', true, $domain."it/"),
-    'jp' => array('Japanese', 'Shift_JIS', false, 'http://stage.itp.tsoa.nyu.edu/~tk403/proce55ing_reference_jp/'),
-    'kn' => array('Korean', 'utf-8', false, 'http://www.nabi.or.kr/processing/'),
-    'es' => array('Spanish', 'utf-8', true, $domain."es/"),
-    'tr' => array('Turkish', 'ISO-8859-9', true, $domain."tr/"),
-    'he' => array('Hebrew', 'Windows-1255', false, ''),
-    'ru' => array('Russian', 'ISO-8859-5', false, ''),
-    'pl' => array('Polish', 'ISO-8859-2', false, '')
+    'en' => array('English', 'utf-8', true, $domain) //,
+    //'zh' => array('Chinese Traditional', 'big5', false, $domain."zh/"),
+    //'zh-cn' => array('Chinese Simplified', 'GB2312', false, $domain."zh-cn/"),
+    //'fr' => array('French', 'utf-8', true, $domain."fr/"),
+    //'id' => array('Indonesian', 'utf-8', false, $domain."id/"),
+    //'it' => array('Italian', 'utf-8', true, $domain."it/"),
+    //'jp' => array('Japanese', 'Shift_JIS', false, 'http://stage.itp.tsoa.nyu.edu/~tk403/proce55ing_reference_jp/'),
+    //'kn' => array('Korean', 'utf-8', false, 'http://www.nabi.or.kr/processing/'),
+    //'es' => array('Spanish', 'utf-8', true, $domain."es/"),
+    //'tr' => array('Turkish', 'ISO-8859-9', true, $domain."tr/"),
+    //'he' => array('Hebrew', 'Windows-1255', false, ''),
+    //'ru' => array('Russian', 'ISO-8859-5', false, ''),
+    //'pl' => array('Polish', 'ISO-8859-2', false, '')
     );
 // Langauges with finished references available to the public
 $FINISHED = array('en');
