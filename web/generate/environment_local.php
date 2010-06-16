831215db52eb835c94d2e7aa75050fef8b093d60
REAS <reas@processing.org>
2010-06-16 01:17:59 +0000

    Fixing local environment reference engine

--- web/generate/environment_local.php
+++ web/generate/environment_local.php
@@ -7,15 +7,6 @@ $benchmark_start = microtime_float();
 // arguments
 $lang = isSet($_POST['lang']) ? $_POST['lang'] : 'en';
 
-// make troubleshooting file
-$source = CONTENTDIR."/api_$lang/troubleshooting/";
-$path = DISTDIR."/troubleshooting/";
-make_necessary_directories($path."images/file");
-$page = new LocalPage('Troubleshooting', 'Troubleshooting', 'Troubleshooting', '../');
-$page->content(file_get_contents($source."index.html"));
-$page->language($lang);
-writeFile('distribution/troubleshooting/index.html', $page->out());
-copydirr($source.'/images', $path.'/images');
 
 // get translation file
 $translation = new Translation($lang);
@@ -29,18 +20,6 @@ $page->content(file_get_contents($source."index.html"));
 $page->language($lang);
 writeFile('distribution/environment/index.html', $page->out());
 copydirr($source.'/images', $path.'/images');
-
-// make export page
-$page = new LocalPage("Environment (IDE)", "Environment", "Environment", '../');
-$page->content(file_get_contents($source."export.html"));
-$page->language($lang);
-writeFile('distribution/environment/export.html', $page->out());
-
-// make platforms page
-$page = new LocalPage("Environment (IDE)", "Environment", "Environment", '../');
-$page->content(file_get_contents($source."platforms.html"));
-$page->language($lang);
-writeFile('distribution/environment/platforms.html', $page->out());
 
 
 $benchmark_end = microtime_float();
