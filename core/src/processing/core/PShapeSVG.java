ed3982d43520d6fecba5787c51fd3dd6cda8212c
benfry <fry@processing.org>
2010-06-25 15:38:02 +0000

    doc tweaks

--- core/src/processing/core/PShapeSVG.java
+++ core/src/processing/core/PShapeSVG.java
@@ -1,3 +1,26 @@
+/* -*- mode: java; c-basic-offset: 2; indent-tabs-mode: nil -*- */
+
+/*
+  Part of the Processing project - http://processing.org
+
+  Copyright (c) 2006-10 Ben Fry and Casey Reas
+  Copyright (c) 2004-06 Michael Chang
+
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License version 2.1 as published by the Free Software Foundation.
+
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General
+  Public License along with this library; if not, write to the
+  Free Software Foundation, Inc., 59 Temple Place, Suite 330,
+  Boston, MA  02111-1307  USA
+*/
+
 package processing.core;
 
 import java.awt.Paint;
@@ -121,6 +144,7 @@ import processing.xml.XMLElement;
 public class PShapeSVG extends PShape {
   XMLElement element;
 
+  /// Values between 0 and 1.
   float opacity;
   float strokeOpacity;
   float fillOpacity;
