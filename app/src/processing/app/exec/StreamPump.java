de2247d4bb3d25cda02b22ac793c39517ce21602
benfry <fry@processing.org>
2010-07-17 20:19:31 +0000

    fix for size regex, streampump edits, additional todo items

--- app/src/processing/app/exec/StreamPump.java
+++ app/src/processing/app/exec/StreamPump.java
@@ -12,7 +12,6 @@ import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ThreadFactory;
-import processing.app.EditorConsole;
 
 
 /**
