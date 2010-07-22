06ec53e828ce22d5bad3febab3d159da23c6be35
lonnen <Chris.Lonnen@gmail.com>
2010-07-22 00:39:06 +0000

    Restoring editor code from local backup with some major updates: Added a builder and nature for Processing projects. Moved preprocessing functionality from the toolbar button to the builder. Enchanced the preprocessor to mark errors on the files causing the error. Setup the nature to associate processing projects with the builder.

---
+++ editor/org.processing.editor/src/org/processing/editor/javadoc/JavaDocCompletionProcessor.java
@@ -0,0 +1,72 @@
+/*******************************************************************************
+ * Copyright (c) 2000, 2005 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+package org.processing.editor.javadoc;
+
+
+import org.eclipse.jface.text.ITextViewer;
+import org.eclipse.jface.text.contentassist.CompletionProposal;
+import org.eclipse.jface.text.contentassist.ICompletionProposal;
+import org.eclipse.jface.text.contentassist.IContentAssistProcessor;
+import org.eclipse.jface.text.contentassist.IContextInformation;
+import org.eclipse.jface.text.contentassist.IContextInformationValidator;
+
+/**
+ * Example Java doc completion processor. Should work for Processing unmodified. Do we even use Javadoc?
+ */
+public class JavaDocCompletionProcessor implements IContentAssistProcessor {
+
+	protected final static String[] fgProposals= { "@author", "@deprecated", "@exception", "@param", "@return", "@see", "@serial", "@serialData", "@serialField", "@since", "@throws", "@version" }; //$NON-NLS-12$ //$NON-NLS-11$ //$NON-NLS-10$ //$NON-NLS-7$ //$NON-NLS-9$ //$NON-NLS-8$ //$NON-NLS-6$ //$NON-NLS-5$ //$NON-NLS-4$ //$NON-NLS-3$ //$NON-NLS-2$ //$NON-NLS-1$
+
+	/* (non-Javadoc)
+	 * Method declared on IContentAssistProcessor
+	 */
+	public ICompletionProposal[] computeCompletionProposals(ITextViewer viewer, int documentOffset) {
+		ICompletionProposal[] result= new ICompletionProposal[fgProposals.length];
+		for (int i= 0; i < fgProposals.length; i++)
+			result[i]= new CompletionProposal(fgProposals[i], documentOffset, 0, fgProposals[i].length());
+		return result;
+	}
+	
+	/* (non-Javadoc)
+	 * Method declared on IContentAssistProcessor
+	 */
+	public IContextInformation[] computeContextInformation(ITextViewer viewer, int documentOffset) {
+		return null;
+	}
+	
+	/* (non-Javadoc)
+	 * Method declared on IContentAssistProcessor
+	 */
+	public char[] getCompletionProposalAutoActivationCharacters() {
+		return null;
+	}
+	
+	/* (non-Javadoc)
+	 * Method declared on IContentAssistProcessor
+	 */
+	public char[] getContextInformationAutoActivationCharacters() {
+		return null;
+	}
+	
+	/* (non-Javadoc)
+	 * Method declared on IContentAssistProcessor
+	 */
+	public IContextInformationValidator getContextInformationValidator() {
+		return null;
+	}
+	
+	/* (non-Javadoc)
+	 * Method declared on IContentAssistProcessor
+	 */
+	public String getErrorMessage() {
+		return null;
+	}
+}
