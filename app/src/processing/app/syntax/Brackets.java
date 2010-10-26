fb51fc0cb83fa33febabfcaad3d23799b07ebabf
jdf <jdf@pobox.com>
2010-10-26 10:24:51 +0000

    fix bug with bracket matching when comment starts in column 1

--- app/src/processing/app/syntax/Brackets.java
+++ app/src/processing/app/syntax/Brackets.java
@@ -117,7 +117,6 @@ public class Brackets {
     final int len = text.length();
     for (pos++; pos < len; pos++)
       if (text.charAt(pos) == '\n') {
-        pos++;
         return;
       }
   }
