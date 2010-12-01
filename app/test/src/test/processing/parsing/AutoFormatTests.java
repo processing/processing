978c76ad4737f9a4fc0211110e085c34fb6124cc
jdf <jdf@pobox.com>
2010-12-01 03:33:22 +0000

    Fix http://code.google.com/p/processing/issues/detail?id=462

--- app/test/src/test/processing/parsing/AutoFormatTests.java
+++ app/test/src/test/processing/parsing/AutoFormatTests.java
@@ -5,6 +5,7 @@ import static org.junit.Assert.fail;
 import static test.processing.parsing.ProcessingTestUtil.res;
 import java.io.File;
 import java.io.FileWriter;
+import java.io.IOException;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
@@ -17,18 +18,11 @@ public class AutoFormatTests {
 
   static void expectGood(final String id) {
     try {
-      final String program = ProcessingTestUtil.format(res(id + ".pde"));
-      final File expectedFile = res(id + ".expected");
-      if (expectedFile.exists()) {
-        final String expected = ProcessingTestUtil.read(expectedFile);
-        assertEquals(expected, program);
-      } else {
-        System.err.println("WARN: " + id
-            + " does not have an expected output file. Generating.");
-        final FileWriter sug = new FileWriter(res(id + ".expected"));
-        sug.write(ProcessingTestUtil.normalize(program));
-        sug.close();
-      }
+      final String formattedProgram = ProcessingTestUtil.format(res(id + ".pde"));
+      final File goldenFile = res(id + ".expected");
+      checkGolden(formattedProgram, goldenFile);
+      // check that the formatted text doesn't change
+      checkGolden(ProcessingTestUtil.format(formattedProgram), goldenFile);
     } catch (Exception e) {
       if (!e.equals(e.getCause()) && e.getCause() != null)
         fail(e.getCause().toString());
@@ -38,11 +32,25 @@ public class AutoFormatTests {
     }
   }
 
+  private static void checkGolden(final String expectedText,
+                                  final File goldenFile) throws IOException {
+    if (goldenFile.exists()) {
+      final String expected = ProcessingTestUtil.read(goldenFile);
+      assertEquals(expected, expectedText);
+    } else {
+      System.err.println("WARN: golden file " + goldenFile
+          + " does not exist. Generating.");
+      final FileWriter sug = new FileWriter(goldenFile);
+      sug.write(ProcessingTestUtil.normalize(expectedText));
+      sug.close();
+    }
+  }
+
   @Test
   public void bug109() {
     expectGood("bug109");
   }
-  
+
   @Test
   public void bug420() {
     expectGood("bug420");
