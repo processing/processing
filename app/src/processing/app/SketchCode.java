2586a1fe3b737956a95b4e0f4bc77f60813d5f90
benfry <fry@processing.org>
2011-04-16 16:49:28 +0000

    more tweaks for find/replace et al

--- app/src/processing/app/SketchCode.java
+++ app/src/processing/app/SketchCode.java
@@ -114,8 +114,8 @@ public class SketchCode {
   
   
   protected boolean renameTo(File what, String ext) {
-    System.out.println("renaming " + file);
-    System.out.println("      to " + what);
+//    System.out.println("renaming " + file);
+//    System.out.println("      to " + what);
     boolean success = file.renameTo(what);
     if (success) {
       this.file = what;  // necessary?
