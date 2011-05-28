b12f8c041777c78e08e34e5edc26974368fe1bea
codeanticode <andres.colubri@gmail.com>
2011-05-28 05:45:27 +0000

    Fixed issue 624 on android

--- android/core/src/processing/core/PGraphicsAndroid3D.java
+++ android/core/src/processing/core/PGraphicsAndroid3D.java
@@ -5677,11 +5677,13 @@ public class PGraphicsAndroid3D extends PGraphics {
       this.setCache(a3d, texture);
       this.setParams(a3d, params);
       
+      // The texture crop should also take into account
+      // the Y inversion, this is why using hight and -height.
       texCrop = new int[4];
       texCrop[0] = 0;
-      texCrop[1] = 0;
+      texCrop[1] = height;
       texCrop[2] = width;
-      texCrop[3] = height;     
+      texCrop[3] = -height;     
     }
   }  
   
