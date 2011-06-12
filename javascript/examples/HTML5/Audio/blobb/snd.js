307ec246f4d772d5528b8f35d952550fff48b966
fjenett <fjenett@gmail.com>
2011-06-12 11:57:28 +0000

    js mode HTML5 audio example

---
+++ javascript/examples/HTML5/Audio/blobb/snd.js
@@ -0,0 +1,41 @@
+
+window.onload = function () {
+    var audioNode = document.getElementsByTagName("audio")[0];
+    
+    function tryFindSketch () {
+        var sketch = Processing.instances[0];
+        if ( sketch == undefined )
+            return setTimeout(tryFindSketch,200); // retry
+        
+            sketch.newAudio( audioNode );
+    }
+    
+    addEvent(audioNode, 'canplaythrough', function () {
+        tryFindSketch();
+    });
+}
+
+// http://html5demos.com/
+var addEvent = (function () {
+  if (document.addEventListener) {
+    return function (el, type, fn) {
+      if (el && el.nodeName || el === window) {
+        el.addEventListener(type, fn, false);
+      } else if (el && el.length) {
+        for (var i = 0; i < el.length; i++) {
+          addEvent(el[i], type, fn);
+        }
+      }
+    };
+  } else {
+    return function (el, type, fn) {
+      if (el && el.nodeName || el === window) {
+        el.attachEvent('on' + type, function () { return fn.call(el, window.event); });
+      } else if (el && el.length) {
+        for (var i = 0; i < el.length; i++) {
+          addEvent(el[i], type, fn);
+        }
+      }
+    };
+  }
+})();
