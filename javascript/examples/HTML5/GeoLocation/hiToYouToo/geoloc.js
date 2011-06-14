381b9e10cd6c4ae06a9d54f89e532cd154c73957
fjenett <fjenett@gmail.com>
2011-06-14 21:22:23 +0000

    js mode updated geolocation example

--- javascript/examples/HTML5/GeoLocation/hiToYouToo/geoloc.js
+++ javascript/examples/HTML5/GeoLocation/hiToYouToo/geoloc.js
@@ -1,4 +1,8 @@
 
+// lazy load the google geocoding api
+document.write( "<script src=\"http://maps.google.com/maps/api/js?sensor=false\" "+
+                "type=\"text/javascript\"></script>");
+
 window.onload = function () {
     tryFindSketch();
 }
@@ -11,7 +15,8 @@ function tryFindSketch () {
     if ( navigator.geolocation ) {
         navigator.geolocation.getCurrentPosition( function(position) {
             /*success*/
-            sketch.setGeoLocation(position);
+            sketch.setGeoLocation(position.coords.latitude, position.coords.longitude);
+            tryReverseGeocoding( sketch, position.coords );
         }, function( position_error ) {
             /*error*/
             sketch.geoLocationError(position_error.message);
@@ -19,4 +24,28 @@ function tryFindSketch () {
     } else {
         sketch.geoLocationError( "Your browser does not support location services." );
     }
+}
+
+function tryReverseGeocoding ( sketch, latlng ) {
+    var geocoder = new google.maps.Geocoder();
+    geocoder.geocode({ 
+        'latLng': new google.maps.LatLng(latlng.latitude,latlng.longitude) 
+    },
+    function (data, status) {
+        if (status == google.maps.GeocoderStatus.OK) {
+            if ( data.length > 0 ) {
+                for ( var d in data ) {
+                    for ( var t in data[d].types ) {
+                        if ( data[d].types[t] == 'political' ) {
+                            sketch.setAddressString( data[d].formatted_address );
+                            return; // done
+                        }
+                    }
+                }
+                sketch.setAddressString( data[0].formatted_address ); // fallback
+            }
+        } else {
+            // ignore ..
+        }
+    });
 }
