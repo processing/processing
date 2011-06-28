96ec65d8982f12fa687e148e99da22ebe8610c28
pesckal <pkalauskas@gmail.com>
2011-06-28 23:26:49 +0000

    Category selector for library manager

--- app/src/processing/app/LibraryListPanel.java
+++ app/src/processing/app/LibraryListPanel.java
@@ -31,21 +31,27 @@ import javax.swing.text.*;
 import java.awt.event.*;
 import java.awt.font.*;
 import java.awt.*;
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.text.*;
 
 import processing.app.LibraryListing.LibraryInfo;
+import processing.app.LibraryManager.LibraryInstaller;
 
 public class LibraryListPanel extends JPanel implements Scrollable {
   
   HashMap<LibraryInfo, LibraryPanel> libPanelsByInfo;
-  LibraryListing libraries;
   private PreferredViewPositionListener preferredViewPositionListener;
+  private LibraryManager libraryManager;
+  LibraryListing libraries;
 
-  public LibraryListPanel(LibraryListing libraries) {
+  public LibraryListPanel(LibraryManager libraryManager) {
     super();
     
     preferredViewPositionListener = null;
-    this.libraries = libraries;
+    this.libraryManager = libraryManager;
+    libraries = libraryManager.getLibraryListing(null);
     
     setLayout(new GridBagLayout());
     
@@ -81,10 +87,10 @@ public class LibraryListPanel extends JPanel implements Scrollable {
 
   }
   
-  public void filterLibraries(List<String> filters) {
+  public void filterLibraries(String category, List<String> filters) {
     
     List<LibraryInfo> hiddenLibraries = libraries.getAllLibararies();
-    for (LibraryInfo lib : libraries.getFilteredLibraryList(filters)) {
+    for (LibraryInfo lib : libraries.getFilteredLibraryList(category, filters)) {
       libPanelsByInfo.get(lib).setVisible(true);
       hiddenLibraries.remove(lib);
     }
@@ -326,6 +332,9 @@ public class LibraryListPanel extends JPanel implements Scrollable {
    * Panel that expands and gives a brief overview of a library when clicked.
    */
   class LibraryPanel extends JPanel {
+    
+    private static final int BUTTON_WIDTH = 100;
+
     final String unclickedCardId = "unclicked";
 
     final String clickedCardId = "clicked";
@@ -336,7 +345,7 @@ public class LibraryListPanel extends JPanel implements Scrollable {
 
     JPanel headerPanel;
 
-    JLabel nameLabel;
+//    JTextArea briefText;
 
     JPanel infoPanel;
 
@@ -344,7 +353,7 @@ public class LibraryListPanel extends JPanel implements Scrollable {
 
     JPanel clickedCard;
 
-    JTextArea briefText;
+    JTextArea description;
 
     JButton installOrRemove;
 
@@ -377,7 +386,7 @@ public class LibraryListPanel extends JPanel implements Scrollable {
             } else if (c instanceof LibraryPanel) {
               LibraryPanel lp = (LibraryPanel) c;
               if (lp.isInfoShown) {
-                obtainedSpace = lp.briefText.getSize().height;
+                obtainedSpace = lp.description.getSize().height;
                 lp.setShowInfo(false);
                 break;
               }
@@ -402,12 +411,39 @@ public class LibraryListPanel extends JPanel implements Scrollable {
       headerPanel = new JPanel();
       headerPanel.setFocusable(true);
       headerPanel.setOpaque(false);
-      headerPanel.setLayout(new BoxLayout(headerPanel, BoxLayout.X_AXIS));
+      headerPanel.setLayout(new GridBagLayout());
 
-      nameLabel = new JLabel(libInfo.name);
-      headerPanel.add(this.nameLabel);
-      headerPanel.add(Box.createHorizontalGlue());
-
+      GridBagConstraints c = new GridBagConstraints();
+      c.gridx = 0;
+      c.gridy = 0;
+      c.weightx = 1;
+      c.anchor = GridBagConstraints.WEST;
+      JLabel nameLabel = new JLabel(libInfo.name);
+      headerPanel.add(nameLabel, c);
+      
+      c = new GridBagConstraints();
+      c.gridx = 0;
+      c.gridy = 1;
+      c.weightx = 1;
+      c.fill = GridBagConstraints.HORIZONTAL;
+      c.anchor = GridBagConstraints.WEST;
+//      briefText = new JTextArea(libInfo.brief);
+//      briefText.setHighlighter(null);
+//      briefText.setOpaque(false);
+//      briefText.setEditable(false);
+//      briefText.setLineWrap(true);
+//      briefText.setWrapStyleWord(true);
+//      Font font = briefText.getFont();
+//      font = font.deriveFont(font.getSize() * 0.85f);
+//      briefText.setFont(font);
+//      headerPanel.add(briefText, c);
+      
+      c = new GridBagConstraints();
+      c.gridx = 1;
+      c.gridy = 1;
+      c.anchor = GridBagConstraints.EAST;
+      headerPanel.add(Box.createRigidArea(new Dimension(BUTTON_WIDTH, 1)), c);
+      
       add(headerPanel);
     }
 
@@ -431,48 +467,50 @@ public class LibraryListPanel extends JPanel implements Scrollable {
       unclickedCard.setFocusable(true);
       clickedCard.setFocusable(true);
 
-      briefText = new JTextArea(libInfo.description);
+      description = new JTextArea(libInfo.description);
       installOrRemove = new JButton();
-      // installOrRemove = new JButton("Install");
-      // ActionListener installLibAction = new ActionListener() {
-      //
-      // public void actionPerformed(ActionEvent arg) {
-      // try {
-      // URL url = new URL(libraryUri.getText());
-      // // System.out.println("Installing library: " + url);
-      // File libFile = downloadLibrary(url);
-      // if (libFile != null) {
-      // installLibrary(libFile);
-      // }
-      // } catch (MalformedURLException e) {
-      // System.err.println("Malformed URL");
-      // }
-      // libraryUri.setText("");
-      // }
-      // };
-      // installOrRemove.addActionListener(installLibAction);
+      if (libInfo.isInstalled) {
+        
+      } else {
+        installOrRemove.setText("Install");
+        ActionListener installLibAction = new ActionListener() {
+
+          public void actionPerformed(ActionEvent arg) {
+            try {
+              URL url = new URL(libInfo.link);
+              
+              libraryManager.installLibraryFromUrl(url, null);
+            } catch (MalformedURLException e) {
+              Base.showWarning("Install Failed",
+                               "The link fetched from Processing.org is invalid.\n" +
+                               "You can still intall this library manually by visiting\n" +
+                               "the library's website.", e);
+            }
+          }
+        };
+        installOrRemove.addActionListener(installLibAction);
+      }
 
       clickedCard.setLayout(new BoxLayout(clickedCard, BoxLayout.X_AXIS));
 
-      briefText.setHighlighter(null);
-      briefText.setOpaque(false);
-      briefText.setEditable(false);
-      briefText.setLineWrap(true);
-      briefText.setWrapStyleWord(true);
-      Font font = this.briefText.getFont();
+      description.setHighlighter(null);
+      description.setOpaque(false);
+      description.setEditable(false);
+      description.setLineWrap(true);
+      description.setWrapStyleWord(true);
+      Font font = this.description.getFont();
       font = font.deriveFont(font.getSize() * 0.9f);
-      briefText.setFont(font);
+      description.setFont(font);
 
-      clickedCard.add(briefText);
+      clickedCard.add(description);
       clickedCard.add(Box.createHorizontalGlue());
       clickedCard.add(installOrRemove);
 
       installOrRemove.setAlignmentY(Component.BOTTOM_ALIGNMENT);
-      briefText.setAlignmentY(Component.BOTTOM_ALIGNMENT);
+      description.setAlignmentY(Component.BOTTOM_ALIGNMENT);
 
-      installOrRemove.setText("Install");
       Dimension installButtonDimensions = installOrRemove.getPreferredSize();
-      installButtonDimensions.width = 100;
+      installButtonDimensions.width = BUTTON_WIDTH;
       installOrRemove.setPreferredSize(installButtonDimensions);
 
       infoPanel.add(unclickedCard, unclickedCardId);
@@ -504,14 +542,14 @@ public class LibraryListPanel extends JPanel implements Scrollable {
      */
     public void updateSize(int width) {
       if (isInfoShown) {
-        Dimension textDimentions = briefText.getPreferredSize();
+        Dimension textDimentions = description.getPreferredSize();
         textDimentions.width = width - installOrRemove.getPreferredSize().width;
-        textDimentions.height = calculateHeight(briefText, textDimentions.width);
+        textDimentions.height = calculateHeight(description, textDimentions.width);
 
-        briefText.setMaximumSize(textDimentions);
-        briefText.setMinimumSize(textDimentions);
-        briefText.setPreferredSize(textDimentions);
-        briefText.setSize(textDimentions);
+        description.setMaximumSize(textDimentions);
+        description.setMinimumSize(textDimentions);
+        description.setPreferredSize(textDimentions);
+        description.setSize(textDimentions);
       }
 
       Dimension d;
