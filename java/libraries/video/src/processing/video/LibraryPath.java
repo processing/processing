5373ca197a63d9fcb5e6840418972737afe8906e
codeanticode <andres.colubri@gmail.com>
2011-08-18 22:31:26 +0000

    Removed remaining references to the GSVideo names which broke the library loading mechanism

--- java/libraries/video/src/processing/video/LibraryPath.java
+++ java/libraries/video/src/processing/video/LibraryPath.java
@@ -27,7 +27,7 @@ class LibraryPath {
   // This method returns the folder inside which the gstreamer library folder
   // should be located.
   String get() {
-    URL url = this.getClass().getResource("GSLibraryPath.class");
+    URL url = this.getClass().getResource("LibraryPath.class");
     if (url != null) {
       // Convert URL to string, taking care of spaces represented by the "%20"
       // string.
@@ -36,19 +36,19 @@ class LibraryPath {
 
       int n1 = -1;
       if (Platform.isWindows()) {
-        n1 = path.indexOf("/lib/GSVideo.jar"); // location of GSVideo.jar in
+        n1 = path.indexOf("/lib/video.jar"); // location of video.jar in
                                                // exported apps.
         if (n1 == -1)
-          n1 = path.indexOf("/GSVideo.jar"); // location of GSVideo.jar in
+          n1 = path.indexOf("/video.jar"); // location of video.jar in
                                              // library folder.
 
         // In Windows, path string starts with "jar file/C:/..."
         // so the substring up to the first / is removed.
         n0++;
       } else if (Platform.isMac()) {
-        // In Mac, getting the index of GSVideo.jar is enough in the case of sketches running from the PDE
+        // In Mac, getting the index of video.jar is enough in the case of sketches running from the PDE
         // as well as exported applications.
-        n1 = path.indexOf("GSVideo.jar");        
+        n1 = path.indexOf("video.jar");        
       } else if (Platform.isLinux()) {
         // TODO: what's up?
       }
