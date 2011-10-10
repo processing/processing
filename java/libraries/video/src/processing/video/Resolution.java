fa2a552c5a3864910c272c51529016e81616d85e
codeanticode <andres.colubri@gmail.com>
2011-10-10 21:11:51 +0000

    Added fps

--- java/libraries/video/src/processing/video/Resolution.java
+++ java/libraries/video/src/processing/video/Resolution.java
@@ -50,6 +50,6 @@ public class Resolution {
   }  
   
   public String toString() {
-    return width + "x" + height + ", " + PApplet.nfc(fps, 2) + " (" + fpsString +")";    
+    return width + "x" + height + ", " + PApplet.nfc(fps, 2) + "fps (" + fpsString +")";    
   }
 }
