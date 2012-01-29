1e1d7822fdce28de932ba59a3d86327cbe6d57b8
codeanticode <andres.colubri@gmail.com>
2012-01-29 21:04:08 +0000

    Corrected size of cache of point attributes in PShape3D

--- java/libraries/opengl/src/processing/opengl/PShape3D.java
+++ java/libraries/opengl/src/processing/opengl/PShape3D.java
@@ -2978,7 +2978,7 @@ public class PShape3D extends PShape {
         
         if (modifiedPointAttributes) {
           if (root.pointAttributesCache == null) { 
-            root.pointAttributesCache = new VertexCache(3);
+            root.pointAttributesCache = new VertexCache(2);
           }            
           root.pointAttributesCache.add(root.pointVertCopyOffset, tess.pointVertexCount, tess.pointAttributes);            
           modifiedPointAttributes = false;
