a5d6da04dce22086e5398acf8dd227032bab4e4f
codeanticode <andres.colubri@gmail.com>
2012-02-06 23:30:08 +0000

    PGL layer just exposes gl functions and constants

--- android/core/src/processing/core/PShader.java
+++ android/core/src/processing/core/PShader.java
@@ -151,8 +151,8 @@ public class PShader {
    * Links the shader program and validates it.
    */
   public void setup() {
-    pgl.linkProgram(programObject);
-    pgl.validateProgram(programObject);
+    pgl.glLinkProgram(programObject);
+    pgl.glValidateProgram(programObject);
     checkLogInfo("GLSL program validation: ", programObject);
     initialized = true;
   }
@@ -177,14 +177,14 @@ public class PShader {
     // textures.
     // gl.glUniform1iARB(loc, unit); 
     
-    pgl.startProgram(programObject);
+    pgl.glUseProgram(programObject);
   }
 
   /**
    * Stops the execution of the shader program.
    */
   public void stop() {
-    pgl.stopProgram();
+    pgl.glUseProgram(0);
   }    
   
   /**
@@ -194,7 +194,7 @@ public class PShader {
    * @return int
    */
   public int getAttribLocation(String name) {
-    return pgl.getAttribLocation(programObject, name);
+    return pgl.glGetAttribLocation(programObject, name);
   }
 
   /**
@@ -204,335 +204,114 @@ public class PShader {
    * @return int
    */
   public int getUniformLocation(String name) {
-    return pgl.getUniformLocation(programObject, name);
-  }
-
-  /**
-   * Sets the texture uniform name with the texture unit to use
-   * in the said uniform. 
-   * 
-   * @param name String
-   * @param unit int
-   */  
-  public void setTexUniform(String name, int unit) {
-    int loc = getUniformLocation(name);
-    if (-1 < loc) {
-      pgl.setIntUniform(loc, unit);
-    }
-  }  
-
-  public void setTexUniform(int loc, int unit) {    
-    pgl.setIntUniform(loc, unit);
-  }    
-  
-  /**
-   * Sets the texture uniform with the unit of tex is attached to
-   * at the moment of running the shader. 
-   * 
-   * @param name String
-   * @param tex GLTexture
-   */  
-  /*
-  public void setTexUniform(String name, PTexture tex) {
-    int loc = getUniformLocation(name);
-    if (-1 < loc) {
-      int tu = tex.getTextureUnit();
-      if (-1 < tu) {
-        // If tex is already bound to a texture unit, we use
-        // it as the value for the uniform.
-        gl.glUniform1iARB(loc, tu);  
-      }
-      tex.setTexUniform(loc);
-    }
-  }    
-  */
-  
-  /**
-   * Sets the int uniform with name to the given value 
-   * 
-   * @param name String
-   * @param x int
-   */  
-  public void setIntUniform(String name, int x) {
-    int loc = getUniformLocation(name);
-    if (-1 < loc) {
-      pgl.setIntUniform(loc, x);
-    }
+    return pgl.glGetUniformLocation(programObject, name);
   }
 
   public void setIntUniform(int loc, int x) {
-    pgl.setIntUniform(loc, x);
-  }  
-  
-  /**
-   * Sets the float uniform with name to the given value 
-   * 
-   * @param name String
-   * @param x float
-   */    
-  public void setFloatUniform(String name, float x) {
-    int loc = getUniformLocation(name);
-    if (-1 < loc) {
-      pgl.setFloatUniform(loc, x);
-    }
-  }
-
-  public void setFloatUniform(int loc, float x) {
-    pgl.setFloatUniform(loc, x);
-  }  
-  
-
-  public void setFloatArrayUniform(int loc, float[] x) {
-    pgl.setFloat1ArrayUniform(loc, x);
-  }
-  
-  public void setVec2ArrayUniform(int loc, float[] x) {
-    pgl.setFloat2ArrayUniform(loc, x);
-  }
-
-  public void setVec3ArrayUniform(int loc, float[] x) {
-    pgl.setFloat3ArrayUniform(loc, x);
+    pgl.glUniform1i(loc, x);
   }  
 
-  public void setVec4ArrayUniform(int loc, float[] x) {
-    pgl.setFloat4ArrayUniform(loc, x);
+  
+  public void set1FloatUniform(int loc, float x) {
+    pgl.glUniform1f(loc, x);
+  }  
+  
+  public void set2FloatUniform(int loc, float x, float y) {
+    pgl.glUniform2f(loc, x, y);
   }    
   
-  /**
-   * Sets the vec2 uniform with name to the given values. 
-   * 
-   * @param nam String
-   * @param x float
-   * @param y float          
-   */    
-  public void setVecUniform(String name, float x, float y) {
-    int loc = getUniformLocation(name);    
-    if (-1 < loc) {
-      pgl.setFloatUniform(loc, x, y);
-    }
+
+  public void set3FloatUniform(int loc, float x, float y, float z) {
+    pgl.glUniform3f(loc, x, y, z);
   }  
 
-  public void setVecUniform(int loc, float x, float y) {
-    pgl.setFloatUniform(loc, x, y);
-  }    
   
-  /**
-   * Sets the vec3 uniform with name to the given values. 
-   * 
-   * @param name String
-   * @param x float
-   * @param y float
-   * @param z float                             
-   */    
-  public void setVecUniform(String name, float x, float y, float z) {
-    int loc = getUniformLocation(name);
-    if (-1 < loc) {
-      pgl.setFloatUniform(loc, x, y, z);
-    }
-  }  
-
-  public void setVecUniform(int loc, float x, float y, float z) {
-    pgl.setFloatUniform(loc, x, y, z);
-  }  
-  
-  /**
-   * Sets the vec4 uniform with name to the given values. 
-   * 
-   * @param name String
-   * @param x float
-   * @param y float
-   * @param z float
-   * @param w float
-   */    
-  public void setVecUniform(String name, float x, float y, float z, float w) {
-    int loc = getUniformLocation(name);
-    if (-1 < loc) {
-      pgl.setFloatUniform(loc, x, y, z, w);
-    }
-  }  
-  
-  public void setVecUniform(int loc, float x, float y, float z, float w) {
-    pgl.setFloatUniform(loc, x, y, z, w);
+  public void set4FloatUniform(int loc, float x, float y, float z, float w) {
+    pgl.glUniform4f(loc, x, y, z, w);
   }   
   
-  /**
-   * Sets the mat2 uniform with name to the given values 
-   * 
-   * @param name String
-   * @param m00 float
-   *        ...
-   */      
-  public void setMatUniform(String name, float m00, float m01,
-                                         float m10, float m11) {
-    int loc = getUniformLocation(name);
-    if (-1 < loc)  {
-      pgl.setMatUniform(loc, m00, m01, 
-                             m10, m11);    
-    }
-  }  
   
-  public void setMatUniform(int loc, float m00, float m01,
-                                     float m10, float m11) {
-    pgl.setMatUniform(loc, m00, m01, 
-                           m10, m11);    
-  }    
-  
-  /**
-   * Sets the mat3 uniform with name to the given values 
-   * 
-   * @param name String
-   * @param m00 float
-   *        ...
-   */        
-  public void setMatUniform(String name, float m00, float m01, float m02,
-                                         float m10, float m11, float m12,
-                                         float m20, float m21, float m22) {
-    int loc = getUniformLocation(name);
-    if (-1 < loc)  {
-      pgl.setMatUniform(loc, m00, m01, m02, 
-                             m10, m11, m12, 
-                             m20, m21, m22);     
-    }
-  }
-
-  public void setMatUniform(int loc, float m00, float m01, float m02,
-                                     float m10, float m11, float m12,
-                                     float m20, float m21, float m22) {
-    pgl.setMatUniform(loc, m00, m01, m02, 
-                           m10, m11, m12, 
-                           m20, m21, m22);     
+  public void set1FloatVecUniform(int loc, float[] vec) {
+    pgl.glUniform1fv(loc, vec.length, vec, 0);
   }
   
-  /**
-   * Sets the mat3 uniform with name to the given values 
-   * 
-   * @param name String
-   * @param m00 float
-   *        ...
-   */        
-  public void setMatUniform(String name, float m00, float m01, float m02, float m03,
-                                         float m10, float m11, float m12, float m13,
-                                         float m20, float m21, float m22, float m23,
-                                         float m30, float m31, float m32, float m33) {
-    int loc = getUniformLocation(name);
-    if (-1 < loc)  {
-      pgl.setMatUniform(loc, m00, m01, m02, m03, 
-                             m10, m11, m12, m13, 
-                             m20, m21, m22, m23, 
-                             m30, m31, m32, m33); 
-    }
-  }  
   
-  public void setMatUniform(int loc, float m00, float m01, float m02, float m03,
-                                     float m10, float m11, float m12, float m13,
-                                     float m20, float m21, float m22, float m23,
-                                     float m30, float m31, float m32, float m33) {
-    pgl.setMatUniform(loc, m00, m01, m02, m03, 
-                           m10, m11, m12, m13, 
-                           m20, m21, m22, m23, 
-                           m30, m31, m32, m33); 
-  }    
-  
-  /**
-   * Sets the float attribute with name to the given value 
-   * 
-   * @param name String
-   * @param x float
-   */          
-  public void setFloatAttribute(String name, float x) {
-    int loc = getAttribLocation(name);
-    if (-1 < loc) {
-      pgl.setFloatAttrib(loc, x);
-    }
+  public void set2FloatVecUniform(int loc, float[] vec) {
+    pgl.glUniform2fv(loc, vec.length / 2, vec, 0);
   }
 
-  public void setFloatAttribute(int loc, float x) {
-    pgl.setFloatAttrib(loc, x);
-  }  
   
-  /**
-   * Sets the vec2 attribute with name to the given values 
-   * 
-   * @param name String
-   * @param float x
-   * @param float y          
-   */
-  public void setVecAttribute(String name, float x, float y) {
-    int loc = getAttribLocation(name);
-    if (-1 < loc) {
-      pgl.setFloatAttrib(loc, x, y);
-    }
+  public void set3FloatVecUniform(int loc, float[] vec) {
+    pgl.glUniform3fv(loc, vec.length / 3, vec, 0);
   }  
 
-  public void setVecAttribute(int loc, float x, float y) {
-    pgl.setFloatAttrib(loc, x, y);
+  
+  public void set4FloatVecUniform(int loc, float[] vec) {
+    pgl.glUniform4fv(loc, vec.length / 4, vec, 0);
   }    
   
-  /**
-   * Sets the vec3 attribute with name to the given values 
-   * 
-   * @param name String
-   * @param float x
-   * @param float y
-   * @param float z          
-   */               
-  public void setVecAttribute(String name, float x, float y, float z) {
-    int loc = getAttribLocation(name);
-    if (-1 < loc) {
-      pgl.setFloatAttrib(loc, x, y, z);
-    }
-  }  
+  
+  public void set2x2MatUniform(int loc, float[] mat) {
+    pgl.glUniformMatrix2fv(loc, 1, false, mat, 0);    
+  }    
+  
 
-  public void setVecAttribute(int loc, float x, float y, float z) {
-    pgl.setFloatAttrib(loc, x, y, z);
-  }    
-  
-  /**
-   * Sets the vec4 attribute with name to the given values 
-   * 
-   * @param name String
-   * @param float x
-   * @param float y
-   * @param float z
-   * @param float w          
-   */                 
-  public void setVecAttribute(String name, float x, float y, float z, float w) {
-    int loc = getAttribLocation(name);
-    if (-1 < loc) {
-      pgl.setFloatAttrib(loc, x, y, z, w);
-    }
+  public void set3x3MatUniform(int loc, float[] mat) {
+    pgl.glUniformMatrix3fv(loc, 1, false, mat, 0);     
   }
+  
+  
+  public void set4x4MatUniform(int loc, float[] mat) {
+    pgl.glUniformMatrix4fv(loc, 1, false, mat, 0); 
+  }    
 
-  public void setVecAttribute(int loc, float x, float y, float z, float w) {
-    pgl.setFloatAttrib(loc, x, y, z, w);
+  
+  public void set1FloatAttribute(int loc, float x) {
+    pgl.glVertexAttrib1f(loc, x);
   }  
+  
+
+  public void set2FloatAttribute(int loc, float x, float y) {
+    pgl.glVertexAttrib2f(loc, x, y);
+  }    
+  
+
+  public void set3FloatAttribute(int loc, float x, float y, float z) {
+    pgl.glVertexAttrib3f(loc, x, y, z);
+  }    
+  
+
+  public void set4FloatAttribute(int loc, float x, float y, float z, float w) {
+    pgl.glVertexAttrib4f(loc, x, y, z, w);
+  }  
+  
   
   /**
    * @param shaderSource a string containing the shader's code
    * @param filename the shader's filename, used to print error log information
    */
-  private void attachVertexShader(String shaderSource, String file) {
+  protected void attachVertexShader(String shaderSource, String file) {
     vertexShader = pg.createGLSLVertShaderObject();
     
-    pgl.setShaderSource(vertexShader, shaderSource);
-    pgl.compileShader(vertexShader);
+    pgl.glShaderSource(vertexShader, shaderSource);
+    pgl.glCompileShader(vertexShader);
     
     checkLogInfo("Vertex shader " + file + " compilation: ", vertexShader);
-    pgl.attachShader(programObject, vertexShader);    
+    pgl.glAttachShader(programObject, vertexShader);    
   }  
       
   /**
    * @param shaderSource a string containing the shader's code
    * @param filename the shader's filename, used to print error log information
    */
-  private void attachFragmentShader(String shaderSource, String file) {
+  protected void attachFragmentShader(String shaderSource, String file) {
     fragmentShader = pg.createGLSLFragShaderObject();
     
-    pgl.setShaderSource(fragmentShader, shaderSource);
-    pgl.compileShader(fragmentShader);
+    pgl.glShaderSource(fragmentShader, shaderSource);
+    pgl.glCompileShader(fragmentShader);
     
     checkLogInfo("Fragment shader " + file + " compilation: ", fragmentShader);
-    pgl.attachShader(programObject, fragmentShader);
+    pgl.glAttachShader(programObject, fragmentShader);
   }
     
   /**
@@ -560,43 +339,6 @@ public class PShader {
       pg.deleteGLSLProgramObject(programObject);
       programObject = 0;
     }
-  }
-  
-  /*
-   * 
-   * 
-   * GL.GL_BOOL_ARB GL.GL_BOOL_VEC2_ARB GL.GL_BOOL_VEC3_ARB GL.GL_BOOL_VEC4_ARB
-   * GL.GL_FLOAT GL.GL_FLOAT_MAT2_ARB GL.GL_FLOAT_MAT3_ARB GL.GL_FLOAT_MAT4_ARB
-   * GL.GL_FLOAT_VEC2_ARB GL.GL_FLOAT_VEC3_ARB GL.GL_FLOAT_VEC4_ARB GL.GL_INT
-   * GL.GL_INT_VEC2_ARB GL.GL_INT_VEC3_ARB GL.GL_INT_VEC4_ARB
-   * 
-   * gl.glUseProgramObjectARB(program);
-   * 
-   * // Build a mapping from texture parameters to texture units.
-   * texUnitMap.clear(); int count, size; int type; byte[] paramName = new
-   * byte[1024];
-   * 
-   * int texUnit = 0;
-   * 
-   * int[] iVal = { 0 }; gl.glGetObjectParameterivARB(fragmentProgram,
-   * GL.GL_OBJECT_COMPILE_STATUS_ARB, iVal, 0); count = iVal[0]; for (int i = 0;
-   * i < count; ++i) { IntBuffer iVal1 = BufferUtil.newIntBuffer(1); IntBuffer
-   * iVal2 = BufferUtil.newIntBuffer(1);
-   * 
-   * ByteBuffer paramStr = BufferUtil.newByteBuffer(1024);
-   * gl.glGetActiveUniformARB(program, i, 1024, null, iVal1, iVal2, paramStr);
-   * size = iVal1.get(); type = iVal2.get(); paramStr.get(paramName);
-   * 
-   * if ((type == GL.GL_SAMPLER_1D_ARB) || (type == GL.GL_SAMPLER_2D_ARB) ||
-   * (type == GL.GL_SAMPLER_3D_ARB) || (type == GL.GL_SAMPLER_CUBE_ARB) || (type
-   * == GL.GL_SAMPLER_1D_SHADOW_ARB) || (type == GL.GL_SAMPLER_2D_SHADOW_ARB) ||
-   * (type == GL.GL_SAMPLER_2D_RECT_ARB) || (type ==
-   * GL.GL_SAMPLER_2D_RECT_SHADOW_ARB)) { String strName = new String(paramName,
-   * 0, 1024); Integer unit = texUnit; texUnitMap.put(strName, unit); int
-   * location = gl.glGetUniformLocationARB(program, strName);
-   * gl.glUniform1iARB(location, texUnit); ++texUnit; } }
-   * 
-   * gl.glUseProgramObjectARB(0); }
-   */
+  }  
 }
 
