b973729edaf1ec10d3a88b60a9f8c6e100921f83
codeanticode <andres.colubri@gmail.com>
2012-02-10 23:22:21 +0000

    Another missing normalization

--- android/core/src/processing/core/PGraphicsAndroid3D.java
+++ android/core/src/processing/core/PGraphicsAndroid3D.java
@@ -4189,7 +4189,7 @@ public class PGraphicsAndroid3D extends PGraphics {
     float nx = dx * modelviewInv.m00 + dy * modelviewInv.m10 + dz * modelviewInv.m20;
     float ny = dx * modelviewInv.m01 + dy * modelviewInv.m11 + dz * modelviewInv.m21;
     float nz = dx * modelviewInv.m02 + dy * modelviewInv.m12 + dz * modelviewInv.m22;    
-     
+    
     float invn = 1.0f / PApplet.dist(0, 0, 0, nx, ny, nz);
     lightNormal[3 * num + 0] = invn * nx;
     lightNormal[3 * num + 1] = invn * ny;
