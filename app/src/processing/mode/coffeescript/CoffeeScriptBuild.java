bff2cf04f1dd88c7864dc8307066c8fe529953df
fjenett <fjenett@gmail.com>
2012-02-14 17:50:40 +0000

    removing coffeescript mode

--- app/src/processing/mode/coffeescript/CoffeeScriptBuild.java
+++
@@ -1,8 +0,0 @@
-package processing.mode.coffeescript;
-
-public class CoffeeScriptBuild
-{
-	public final static String TEMPLATE_FOLDER_NAME = "template_js";
-	public final static String EXPORTED_FOLDER_NAME = "applet_js";
-	public final static String TEMPLATE_FILE_NAME = "template.html";
-}
