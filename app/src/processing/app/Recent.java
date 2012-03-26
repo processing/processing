fefeb8d3f5dab2229f3262a83c25e64dc7402590
benfry <fry@processing.org>
2012-03-26 02:53:09 +0000

    continuing work on recent sketches

--- app/src/processing/app/Recent.java
+++ app/src/processing/app/Recent.java
@@ -21,13 +21,10 @@
 
 package processing.app;
 
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.event.*;
 import java.io.*;
 import java.util.ArrayList;
-
-import javax.swing.JMenu;
-import javax.swing.JMenuItem;
+import javax.swing.*;
 
 import processing.core.PApplet;
 
@@ -131,6 +128,8 @@ public class Recent {
 //      }
         save();  // write the recent file with the latest
       }
+    } else {
+//      System.out.println("sketch not null in handleOpen: " + record.getPath());
     }
     // otherwise the other handleOpen() will be called once it opens 
   }
@@ -138,6 +137,7 @@ public class Recent {
   
   /** Called by Base when a new sketch is opened. */
   synchronized void handleOpen(Editor editor, JMenu menu) {
+//    System.out.println("handleOpen editor " + editor.getSketch().getMainFilePath());
     Record newRec = new Record(editor, editor.getSketch());
     // If this is already in the menu, remove it
     int index = findRecord(newRec);
@@ -220,90 +220,4 @@ public class Recent {
       return getPath().equals(r.getPath());
     }
   }
-
-
-//  class Record {
-//    // path to the main .pde file for the sketch 
-//    String path;
-//    // placement of the window
-////    int windowX, windowY, windowW, windowH;
-//    Rectangle editorBounds;
-//    // width/height of the screen on which this window was placed
-////    int displayW, displayH;
-//    String deviceName;
-//    Rectangle deviceBounds;
-//
-//    
-//    Record(BufferedReader reader) throws IOException {
-//      String line = reader.readLine();
-//      String[] pieces = PApplet.split(line, '\t');
-//      path = pieces[0];
-//      
-////      windowX = Integer.parseInt(pieces[1]);
-////      windowY = Integer.parseInt(pieces[2]);
-////      windowW = Integer.parseInt(pieces[3]);
-////      windowH = Integer.parseInt(pieces[4]);
-//      
-////      displayW = Integer.parseInt(pieces[5]);
-////      displayH = Integer.parseInt(pieces[6]);
-//    }
-//
-//
-//    void update(Editor editor) {
-//      path = editor.getSketch().getMainFilePath();
-//      editorBounds = editor.getBounds();
-//      GraphicsConfiguration config = editor.getGraphicsConfiguration();
-//      GraphicsDevice device = config.getDevice();
-//      deviceBounds = config.getBounds();
-//      deviceName = device.getIDstring();
-//    }
-//    
-//    
-//    void write(PrintWriter writer) {
-//      writer.print(path);
-//      writer.print('\t');
-//      writeRect(writer, editorBounds);
-//      writer.print('\t');
-//      writer.print(deviceName);
-//      writer.print('\t');
-//      writeRect(writer, deviceBounds);
-//    }
-//
-//
-//    void writeRect(PrintWriter writer, Rectangle rect) {
-//      writer.print(rect.x);
-//      writer.print('\t');
-//      writer.print(rect.y);
-//      writer.print('\t');
-//      writer.print(rect.width);
-//      writer.print('\t');
-//      writer.print(rect.height);
-//    }
-//  }
-  
-//  Dimension screen = Toolkit.getDefaultToolkit().getScreenSize();
-//  Preferences.setInteger("last.screen.width", screen.width);
-//  Preferences.setInteger("last.screen.height", screen.height);
-//
-//  String untitledPath = untitledFolder.getAbsolutePath();
-//
-//  // Save the sketch path and window placement for each open sketch
-//  int index = 0;
-//  for (Editor editor : editors) {
-//    String path = editor.getSketch().getMainFilePath();
-//    // In case of a crash, save untitled sketches if they contain changes.
-//    // (Added this for release 0158, may not be a good idea.)
-//    if (path.startsWith(untitledPath) &&
-//        !editor.getSketch().isModified()) {
-//      continue;
-//    }
-//    Preferences.set("last.sketch" + index + ".path", path);
-//
-//    int[] location = editor.getPlacement();
-//    String locationStr = PApplet.join(PApplet.str(location), ",");
-//    Preferences.set("last.sketch" + index + ".location", locationStr);
-//    index++;
-//  }
-//  Preferences.setInteger("last.sketch.count", index);
-//  Preferences.set("last.sketch.mode", defaultMode.getClass().getName());
 }
