c071b695346d1ee04c7e2ce0356162fd7435e41a
codeanticode <andres.colubri@gmail.com>
2012-03-29 19:32:39 +0000

    Fixed invocation of updatePixelsImpl() in PImage

--- android/core/src/processing/opengl/PTexture.java
+++ android/core/src/processing/opengl/PTexture.java
@@ -769,7 +769,7 @@ public class PTexture implements PConstants {
     pgl.glTexImage2D(glTarget, 0, glFormat, glWidth, glHeight, 0, PGL.GL_RGBA, PGL.GL_UNSIGNED_BYTE, null);
     
     // Makes sure that the texture buffer in video memory doesn't contain any garbage.
-    pgl.initTexture(glTarget, width, height, PGL.GL_RGBA, PGL.GL_UNSIGNED_BYTE);
+    pgl.initTexture(glTarget, PGL.GL_RGBA, width, height);
     
     pgl.glBindTexture(glTarget, 0);
     pgl.disableTexturing(glTarget);
