c7e1cad5976a698e4bd13ef4ce185f1b2142a00c
codeanticode <andres.colubri@gmail.com>
2012-03-29 21:15:48 +0000

    Boundary checking in updatePixelsImpl()

--- core/src/processing/core/PImage.java
+++ core/src/processing/core/PImage.java
@@ -532,25 +532,24 @@ public class PImage implements PConstants, Cloneable {
     int y2 = y + h;
 
     if (!modified) {
-      mx1 = x;
-      mx2 = x2;
-      my1 = y;
-      my2 = y2;
+      mx1 = PApplet.max(0, x);
+      mx2 = PApplet.min(width - 1, x2);
+      my1 = PApplet.max(0, y);
+      my2 = PApplet.min(height - 1, y2);
       modified = true;
 
     } else {
-      if (x < mx1) mx1 = x;
-      if (x > mx2) mx2 = x;
-      if (y < my1) my1 = y;
+      if (x < mx1) mx1 = PApplet.max(0, x);
+      if (x > mx2) mx2 = PApplet.min(width - 1, x);
+      if (y < my1) my1 = PApplet.max(0, y);
       if (y > my2) my2 = y;
 
-      if (x2 < mx1) mx1 = x2;
-      if (x2 > mx2) mx2 = x2;
-      if (y2 < my1) my1 = y2;
-      if (y2 > my2) my2 = y2;
+      if (x2 < mx1) mx1 = PApplet.max(0, x2);
+      if (x2 > mx2) mx2 = PApplet.min(width - 1, x2);
+      if (y2 < my1) my1 = PApplet.max(0, y2);
+      if (y2 > my2) my2 = PApplet.min(height - 1, y2);
     }
   }
-
 
 
   //////////////////////////////////////////////////////////////
