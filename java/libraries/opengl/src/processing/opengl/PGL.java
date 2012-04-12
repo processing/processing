bac561d598fd72db05b447911ce09e3a8d0d61ec
codeanticode <andres.colubri@gmail.com>
2012-04-12 13:33:30 +0000

    Moved javaToNativeARGB() to PGL, fixed color update in PShape3D.

--- java/libraries/opengl/src/processing/opengl/PGL.java
+++ java/libraries/opengl/src/processing/opengl/PGL.java
@@ -1476,6 +1476,19 @@ public class PGL {
   }
 
 
+  static public int javaToNativeARGB(int color) {
+    if (BIG_ENDIAN) {
+      return ((color >> 24) & 0xff) | 
+             ((color << 8) & 0xffffff00);
+    } else {
+      return (color & 0xff000000) | 
+             ((color << 16) & 0xff0000) | 
+             (color & 0xff00) | 
+             ((color >> 16) & 0xff);
+    }
+  }
+  
+  
   /**
    * Convert native OpenGL format into palatable ARGB format. This function
    * leaves alone (ignores) the alpha component. Also flips the image
