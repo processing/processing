c0f47d82c224dec6407356a02e1034b7025f6dda
codeanticode <andres.colubri@gmail.com>
2012-05-10 19:27:03 +0000

    disabled perspective correction for lines in P2D and handling loadPixels() call outside beginDraw/endDraw

--- java/libraries/opengl/src/processing/opengl/PGraphics2D.java
+++ java/libraries/opengl/src/processing/opengl/PGraphics2D.java
@@ -28,6 +28,7 @@ public class PGraphics2D extends PGraphicsOpenGL {
   public PGraphics2D() {
     super();
     hints[ENABLE_ACCURATE_2D] = true;
+    hints[ENABLE_PERSPECTIVE_CORRECTED_LINES] = false;
   }
   
 }
