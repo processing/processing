e66a93faa3bd4b688c8ed38ad7a13cccec960ee3
codeanticode <andres.colubri@gmail.com>
2012-05-30 18:36:24 +0000

    Put new shader files in

---
+++ android/core/src/processing/opengl/PointShaderFrag.glsl
@@ -0,0 +1,30 @@
+/*
+  Part of the Processing project - http://processing.org
+
+  Copyright (c) 2011-12 Ben Fry and Casey Reas
+
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License version 2.1 as published by the Free Software Foundation.
+
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General
+  Public License along with this library; if not, write to the
+  Free Software Foundation, Inc., 59 Temple Place, Suite 330,
+  Boston, MA  02111-1307  USA
+ */
+
+#ifdef GL_ES
+precision mediump float;
+precision mediump int;
+#endif
+
+varying vec4 vertColor;
+
+void main() {
+  gl_FragColor = vertColor;
+}
