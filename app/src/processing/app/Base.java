dac58915b4990a8028350395a6a6b1e3fcc6d58a
benfry <fry@processing.org>
2012-07-22 16:01:40 +0000

    dealing with missing files on classpath

--- app/src/processing/app/Base.java
+++ app/src/processing/app/Base.java
@@ -234,7 +234,7 @@ public class Base {
       Class.forName("com.sun.jdi.VirtualMachine");
     } catch (ClassNotFoundException cnfe) {
       //String cp = System.getProperty("java.class.path").replace(File.pathSeparatorChar, '\n');
-      String cp = System.getProperty("boot.class.path").replace(File.pathSeparatorChar, '\n');
+      String cp = System.getProperty("sun.boot.class.path").replace(File.pathSeparatorChar, '\n');
 
       Base.openURL("http://wiki.processing.org/w/Supported_Platforms");
       Base.showError("Please install JDK 1.6 or later",
