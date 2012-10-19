bb65620b23c47ac571ee3fdcae43fbc4a9e9a6e1
benfry <fry@processing.org>
2012-10-19 19:59:52 +0000

    default sketchbook was user.dir instead of user.home... oops (issue #1286)

--- app/src/processing/app/Platform.java
+++ app/src/processing/app/Platform.java
@@ -101,11 +101,11 @@ public class Platform {
 
 
   /**
-   * @return if not overridden, a folder named "sketchbook" in user.dir.
+   * @return if not overridden, a folder named "sketchbook" in user.home.
    * @throws Exception so that subclasses can throw a fit
    */
   public File getDefaultSketchbookFolder() throws Exception {
-    return new File(System.getProperty("user.dir"), "sketchbook");
+    return new File(System.getProperty("user.home"), "sketchbook");
   }
 
 
