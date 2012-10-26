de4ea6328820012b33144fffe1ce567bdb1a9a2a
Casey Reas <reas@reas.com>
2012-10-26 19:45:05 +0000

    Basic reference updates for 2b6

--- core/src/processing/core/PGraphics.java
+++ core/src/processing/core/PGraphics.java
@@ -1190,7 +1190,7 @@ public class PGraphics extends PImage implements PConstants {
    *
    * Sets the coordinate space for texture mapping. There are two options,
    * IMAGE, which refers to the actual coordinates of the image, and
-   * NORMALIZED, which refers to a normalized space of values ranging from 0
+   * NORMAL, which refers to a normalized space of values ranging from 0
    * to 1. The default mode is IMAGE. In IMAGE, if an image is 100 x 200
    * pixels, mapping the image onto the entire size of a quad would require
    * the points (0,0) (0,100) (100,200) (0,200). The same mapping in
@@ -1198,7 +1198,7 @@ public class PGraphics extends PImage implements PConstants {
    *
    * ( end auto-generated )
    * @webref image:textures
-   * @param mode either IMAGE or NORMALIZED
+   * @param mode either IMAGE or NORMAL
    * @see PGraphics#texture(PImage)
    */
   public void textureMode(int mode) {
