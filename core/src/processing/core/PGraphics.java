6644ac4735c03e8ba5455d5d529a7e044d8c41cc
benfry <fry@processing.org>
2012-11-20 19:31:53 +0000

    wrong type parameter

--- core/src/processing/core/PGraphics.java
+++ core/src/processing/core/PGraphics.java
@@ -7653,7 +7653,7 @@ public class PGraphics extends PImage implements PConstants {
   /**
    * Version of showWarning() that takes a parsed String.
    */
-  static public void showWarning(String msg, String... args) {  // ignore
+  static public void showWarning(String msg, Object... args) {  // ignore
     showWarning(String.format(msg, args));
   }
 
