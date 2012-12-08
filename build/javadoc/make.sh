26a450d8310c605127542978e340f45325e81760
benfry <fry@processing.org>
2012-12-08 17:35:57 +0000

    updated javadoc

--- build/javadoc/make.sh
+++ build/javadoc/make.sh
@@ -2,7 +2,9 @@
 
 javadoc -public -notimestamp -d core \
     ../../core/src/processing/core/*.java \
-    ../../core/src/processing/xml/*.java
+    ../../core/src/processing/data/*.java \
+    ../../core/src/processing/event/*.java \
+    ../../core/src/processing/opengl/*.java \
 
 # These have to be done in a certain order... Most classes need to know about
 # core, and SVG needs to have the XML library opened earler. I'm probably not
@@ -11,7 +13,9 @@ javadoc -public -notimestamp -d core \
 javadoc -public -notimestamp -d everything \
     -classpath ../../app/lib/antlr.jar:../../app/lib/jna.jar:../../serial/library/RXTXcomm.jar:../../opengl/library/jogl.jar:../../pdf/library/itext.jar:../../app/lib/ecj.jar \
     ../../core/src/processing/core/*.java \
-    ../../core/src/processing/xml/*.java \
+    ../../core/src/processing/data/*.java \
+    ../../core/src/processing/event/*.java \
+    ../../core/src/processing/opengl/*.java \
     ../../app/src/antlr/*.java \
     ../../app/src/processing/app/*.java \
     ../../app/src/processing/app/exec/*.java \
