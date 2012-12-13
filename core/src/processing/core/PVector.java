aea81b1fdd39d7c79e08b1a8377d5df31cea5850
alignedleft <shm@alignedleft.com>
2012-12-13 21:16:22 +0000

    Removed a @see reference from PVector.lerp() as it wasn't being parsed by the ref generator script correctly

--- core/src/processing/core/PVector.java
+++ core/src/processing/core/PVector.java
@@ -925,7 +925,6 @@ public class PVector implements Serializable {
    * @brief Linear interpolate the vector to another vector
    * @param v the vector to lerp to
    * @param amt  The amount of interpolation; some value between 0.0 (old vector) and 1.0 (new vector). 0.1 is very near the new vector. 0.5 is halfway in between.
-   * @see PApplet#lerp(float, float, float)
    */
   public void lerp(PVector v, float amt) {
     x = PApplet.lerp(x,v.x,amt);
