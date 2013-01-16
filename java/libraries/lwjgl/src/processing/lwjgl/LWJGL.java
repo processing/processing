225cd81d492ef8e1644aa8be4143a32eb8916623
codeanticode <andres.colubri@gmail.com>
2013-01-16 20:30:33 +0000

    Added LWJGL interface to hold the P2D/P3D constants

---
+++ java/libraries/lwjgl/src/processing/lwjgl/LWJGL.java
@@ -0,0 +1,6 @@
+package processing.lwjgl;
+
+public interface LWJGL {
+  static final String P2D    = "processing.lwjgl.PGraphics2D";
+  static final String P3D    = "processing.lwjgl.PGraphics3D";  
+}
