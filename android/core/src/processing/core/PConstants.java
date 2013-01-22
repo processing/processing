57e588a7807176464bd9304eca397db908ac1226
benfry <fry@processing.org>
2013-01-22 02:55:43 +0000

    add TAU as alias for TWO_PI (issue #1488)

--- android/core/src/processing/core/PConstants.java
+++ android/core/src/processing/core/PConstants.java
@@ -101,6 +101,7 @@ public interface PConstants {
   static final float THIRD_PI   = PI / 3.0f;
   static final float QUARTER_PI = PI / 4.0f;
   static final float TWO_PI     = PI * 2.0f;
+  static final float TAU        = PI * 2.0f;
 
   static final float DEG_TO_RAD = PI/180.0f;
   static final float RAD_TO_DEG = 180.0f/PI;
