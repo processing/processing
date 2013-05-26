6be1b1ba8ebe4b68a5fdad766d7b9056106242dc
Benjamin Maus <benjamin@feld.is>
2013-05-26 14:19:29 +0200

    Fixes processing/processing#1810 where a png data would be written instead of jpeg data.

--- core/src/processing/core/PImage.java
+++ core/src/processing/core/PImage.java
@@ -3109,6 +3109,7 @@ public class PImage implements PConstants, Cloneable {
       ImageWriter writer = null;
       ImageWriteParam param = null;
       IIOMetadata metadata = null;
+      
       if (extension.equals("jpg") || extension.equals("jpeg")) {
         if ((writer = imageioWriter("jpeg")) != null) {
           // Set JPEG quality to 90% with baseline optimization. Setting this
@@ -3120,12 +3121,16 @@ public class PImage implements PConstants, Cloneable {
           param.setCompressionQuality(0.9f);
         }
       }
-      if ((writer = imageioWriter("png")) != null) {
-        param = writer.getDefaultWriteParam();
-        if (false) {
-          metadata = imageioDPI(writer, param, 100);
+
+      if (extension.equals("png")) {
+        if ((writer = imageioWriter("png")) != null) {
+          param = writer.getDefaultWriteParam();
+          if (false) {
+            metadata = imageioDPI(writer, param, 100);
+          }
         }
       }
+
       if (writer != null) {
         BufferedOutputStream output =
           new BufferedOutputStream(PApplet.createOutput(file));
