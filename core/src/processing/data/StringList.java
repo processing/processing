d4066e2fd5ad3d349a3e3aaf32570e6843a89143
REAS <reas@reas.com>
2013-05-28 11:51:41 -0700

    Reference additions for Lists and Dicts

--- core/src/processing/data/StringList.java
+++ core/src/processing/data/StringList.java
@@ -7,6 +7,13 @@ import java.util.Random;
 import processing.core.PApplet;
 
 /**
+ * Helper class for a list of Strings. Lists are designed to have some of the 
+ * features of ArrayLists, but to maintain the simplicity and efficiency of
+ * working with arrays. 
+ * 
+ * Functions like sort() and shuffle() always act on the list itself. To get 
+ * a sorted copy, use list.copy().sort().
+ * 
  * @webref data:composite
  */
 public class StringList implements Iterable<String> {
