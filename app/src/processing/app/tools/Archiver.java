bd7bbba1e818f609bfcc07e75b6e18154821cd0d
Kentaro Fukuchi <kentaro@fukuchi.org>
2013-06-06 06:14:59 +0900

    Pass null as a caller's data.

--- app/src/processing/app/tools/Archiver.java
+++ app/src/processing/app/tools/Archiver.java
@@ -95,7 +95,7 @@ public class Archiver implements Tool {
     } while (newbie.exists());
 
     // open up a prompt for where to save this fella
-    PApplet.selectOutput("Archive sketch as...", "fileSelected", newbie, this, editor);
+    PApplet.selectOutput("Archive sketch as...", "fileSelected", null, newbie, this, editor);
   }
 
 
