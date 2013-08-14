39548eaf5d7096b1295a33a0226408ca8da33a3b
codeanticode <andres.colubri@gmail.com>
2013-08-14 14:11:19 -0400

    fixes issue #1994

--- core/src/processing/opengl/PShader.java
+++ core/src/processing/opengl/PShader.java
@@ -563,7 +563,7 @@ public class PShader {
           pgl.uniform3i(loc, v[0], v[1], v[2]);
         } else if (val.type == UniformValue.INT4) {
           int[] v = ((int[])val.value);
-          pgl.uniform4i(loc, v[0], v[1], v[2], v[4]);
+          pgl.uniform4i(loc, v[0], v[1], v[2], v[3]);
         } else if (val.type == UniformValue.FLOAT1) {
           float[] v = ((float[])val.value);
           pgl.uniform1f(loc, v[0]);
