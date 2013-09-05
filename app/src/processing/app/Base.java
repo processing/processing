95a466ada523579b5c16879c84931fcad802adf5
Ben Fry <fry@processing.org>
2013-09-05 17:17:41 -0400

    starting the next release

--- app/src/processing/app/Base.java
+++ app/src/processing/app/Base.java
@@ -45,10 +45,10 @@ import processing.core.*;
  */
 public class Base {
   // Added accessors for 0218 because the UpdateCheck class was not properly
-  // updating the values, because javac was inlining the static final values.
-  static private final int REVISION = 221;
+  // updating the values, due to javac inlining the static final values.
+  static private final int REVISION = 222;
   /** This might be replaced by main() if there's a lib/version.txt file. */
-  static private String VERSION_NAME = "0221"; //$NON-NLS-1$
+  static private String VERSION_NAME = "0222"; //$NON-NLS-1$
   /** Set true if this a proper release rather than a numbered revision. */
 //  static private boolean RELEASE = false;
 
