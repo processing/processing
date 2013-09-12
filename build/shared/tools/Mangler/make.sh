77dd894f479792daeeafeec0a6564c2f75d56707
Ben Fry <fry@processing.org>
2013-09-12 18:00:58 -0400

    misc fixes, todo updates

--- build/shared/tools/Mangler/make.sh
+++ build/shared/tools/Mangler/make.sh
@@ -3,7 +3,7 @@
 # The pde.jar file may be buried inside the .app file on Mac OS X.
 PDE=`find ../.. -name pde.jar`
 
-javac -target 1.5 \
+javac -target 1.6 \
   -cp "../../lib/core.jar:$PDE" \
   -d bin \
   src/Mangler.java
