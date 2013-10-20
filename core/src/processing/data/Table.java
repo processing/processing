eabee7e4cf05a6ba807d7c5a1d723b6e6ff785d0
Ben Fry <fry@processing.org>
2013-10-20 10:09:30 -0400

    fix random insertRow() issue

--- core/src/processing/data/Table.java
+++ core/src/processing/data/Table.java
@@ -1859,35 +1859,35 @@ public class Table {
         case INT: {
           int[] intTemp = new int[rowCount+1];
           System.arraycopy(columns[col], 0, intTemp, 0, insert);
-          System.arraycopy(columns[col], insert, intTemp, insert+1, (rowCount - insert) + 1);
+          System.arraycopy(columns[col], insert, intTemp, insert+1, rowCount - insert);
           columns[col] = intTemp;
           break;
         }
         case LONG: {
           long[] longTemp = new long[rowCount+1];
           System.arraycopy(columns[col], 0, longTemp, 0, insert);
-          System.arraycopy(columns[col], insert, longTemp, insert+1, (rowCount - insert) + 1);
+          System.arraycopy(columns[col], insert, longTemp, insert+1, rowCount - insert);
           columns[col] = longTemp;
           break;
         }
         case FLOAT: {
           float[] floatTemp = new float[rowCount+1];
           System.arraycopy(columns[col], 0, floatTemp, 0, insert);
-          System.arraycopy(columns[col], insert, floatTemp, insert+1, (rowCount - insert) + 1);
+          System.arraycopy(columns[col], insert, floatTemp, insert+1, rowCount - insert);
           columns[col] = floatTemp;
           break;
         }
         case DOUBLE: {
           double[] doubleTemp = new double[rowCount+1];
           System.arraycopy(columns[col], 0, doubleTemp, 0, insert);
-          System.arraycopy(columns[col], insert, doubleTemp, insert+1, (rowCount - insert) + 1);
+          System.arraycopy(columns[col], insert, doubleTemp, insert+1, rowCount - insert);
           columns[col] = doubleTemp;
           break;
         }
         case STRING: {
           String[] stringTemp = new String[rowCount+1];
           System.arraycopy(columns[col], 0, stringTemp, 0, insert);
-          System.arraycopy(columns[col], insert, stringTemp, insert+1, (rowCount - insert) + 1);
+          System.arraycopy(columns[col], insert, stringTemp, insert+1, rowCount - insert);
           columns[col] = stringTemp;
           break;
         }
