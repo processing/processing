ca28d6f410ec740bcdd0d8f45a33f5c09951756d
Ben Fry <fry@processing.org>
2014-04-14 17:27:54 -0400

    additional fix for #1561

--- app/src/processing/app/contrib/ContributionPanel.java
+++ app/src/processing/app/contrib/ContributionPanel.java
@@ -122,6 +122,7 @@ class ContributionPanel extends JPanel {
           updateButton.setEnabled(false);
           installRemoveButton.setEnabled(false);
           installProgressBar.setVisible(true);
+          installProgressBar.setIndeterminate(true);
 
           ((LocalContribution) contrib).removeContribution(listPanel.contribManager.editor,
                                                            new JProgressMonitor(installProgressBar) {
