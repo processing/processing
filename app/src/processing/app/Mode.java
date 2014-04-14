51e1c313d7ab5d08089104d9472aed1fd2396fe6
Jonathan Feinberg <jdf@pobox.com>
2014-04-14 15:53:19 -0400

    Permit Modes to specify different default file extension for auxilliary (not main pde) files created in new tabs.

--- app/src/processing/app/Mode.java
+++ app/src/processing/app/Mode.java
@@ -1046,6 +1046,18 @@ public abstract class Mode {
   abstract public String getDefaultExtension();
 
 
+  /**
+   * Returns the appropriate file extension to use for auxilliary source files in a sketch.
+   * For example, in a Java-mode sketch, auxilliary files should be name "Foo.java"; in
+   * Python mode, they should be named "foo.py".
+   * 
+   * <p>Modes that do not override this function will get the default behavior of returning the
+   * default extension.
+   */
+  public String getModuleExtension() {
+    return getDefaultExtension();
+  }
+
 
   /**
    * Returns a String[] array of proper extensions.
