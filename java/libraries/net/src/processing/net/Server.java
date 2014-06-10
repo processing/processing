e2c9a52dba9017465cf73279072da049d7a5deb0
Kyle Feuz <kyle.feuz@aggiemail.usu.edu>
2014-06-09 23:06:17 -0700

    Fixes NPE when writing to disconnected client, Issue #2577

--- java/libraries/net/src/processing/net/Server.java
+++ java/libraries/net/src/processing/net/Server.java
@@ -309,8 +309,8 @@ public class Server implements Runnable {
   public void write(int data) {  // will also cover char
     int index = 0;
     while (index < clientCount) {
-      clients[index].write(data);
       if (clients[index].active()) {
+        clients[index].write(data);
         index++;
       } else {
         removeIndex(index);
@@ -322,8 +322,8 @@ public class Server implements Runnable {
   public void write(byte data[]) {
     int index = 0;
     while (index < clientCount) {
-      clients[index].write(data);
       if (clients[index].active()) {
+        clients[index].write(data);
         index++;
       } else {
         removeIndex(index);
@@ -335,8 +335,8 @@ public class Server implements Runnable {
   public void write(String data) {
     int index = 0;
     while (index < clientCount) {
-      clients[index].write(data);
       if (clients[index].active()) {
+        clients[index].write(data);
         index++;
       } else {
         removeIndex(index);
