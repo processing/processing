01c99f416f5f828a79633723557dd3ff358b3ba8
patrick <pvrs12@gmail.com>
2014-07-07 10:20:27 -0400

    Re-added in commit for Pull request

--- app/src/processing/app/Editor.java
+++ app/src/processing/app/Editor.java
@@ -32,6 +32,7 @@ import java.awt.datatransfer.*;
 import java.awt.event.*;
 import java.awt.print.*;
 import java.io.*;
+import java.nio.file.*;
 import java.util.*;
 import java.util.List;
 import java.util.Timer;
@@ -2330,6 +2331,8 @@ public abstract class Editor extends JFrame implements RunnerListener {
       Base.showWarning("Error", "Could not create the sketch.", e);
       return false;
     }
+    initFileChangeListener();
+    
     header.rebuild();
     updateTitle();
     // Disable untitled setting from previous document, if any
@@ -2348,6 +2351,80 @@ public abstract class Editor extends JFrame implements RunnerListener {
 //      statusError(e);
 //      return false;
 //    }
+  }
+  
+
+  //true when the file is saved, before the next scan for updates
+  private boolean saved;
+
+  private void initFileChangeListener() {
+    WatchKey key = null;
+    try {
+      WatchService watchService = FileSystems.getDefault().newWatchService();
+      key = sketch.getFolder().toPath()
+        .register(watchService,
+//                  StandardWatchEventKinds.ENTRY_CREATE,
+//                  StandardWatchEventKinds.ENTRY_DELETE,
+                  StandardWatchEventKinds.ENTRY_MODIFY);
+    } catch (IOException e) {
+      //registring the watch failed, ignore it
+    }
+
+    final WatchKey finKey = key;
+
+    if (key != null) {
+      //the key can now be polled for changes in the files
+      Thread th = new Thread(new Runnable() {
+        @Override
+        public void run() {
+          //polls for changes to the directory
+          while (true) {
+            try {
+              //check every 5s if the file has changed
+              Thread.sleep(5000);
+            } catch (InterruptedException e) {
+            }
+            List<WatchEvent<?>> events = finKey.pollEvents();
+            processFileEvents(events);
+
+            //if the directory was deleted, then quit the loop
+            if (!finKey.isValid()) {
+              break;
+            }
+          }
+        }
+      });
+      th.start();
+    }
+  }
+
+  /**
+   * called when a file is changed
+   * 
+   * @param events
+   *          the list of events that have occured in the registered folder
+   *          (sketch.getFolder())
+   */
+  private void processFileEvents(List<WatchEvent<?>> events) {
+    for (WatchEvent<?> e : events) {
+      //if the file was saved, don't prompt anything
+      if (saved)
+        break;
+      if (e.kind().equals(StandardWatchEventKinds.ENTRY_MODIFY)) {
+        int response = Base
+          .showYesNoQuestion(Editor.this, "File Modified",
+                             "A file has been modified externally",
+                             "Would you like to reload the sketch?");
+        if (response == 0) {
+          //reload the sketch
+          sketch.reload();
+          header.rebuild();
+        }
+      } else {
+        //for now, do nothing
+      }
+    }
+    saved = false;
   }
 
 
@@ -2403,6 +2480,7 @@ public abstract class Editor extends JFrame implements RunnerListener {
     statusNotice("Saving...");
     try {
       if (sketch.save()) {
+        saved = true;
         statusNotice("Done Saving.");
       } else {
         statusEmpty();
