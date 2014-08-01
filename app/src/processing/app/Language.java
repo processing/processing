43532ad6c2e50c8c9080b7f3f8342eaf66e2ca3b
Federico Bond <federicobond@gmail.com>
2014-08-01 01:16:12 -0300

    Add Spanish language

--- app/src/processing/app/Language.java
+++ app/src/processing/app/Language.java
@@ -44,6 +44,8 @@ public class Language {
 		this.languages.put(Locale.GERMAN.getLanguage(), Locale.GERMAN.getDisplayLanguage(Locale.GERMAN));
 		// ja, Japanese
 		this.languages.put(Locale.JAPANESE.getLanguage(), Locale.JAPANESE.getDisplayLanguage(Locale.JAPANESE));
+		// es, Spanish
+		this.languages.put("es", Locale.forLanguageTag("es").getDisplayLanguage(Locale.forLanguageTag("es")));
 
 		// Set default language
 		if (!this.languages.containsKey(this.language)) {
