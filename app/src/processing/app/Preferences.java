4f12d18f054b451d5ead0285a2b5cfd2fe81884e
Federico Bond <federicobond@gmail.com>
2014-08-22 01:07:52 -0300

    Fix preferences file path in preferences window

--- app/src/processing/app/Preferences.java
+++ app/src/processing/app/Preferences.java
@@ -151,6 +151,10 @@ public class Preferences {
     }
   }
 
+  static public File getPreferencesFile() {
+    return preferencesFile;
+  }
+
 
   // .................................................................
 
