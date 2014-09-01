75007764bc2e9a51cce57b3d77365223e58ec668
Federico Bond <federicobond@gmail.com>
2014-08-31 21:30:13 -0300

    Fix localization in OS X

--- app/src/processing/app/Language.java
+++ app/src/processing/app/Language.java
@@ -87,6 +87,7 @@ public class Language {
   
   static private String[] listSupported() {
     // List of languages in alphabetical order. (Add yours here.)
+    // Also remember to add it to the corresponding build/build.xml rule.
     final String[] SUPPORTED = {
       "de", // German, Deutsch
       "en", // English
@@ -147,6 +148,7 @@ public class Language {
     } catch (Exception e) {
       e.printStackTrace();
     }
+    Base.getPlatform().saveLanguage(language);
   }
   
   
