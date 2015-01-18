01e5d5b341d56273c115e5c8fe4f466f0b500ad3
Patrick Vares <pvrs12@gmail.com>
2015-01-18 01:26:51 -0500

    reverting app/.classpath and PSurfaceAWT.java (not sure how they got changed)

--- core/src/processing/core/PSurfaceAWT.java
+++ core/src/processing/core/PSurfaceAWT.java
@@ -831,9 +831,10 @@ public class PSurfaceAWT implements PSurface {
 
 //    canvas.setSize(wide, high);
 //    frame.setSize(wide, high);
-    setFrameSize(); //wide, high);
+    if (frame != null) {  // canvas only
+      setFrameSize(); //wide, high);
+    }
     setCanvasSize();
-    frame.setLocationRelativeTo(null);
 //    if (frame != null) {
 //      frame.setLocationRelativeTo(null);
 //    }
