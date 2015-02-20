38eeb0be6f80ca0d1bb3f3a0d0a489cea23cc443
codeanticode <andres.colubri@gmail.com>
2015-02-20 16:44:16 -0500

    removed LWJGL library

--- java/libraries/lwjgl/src/processing/lwjgl/LWJGL.java
+++
@@ -1,8 +0,0 @@
-package processing.lwjgl;
-
-public interface LWJGL {
-  static final String P2D       = "processing.lwjgl.PGraphics2D";
-  static final String P3D       = "processing.lwjgl.PGraphics3D";
-  static final String P2D_2X    = "processing.lwjgl.PGraphics2D2X";
-  static final String P3D_2X    = "processing.lwjgl.PGraphics3D2X";    
-}
