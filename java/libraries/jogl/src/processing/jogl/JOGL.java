a1ec718a8dff5c874b53bbfa8c9e5a272e4cba16
codeanticode <andres.colubri@gmail.com>
2015-05-14 18:22:18 -0400

    removed jogl library

--- java/libraries/jogl/src/processing/jogl/JOGL.java
+++
@@ -1,8 +0,0 @@
-package processing.jogl;
-
-public interface JOGL {
-  static final String P2D       = "processing.jogl.PGraphics2D";
-  static final String P3D       = "processing.jogl.PGraphics3D";
-  static final String P2D_2X    = "processing.jogl.PGraphics2D2X";
-  static final String P3D_2X    = "processing.jogl.PGraphics3D2X";    
-}
