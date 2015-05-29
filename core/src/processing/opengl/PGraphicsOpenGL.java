1505abe114e450e675a544503441a45857f838bf
codeanticode <andres.colubri@gmail.com>
2015-05-29 07:52:29 -0400

    fixed case in name of TexLight shader

--- core/src/processing/opengl/PGraphicsOpenGL.java
+++ core/src/processing/opengl/PGraphicsOpenGL.java
@@ -162,7 +162,7 @@ public class PGraphicsOpenGL extends PGraphics {
   static protected URL defLightShaderVertURL =
     PGraphicsOpenGL.class.getResource("/processing/opengl/shaders/LightVert.glsl");
   static protected URL defTexlightShaderVertURL =
-    PGraphicsOpenGL.class.getResource("/processing/opengl/shaders/TexlightVert.glsl");
+    PGraphicsOpenGL.class.getResource("/processing/opengl/shaders/TexLightVert.glsl");
   static protected URL defColorShaderFragURL =
     PGraphicsOpenGL.class.getResource("/processing/opengl/shaders/ColorFrag.glsl");
   static protected URL defTextureShaderFragURL =
@@ -170,7 +170,7 @@ public class PGraphicsOpenGL extends PGraphics {
   static protected URL defLightShaderFragURL =
     PGraphicsOpenGL.class.getResource("/processing/opengl/shaders/LightFrag.glsl");
   static protected URL defTexlightShaderFragURL =
-    PGraphicsOpenGL.class.getResource("/processing/opengl/shaders/TexlightFrag.glsl");
+    PGraphicsOpenGL.class.getResource("/processing/opengl/shaders/TexLightFrag.glsl");
 
   static protected URL defLineShaderVertURL =
     PGraphicsOpenGL.class.getResource("/processing/opengl/shaders/LineVert.glsl");
