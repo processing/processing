9dd3b635ecc25a60eae2a25a34925b235cdf7237
Ben Fry <ben@fathom.info>
2015-06-09 21:28:28 -0400

    starting the next release

--- app/src/processing/app/Base.java
+++ app/src/processing/app/Base.java
@@ -56,11 +56,10 @@ import processing.core.*;
 public class Base {
   // Added accessors for 0218 because the UpdateCheck class was not properly
   // updating the values, due to javac inlining the static final values.
-  static private final int REVISION = 237;
+  static private final int REVISION = 238;
   /** This might be replaced by main() if there's a lib/version.txt file. */
-  static private String VERSION_NAME = "0237"; //$NON-NLS-1$
+  static private String VERSION_NAME = "0238"; //$NON-NLS-1$
   /** Set true if this a proper release rather than a numbered revision. */
-//  static private boolean RELEASE = false;
 
   /** True if heavy debugging error/log messages are enabled */
   static public boolean DEBUG = false;
