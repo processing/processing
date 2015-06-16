a368ecc6d137f76943b93db772f27e75338a0b57
Ben Fry <ben@fathom.info>
2015-06-16 07:16:32 -0400

    catch errors as well as exceptions

--- app/src/processing/app/Base.java
+++ app/src/processing/app/Base.java
@@ -150,10 +150,10 @@ public class Base {
         public void run() {
           try {
             createAndShowGUI(args);
-          } catch (Exception e) {
+          } catch (Throwable t) {
             showBadnessTrace("It was not meant to be",
                              "A serious problem happened during startup. Please report:\n" +
-                             "http://github.com/processing/processing/issues/new", e, true);
+                             "http://github.com/processing/processing/issues/new", t, true);
           }
         }
     });
