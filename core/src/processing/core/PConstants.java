81b51cc16e9675f5dec247c7250d4d05f24b787e
Ben Fry <ben@fathom.info>
2015-06-21 16:02:43 -0400

    remove reference to FX so ARM can compile w/o it

--- core/src/processing/core/PConstants.java
+++ core/src/processing/core/PConstants.java
@@ -70,7 +70,7 @@ public interface PConstants {
 //  static final String E2D = PGraphicsDanger2D.class.getName();
 
   // Experimental JavaFX renderer; even better 2D performance
-  static final String FX2D = PGraphicsFX2D.class.getName();
+  static final String FX2D = "processing.core.PGraphicsFX2D";
 
   static final String PDF = "processing.pdf.PGraphicsPDF";
   static final String SVG = "processing.svg.PGraphicsSVG";
