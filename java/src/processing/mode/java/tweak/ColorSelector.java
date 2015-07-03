9c3504915d1f88181564cf9054fc7c2638c534b4
Ben Fry <ben@fathom.info>
2015-07-03 11:17:14 -0400

    tabs to spaces, some (but not all) formatting fixed

--- java/src/processing/mode/java/tweak/ColorSelector.java
+++ java/src/processing/mode/java/tweak/ColorSelector.java
@@ -1,21 +1,21 @@
 /* -*- mode: java; c-basic-offset: 2; indent-tabs-mode: nil -*- */
 
 /*
-Part of the Processing project - http://processing.org
-Copyright (c) 2012-15 The Processing Foundation
+  Part of the Processing project - http://processing.org
+  Copyright (c) 2012-15 The Processing Foundation
 
-This program is free software; you can redistribute it and/or modify
-it under the terms of the GNU General Public License version 2
-as published by the Free Software Foundation.
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License version 2
+  as published by the Free Software Foundation.
 
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
 
-You should have received a copy of the GNU General Public License
-along with this program; if not, write to the Free Software Foundation, Inc.
-51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software Foundation, Inc.
+  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
 */
 
 package processing.mode.java.tweak;
@@ -38,229 +38,110 @@ import javax.swing.JFrame;
 
 
 public class ColorSelector {
-	int hue, saturation, brightness;
+  int hue, saturation, brightness;
 
-	public JFrame frame;
-	public ColorControlBox colorBox;
-	ColorSelectorBox selectorBox;
-	ColorSelectorSlider selectorSlider;
-	SelectorTopBar topBar;
-
-
-	public ColorSelector(ColorControlBox colorBox) {
-		this.colorBox = colorBox;
-		createFrame();
-	}
+  public JFrame frame;
+  public ColorControlBox colorBox;
+  ColorSelectorBox selectorBox;
+  ColorSelectorSlider selectorSlider;
+  SelectorTopBar topBar;
 
 
-	public void createFrame() {
-		frame = new JFrame();
-		frame.setBackground(Color.BLACK);
-
-		Box box = Box.createHorizontalBox();
-		box.setBackground(Color.BLACK);
-
-		selectorSlider = new ColorSelectorSlider();
-
-		if (!colorBox.isBW) {
-			selectorBox = new ColorSelectorBox();
-			box.add(selectorBox);
-		}
-
-		box.add(Box.createHorizontalGlue());
-		box.add(selectorSlider, BorderLayout.CENTER);
-		box.add(Box.createHorizontalGlue());
-
-		frame.getContentPane().add(box, BorderLayout.CENTER);
-		frame.pack();
-		frame.setResizable(false);
-		frame.setCursor(Cursor.getPredefinedCursor(Cursor.CROSSHAIR_CURSOR));
-	}
+  public ColorSelector(ColorControlBox colorBox) {
+    this.colorBox = colorBox;
+    createFrame();
+  }
 
 
-	public void show(int x, int y) {
-		frame.setLocation(x, y);
-		frame.setVisible(true);
-		frame.repaint();
-	}
+  public void createFrame() {
+    frame = new JFrame();
+    frame.setBackground(Color.BLACK);
+
+    Box box = Box.createHorizontalBox();
+    box.setBackground(Color.BLACK);
+
+    selectorSlider = new ColorSelectorSlider();
+
+    if (!colorBox.isBW) {
+      selectorBox = new ColorSelectorBox();
+      box.add(selectorBox);
+    }
+
+    box.add(Box.createHorizontalGlue());
+    box.add(selectorSlider, BorderLayout.CENTER);
+    box.add(Box.createHorizontalGlue());
+
+    frame.getContentPane().add(box, BorderLayout.CENTER);
+    frame.pack();
+    frame.setResizable(false);
+    frame.setCursor(Cursor.getPredefinedCursor(Cursor.CROSSHAIR_CURSOR));
+  }
 
 
-	public void hide() {
-		this.colorBox = null;
-		frame.setVisible(false);
-	}
+  public void show(int x, int y) {
+    frame.setLocation(x, y);
+    frame.setVisible(true);
+    frame.repaint();
+  }
 
 
-	public void refreshColor() {
-		if (!colorBox.ilegalColor) {
-		  setColor(colorBox.color);
-		}
-	}
+  public void hide() {
+    this.colorBox = null;
+    frame.setVisible(false);
+  }
 
 
-	public void setColor(Color c) {
-		if (selectorBox != null) {
-			selectorBox.setToColor(c);
-		}
-		selectorSlider.setToColor(c);
-		repaintSelector();
-	}
+  public void refreshColor() {
+    if (!colorBox.ilegalColor) {
+      setColor(colorBox.color);
+    }
+  }
 
 
-	public void satBrightChanged() {
-		repaintSelector();
-	}
+  public void setColor(Color c) {
+    if (selectorBox != null) {
+      selectorBox.setToColor(c);
+    }
+    selectorSlider.setToColor(c);
+    repaintSelector();
+  }
 
 
-	public void hueChanged() {
-		if (selectorBox != null) {
-			selectorBox.renderBack();
-		}
-		repaintSelector();
-	}
+  public void satBrightChanged() {
+    repaintSelector();
+  }
 
 
-	public void repaintSelector() {
-		if (selectorBox != null) {
-			selectorBox.repaint();
-		}
-		selectorSlider.repaint();
-	}
+  public void hueChanged() {
+    if (selectorBox != null) {
+      selectorBox.renderBack();
+    }
+    repaintSelector();
+  }
 
 
-	// . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .
-
-
-	class ColorSelectorBox extends JComponent {
-		int lastX, lastY;
-		BufferedImage backImg;
-
-		ColorSelectorBox() {
-			if (!colorBox.ilegalColor) {
-				setToColor(colorBox.color);
-			}
-			renderBack();
-
-			addMouseListener(new MouseAdapter() {
-			  public void mousePressed(MouseEvent e) {
-			    updateMouse(e);
-			  }
-			});
-			addMouseMotionListener(new MouseAdapter() {
-			  public void mouseDragged(MouseEvent e) {
-			    updateMouse(e);
-        }
-      });
-		}
-
-
-		public void paintComponent(Graphics g) {
-		  g.drawImage(backImg, 0, 0, this);
-
-		  Graphics2D g2 = (Graphics2D) g;
-		  // otherwise the oval is hideous
-		  // TODO make a proper hidpi version of all this
-		  g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
-		                      RenderingHints.VALUE_ANTIALIAS_ON);
-
-		  g.setColor(lastY < 128 ? Color.BLACK : Color.WHITE);
-			AffineTransform tx = g2.getTransform();
-			g2.translate(lastX, lastY);
-			//g2.drawOval(0, 0, 5, 5);
-			g2.drawOval(-3, -3, 6, 6);
-			g2.drawLine(-8, 0, -6, 0);
-			g2.drawLine(6, 0, 8, 0);
-			g2.drawLine(0, -8, 0, -6);
-			g2.drawLine(0, 6, 0, 8);
-			g2.setTransform(tx);
-		}
-
-
-		public void renderBack() {
-		  int[] pixels = new int[256 * 256];
-			int index = 0;
-			for (int j = 0; j < 256; j++) {
-			  for (int i = 0; i < 256; i++) {
-			    pixels[index++] =  // color(hue, i, 255-j);
-			      Color.HSBtoRGB(hue / 255f, (i / 255f), (255-j)/255f);
-				}
-			}
-			backImg = new BufferedImage(256, 256, BufferedImage.TYPE_INT_RGB);
-			backImg.getRaster().setDataElements(0, 0, 256, 256, pixels);
-		}
-
-
-		public void setToColor(Color c) {
-			// set selector color
-			float hsb[] = Color.RGBtoHSB(c.getRed(), c.getGreen(), c.getBlue(), null);
-			saturation = (int) (hsb[1] * 255);
-			brightness = (int) (hsb[2] * 255);
-			lastX = saturation;
-			lastY = 255 - brightness;
-		}
-
-
-		void updateMouse(MouseEvent event) {
-		  int mouseX = event.getX();
-		  int mouseY = event.getY();
-
-		  if (mouseX >= 0 && mouseX < 256 &&
-		      mouseY >= 0 && mouseY < 256) {
-		    lastX = mouseX;
-		    lastY = mouseY;
-		    updateColor();
-		  }
-		}
-
-
-		void updateColor() {
-			saturation = lastX;
-			brightness = 255 - lastY;
-
-			satBrightChanged();
-			colorBox.selectorChanged(hue, saturation, brightness);
-		}
-
-
-		public Dimension getPreferredSize() {
-			return new Dimension(256, 256);
-		}
-
-
-		public Dimension getMinimumSize() {
-			return getPreferredSize();
-		}
-
-
-		public Dimension getMaximumSize() {
-			return getPreferredSize();
-		}
-	}
+  public void repaintSelector() {
+    if (selectorBox != null) {
+      selectorBox.repaint();
+    }
+    selectorSlider.repaint();
+  }
 
 
   // . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .
 
 
-	class ColorSelectorSlider extends JComponent {
-	  final int WIDE = 30;
-		BufferedImage backImg;
-		int lastY;
+  class ColorSelectorBox extends JComponent {
+    int lastX, lastY;
+    BufferedImage backImg;
 
-		ColorSelectorSlider() {
-//			size(30, 255);
-//			noLoop();
-//			colorMode(HSB, 255, 255, 255);
-//			strokeWeight(1);
-//			noFill();
-//			loadPixels();
-			if (!colorBox.ilegalColor) {
-				setToColor(colorBox.color);
-			}
+    ColorSelectorBox() {
+      if (!colorBox.ilegalColor) {
+        setToColor(colorBox.color);
+      }
+      renderBack();
 
-			// draw the slider background
-			renderBack();
-
-			addMouseListener(new MouseAdapter() {
+      addMouseListener(new MouseAdapter() {
         public void mousePressed(MouseEvent e) {
           updateMouse(e);
         }
@@ -270,99 +151,218 @@ public class ColorSelector {
           updateMouse(e);
         }
       });
-		}
+    }
 
-		public void paintComponent(Graphics g) {
-			g.drawImage(backImg, 0, 0, this);
 
-			Graphics2D g2 = (Graphics2D) g;
-//			if (colorBox.isBW) {
-//				stroke(lastY<128 ? 0 : 255);
-//			}
-//			else {
-//				stroke(0);
-//			}
-			if (colorBox.isBW && lastY >= 128) {
-			  g2.setColor(Color.WHITE);
-			} else {
-			  g2.setColor(Color.BLACK);
-			}
+    public void paintComponent(Graphics g) {
+      g.drawImage(backImg, 0, 0, this);
 
-			AffineTransform tx = g2.getTransform();
-			g2.translate(0, lastY);
-			// draw left bracket
-//			beginShape();
-//			vertex(5, -2);
-//			vertex(1, -2);
-//			vertex(1, 2);
-//			vertex(5, 2);
-//			endShape();
-			g.drawRect(1, -2, 6, 4);
+      Graphics2D g2 = (Graphics2D) g;
+      // otherwise the oval is hideous
+      // TODO make a proper hidpi version of all this
+      g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
+                          RenderingHints.VALUE_ANTIALIAS_ON);
 
-			// draw middle lines
-			g.drawLine(13, 0, 17, 0);
-			g.drawLine(15, -2, 15, 2);
+      g.setColor(lastY < 128 ? Color.BLACK : Color.WHITE);
+      AffineTransform tx = g2.getTransform();
+      g2.translate(lastX, lastY);
+      //g2.drawOval(0, 0, 5, 5);
+      g2.drawOval(-3, -3, 6, 6);
+      g2.drawLine(-8, 0, -6, 0);
+      g2.drawLine(6, 0, 8, 0);
+      g2.drawLine(0, -8, 0, -6);
+      g2.drawLine(0, 6, 0, 8);
+      g2.setTransform(tx);
+    }
 
-			// draw right bracket
-//			beginShape();
-//			vertex(24, -2);
-//			vertex(28, -2);
-//			vertex(28, 2);
-//			vertex(24, 2);
-//			endShape();
-			g.drawRect(24, -2, 4, 4);
-			g2.setTransform(tx);
 
-			/*
-			if (colorBox.isBW) {
-//				stroke(255);
-//				rect(0, 0, 29, 254);
-			  g.setColor(Color.WHITE);
-			  g.drawRect(0, 0, WIDE, 255);
-			} else {
-//				stroke(0);
-//				line(0, 0, 0, 255);
-//				line(29, 0, 29, 255);
-			  g.setColor(Color.BLACK);
+    public void renderBack() {
+      int[] pixels = new int[256 * 256];
+      int index = 0;
+      for (int j = 0; j < 256; j++) {
+        for (int i = 0; i < 256; i++) {
+          pixels[index++] =  // color(hue, i, 255-j);
+            Color.HSBtoRGB(hue / 255f, (i / 255f), (255-j)/255f);
+        }
+      }
+      backImg = new BufferedImage(256, 256, BufferedImage.TYPE_INT_RGB);
+      backImg.getRaster().setDataElements(0, 0, 256, 256, pixels);
+    }
+
+
+    public void setToColor(Color c) {
+      // set selector color
+      float hsb[] = Color.RGBtoHSB(c.getRed(), c.getGreen(), c.getBlue(), null);
+      saturation = (int) (hsb[1] * 255);
+      brightness = (int) (hsb[2] * 255);
+      lastX = saturation;
+      lastY = 255 - brightness;
+    }
+
+
+    void updateMouse(MouseEvent event) {
+      int mouseX = event.getX();
+      int mouseY = event.getY();
+
+      if (mouseX >= 0 && mouseX < 256 &&
+          mouseY >= 0 && mouseY < 256) {
+        lastX = mouseX;
+        lastY = mouseY;
+        updateColor();
+      }
+    }
+
+
+    void updateColor() {
+      saturation = lastX;
+      brightness = 255 - lastY;
+
+      satBrightChanged();
+      colorBox.selectorChanged(hue, saturation, brightness);
+    }
+
+
+    public Dimension getPreferredSize() {
+      return new Dimension(256, 256);
+    }
+
+
+    public Dimension getMinimumSize() {
+      return getPreferredSize();
+    }
+
+
+    public Dimension getMaximumSize() {
+      return getPreferredSize();
+    }
+  }
+
+
+  // . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .
+
+
+  class ColorSelectorSlider extends JComponent {
+    final int WIDE = 30;
+    BufferedImage backImg;
+    int lastY;
+
+    ColorSelectorSlider() {
+//      size(30, 255);
+//      noLoop();
+//      colorMode(HSB, 255, 255, 255);
+//      strokeWeight(1);
+//      noFill();
+//      loadPixels();
+      if (!colorBox.ilegalColor) {
+        setToColor(colorBox.color);
+      }
+
+      // draw the slider background
+      renderBack();
+
+      addMouseListener(new MouseAdapter() {
+        public void mousePressed(MouseEvent e) {
+          updateMouse(e);
+        }
+      });
+      addMouseMotionListener(new MouseAdapter() {
+        public void mouseDragged(MouseEvent e) {
+          updateMouse(e);
+        }
+      });
+    }
+
+    public void paintComponent(Graphics g) {
+      g.drawImage(backImg, 0, 0, this);
+
+      Graphics2D g2 = (Graphics2D) g;
+//      if (colorBox.isBW) {
+//        stroke(lastY<128 ? 0 : 255);
+//      }
+//      else {
+//        stroke(0);
+//      }
+      if (colorBox.isBW && lastY >= 128) {
+        g2.setColor(Color.WHITE);
+      } else {
+        g2.setColor(Color.BLACK);
+      }
+
+      AffineTransform tx = g2.getTransform();
+      g2.translate(0, lastY);
+      // draw left bracket
+//      beginShape();
+//      vertex(5, -2);
+//      vertex(1, -2);
+//      vertex(1, 2);
+//      vertex(5, 2);
+//      endShape();
+      g.drawRect(1, -2, 6, 4);
+
+      // draw middle lines
+      g.drawLine(13, 0, 17, 0);
+      g.drawLine(15, -2, 15, 2);
+
+      // draw right bracket
+//      beginShape();
+//      vertex(24, -2);
+//      vertex(28, -2);
+//      vertex(28, 2);
+//      vertex(24, 2);
+//      endShape();
+      g.drawRect(24, -2, 4, 4);
+      g2.setTransform(tx);
+
+      /*
+      if (colorBox.isBW) {
+//        stroke(255);
+//        rect(0, 0, 29, 254);
+        g.setColor(Color.WHITE);
+        g.drawRect(0, 0, WIDE, 255);
+      } else {
+//        stroke(0);
+//        line(0, 0, 0, 255);
+//        line(29, 0, 29, 255);
+        g.setColor(Color.BLACK);
         g.drawLine(0, 0, 0, 255);
         g.drawLine(29, 0, 29, 255);
-			}
-			*/
-		}
+      }
+      */
+    }
 
 
-		void renderBack() {
-		  int[] pixels = new int[WIDE * 256];
-			int index = 0;
+    void renderBack() {
+      int[] pixels = new int[WIDE * 256];
+      int index = 0;
 
-			int argb = 0;
-			for (int j = 0; j < 256; j++) {
-			  if (colorBox.isBW) {
-			    int gray = 255 - j;
-			    argb = 0xff000000 | (gray << 16) | (gray << 8) | gray;
-			  } else {
-			    // color(255-j, 255, 255);
-			    argb = Color.HSBtoRGB((255 - j) / 255f, 1, 1);
-			  }
-			  for (int i = 0; i < WIDE; i++) {
-			    pixels[index++] = argb;
-			  }
+      int argb = 0;
+      for (int j = 0; j < 256; j++) {
+        if (colorBox.isBW) {
+          int gray = 255 - j;
+          argb = 0xff000000 | (gray << 16) | (gray << 8) | gray;
+        } else {
+          // color(255-j, 255, 255);
+          argb = Color.HSBtoRGB((255 - j) / 255f, 1, 1);
+        }
+        for (int i = 0; i < WIDE; i++) {
+          pixels[index++] = argb;
+        }
       }
       backImg = new BufferedImage(WIDE, 256, BufferedImage.TYPE_INT_RGB);
       backImg.getRaster().setDataElements(0, 0, WIDE, 256, pixels);
-		}
+    }
 
 
-		void setToColor(Color c) {
-			// set slider position
-			if (colorBox.isBW) {
-				hue = c.getRed();
-			} else {
-				float hsb[] = Color.RGBtoHSB(c.getRed(), c.getGreen(), c.getBlue(), null);
-				hue = (int)(hsb[0]*255);
-			}
-			lastY = 255 - hue;
-		}
+    void setToColor(Color c) {
+      // set slider position
+      if (colorBox.isBW) {
+        hue = c.getRed();
+      } else {
+        float hsb[] = Color.RGBtoHSB(c.getRed(), c.getGreen(), c.getBlue(), null);
+        hue = (int)(hsb[0]*255);
+      }
+      lastY = 255 - hue;
+    }
 
 
     void updateMouse(MouseEvent event) {
@@ -374,57 +374,57 @@ public class ColorSelector {
     }
 
 
-		public void updateColor() {
-			hue = 255 - lastY;
-			hueChanged();
-			colorBox.selectorChanged(hue, saturation, brightness);
-		}
+    public void updateColor() {
+      hue = 255 - lastY;
+      hueChanged();
+      colorBox.selectorChanged(hue, saturation, brightness);
+    }
 
 
-		public Dimension getPreferredSize() {
-			return new Dimension(30, 255);
-		}
+    public Dimension getPreferredSize() {
+      return new Dimension(30, 255);
+    }
 
 
-		public Dimension getMinimumSize() {
-			return getPreferredSize();
-		}
+    public Dimension getMinimumSize() {
+      return getPreferredSize();
+    }
 
 
-		public Dimension getMaximumSize() {
-			return getPreferredSize();
-		}
-	}
+    public Dimension getMaximumSize() {
+      return getPreferredSize();
+    }
+  }
 
 
   // . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .
 
 
-	public class SelectorTopBar extends JComponent {
-		int barWidth;
-		int barHeight = 16;
+  public class SelectorTopBar extends JComponent {
+    int barWidth;
+    int barHeight = 16;
 
-		public SelectorTopBar(int w) {
-			barWidth = w;
-		}
+    public SelectorTopBar(int w) {
+      barWidth = w;
+    }
 
-		@Override
-		public void paintComponent(Graphics g) {
-		  g.setColor(Color.GRAY);
-		  Dimension size = getSize();
-		  g.fillRect(0, 0, size.width, size.height);
-		}
+    @Override
+    public void paintComponent(Graphics g) {
+      g.setColor(Color.GRAY);
+      Dimension size = getSize();
+      g.fillRect(0, 0, size.width, size.height);
+    }
 
-		public Dimension getPreferredSize() {
-			return new Dimension(barWidth, barHeight);
-		}
+    public Dimension getPreferredSize() {
+      return new Dimension(barWidth, barHeight);
+    }
 
-		public Dimension getMinimumSize() {
-			return getPreferredSize();
-		}
+    public Dimension getMinimumSize() {
+      return getPreferredSize();
+    }
 
-		public Dimension getMaximumSize() {
-			return getPreferredSize();
-		}
-	}
+    public Dimension getMaximumSize() {
+      return getPreferredSize();
+    }
+  }
 }
--- java/src/processing/mode/java/tweak/TweakClient.java
+++ java/src/processing/mode/java/tweak/TweakClient.java
@@ -25,164 +25,164 @@ import java.nio.ByteBuffer;
 
 
 public class TweakClient {
-	private DatagramSocket socket;
-	private InetAddress address;
-	private boolean initialized;
-	private int sketchPort;
+  private DatagramSocket socket;
+  private InetAddress address;
+  private boolean initialized;
+  private int sketchPort;
 
-	static final int VAR_INT = 0;
-	static final int VAR_FLOAT = 1;
-	static final int SHUTDOWN = 0xffffffff;
-
-
-	public TweakClient(int sketchPort) {
-		this.sketchPort = sketchPort;
-
-		try {
-			socket = new DatagramSocket();
-			// only local sketch is allowed
-			address = InetAddress.getByName("127.0.0.1");
-			initialized = true;
-
-		} catch (SocketException e) {
-			initialized = false;
-
-		} catch (UnknownHostException e) {
-			socket.close();
-			initialized = false;
-
-		} catch (SecurityException e) {
-			socket.close();
-			initialized = false;
-		}
-	}
+  static final int VAR_INT = 0;
+  static final int VAR_FLOAT = 1;
+  static final int SHUTDOWN = 0xffffffff;
 
 
-	public void shutdown() {
-		if (initialized) {
-		  // send shutdown to the sketch
-		  sendShutdown();
-		  initialized = false;
-		}
-	}
+  public TweakClient(int sketchPort) {
+    this.sketchPort = sketchPort;
+
+    try {
+      socket = new DatagramSocket();
+      // only local sketch is allowed
+      address = InetAddress.getByName("127.0.0.1");
+      initialized = true;
+
+    } catch (SocketException e) {
+      initialized = false;
+
+    } catch (UnknownHostException e) {
+      socket.close();
+      initialized = false;
+
+    } catch (SecurityException e) {
+      socket.close();
+      initialized = false;
+    }
+  }
 
 
-	public boolean sendInt(int index, int val) {
-		if (initialized) {
-		  try {
-		    byte[] buf = new byte[12];
-		    ByteBuffer bb = ByteBuffer.wrap(buf);
-		    bb.putInt(0, VAR_INT);
-		    bb.putInt(4, index);
-		    bb.putInt(8, val);
-		    DatagramPacket packet = new DatagramPacket(buf, buf.length, address, sketchPort);
-		    socket.send(packet);
-		    return true;
-
-		  } catch (Exception e) { }
-		}
-		return false;
-	}
+  public void shutdown() {
+    if (initialized) {
+      // send shutdown to the sketch
+      sendShutdown();
+      initialized = false;
+    }
+  }
 
 
-	public boolean sendFloat(int index, float val) {
-		if (initialized) {
-	    try {
-	    	byte[] buf = new byte[12];
-	    	ByteBuffer bb = ByteBuffer.wrap(buf);
-	    	bb.putInt(0, VAR_FLOAT);
-	    	bb.putInt(4, index);
-	    	bb.putFloat(8, val);
-	    	socket.send(new DatagramPacket(buf, buf.length, address, sketchPort));
-	    	return true;
+  public boolean sendInt(int index, int val) {
+    if (initialized) {
+      try {
+        byte[] buf = new byte[12];
+        ByteBuffer bb = ByteBuffer.wrap(buf);
+        bb.putInt(0, VAR_INT);
+        bb.putInt(4, index);
+        bb.putInt(8, val);
+        DatagramPacket packet = new DatagramPacket(buf, buf.length, address, sketchPort);
+        socket.send(packet);
+        return true;
 
-	    } catch (Exception e) { }
-		}
-		return false;
-	}
-
-
-	public boolean sendShutdown() {
-		if (initialized) {
-	    try {
-	    	byte[] buf = new byte[12];
-	    	ByteBuffer bb = ByteBuffer.wrap(buf);
-	    	bb.putInt(0, SHUTDOWN);
-	    	socket.send(new DatagramPacket(buf, buf.length, address, sketchPort));
-	    	return true;
-
-	    } catch (Exception e) { }
-		}
-		return false;
-	}
+      } catch (Exception e) { }
+    }
+    return false;
+  }
 
 
-	static public String getServerCode(int listenPort,
-	                                   boolean hasInts, boolean hasFloats) {
-		String serverCode = ""+
-		"class TweakModeServer extends Thread\n"+
-		"{\n"+
-		"	protected DatagramSocket socket = null;\n"+
-		"	protected boolean running = true;\n"+
-		"	final int INT_VAR = 0;\n"+
-		"	final int FLOAT_VAR = 1;\n"+
-		"	final int SHUTDOWN = 0xffffffff;\n"+
-		"	public TweakModeServer() {\n"+
-		"		this(\"TweakModeServer\");\n"+
-		"	}\n"+
-		"	public TweakModeServer(String name) {\n"+
-		"		super(name);\n"+
-		"	}\n"+
-		"	public void setup()\n"+
-		"	{\n"+
-		"		try {\n"+
-		"			socket = new DatagramSocket("+listenPort+");\n"+
-		"			socket.setSoTimeout(250);\n"+
-		"		} catch (IOException e) {\n"+
-		"			println(\"error: could not create TweakMode server socket\");\n"+
-		"		}\n"+
-		"	}\n"+
-		"	public void run()\n"+
-		"	{\n"+
-		"		byte[] buf = new byte[256];\n"+
-		"		while(running)\n"+
-		"		{\n"+
-		"			try {\n"+
-		"				DatagramPacket packet = new DatagramPacket(buf, buf.length);\n"+
-		"				socket.receive(packet);\n"+
-		"				ByteBuffer bb = ByteBuffer.wrap(buf);\n"+
-		"				int type = bb.getInt(0);\n"+
-		"				int index = bb.getInt(4);\n";
+  public boolean sendFloat(int index, float val) {
+    if (initialized) {
+      try {
+        byte[] buf = new byte[12];
+        ByteBuffer bb = ByteBuffer.wrap(buf);
+        bb.putInt(0, VAR_FLOAT);
+        bb.putInt(4, index);
+        bb.putFloat(8, val);
+        socket.send(new DatagramPacket(buf, buf.length, address, sketchPort));
+        return true;
 
-		if (hasInts) {
-			serverCode +=
-		"				if (type == INT_VAR) {\n"+
-		"					int val = bb.getInt(8);\n"+
-		"					tweakmode_int[index] = val;\n"+
-		"				}\n"+
-		"				else ";
-		}
-		if (hasFloats) {
-			serverCode +=
-		" 				if (type == FLOAT_VAR) {\n"+
-		"					float val = bb.getFloat(8);\n"+
-		"					tweakmode_float[index] = val;\n"+
-		"				}\n"+
-		"				else";
-		}
-		serverCode +=
-		"				if (type == SHUTDOWN) {\n"+
-		"					running = false;\n"+
-		"				}\n"+
-		"			} catch (SocketTimeoutException e) {\n"+
-		"				// nothing to do here just try receiving again\n"+
-		"			} catch (Exception e) {\n"+
-		"			}\n"+
-		"		}\n"+
-		"		socket.close();\n"+
-		"	}\n"+
-		"}\n\n\n";
+      } catch (Exception e) { }
+    }
+    return false;
+  }
 
-		return serverCode;
-	}
+
+  public boolean sendShutdown() {
+    if (initialized) {
+      try {
+        byte[] buf = new byte[12];
+        ByteBuffer bb = ByteBuffer.wrap(buf);
+        bb.putInt(0, SHUTDOWN);
+        socket.send(new DatagramPacket(buf, buf.length, address, sketchPort));
+        return true;
+
+      } catch (Exception e) { }
+    }
+    return false;
+  }
+
+
+  static public String getServerCode(int listenPort,
+                                     boolean hasInts, boolean hasFloats) {
+    String serverCode = ""+
+    "class TweakModeServer extends Thread\n"+
+    "{\n"+
+    "  protected DatagramSocket socket = null;\n"+
+    "  protected boolean running = true;\n"+
+    "  final int INT_VAR = 0;\n"+
+    "  final int FLOAT_VAR = 1;\n"+
+    "  final int SHUTDOWN = 0xffffffff;\n"+
+    "  public TweakModeServer() {\n"+
+    "    this(\"TweakModeServer\");\n"+
+    "  }\n"+
+    "  public TweakModeServer(String name) {\n"+
+    "    super(name);\n"+
+    "  }\n"+
+    "  public void setup()\n"+
+    "  {\n"+
+    "    try {\n"+
+    "      socket = new DatagramSocket("+listenPort+");\n"+
+    "      socket.setSoTimeout(250);\n"+
+    "    } catch (IOException e) {\n"+
+    "      println(\"error: could not create TweakMode server socket\");\n"+
+    "    }\n"+
+    "  }\n"+
+    "  public void run()\n"+
+    "  {\n"+
+    "    byte[] buf = new byte[256];\n"+
+    "    while(running)\n"+
+    "    {\n"+
+    "      try {\n"+
+    "        DatagramPacket packet = new DatagramPacket(buf, buf.length);\n"+
+    "        socket.receive(packet);\n"+
+    "        ByteBuffer bb = ByteBuffer.wrap(buf);\n"+
+    "        int type = bb.getInt(0);\n"+
+    "        int index = bb.getInt(4);\n";
+
+    if (hasInts) {
+      serverCode +=
+    "        if (type == INT_VAR) {\n"+
+    "          int val = bb.getInt(8);\n"+
+    "          tweakmode_int[index] = val;\n"+
+    "        }\n"+
+    "        else ";
+    }
+    if (hasFloats) {
+      serverCode +=
+    "         if (type == FLOAT_VAR) {\n"+
+    "          float val = bb.getFloat(8);\n"+
+    "          tweakmode_float[index] = val;\n"+
+    "        }\n"+
+    "        else";
+    }
+    serverCode +=
+    "        if (type == SHUTDOWN) {\n"+
+    "          running = false;\n"+
+    "        }\n"+
+    "      } catch (SocketTimeoutException e) {\n"+
+    "        // nothing to do here just try receiving again\n"+
+    "      } catch (Exception e) {\n"+
+    "      }\n"+
+    "    }\n"+
+    "    socket.close();\n"+
+    "  }\n"+
+    "}\n\n\n";
+
+    return serverCode;
+  }
 }
