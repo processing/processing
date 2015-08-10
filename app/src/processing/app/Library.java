07c6f52db25bdc19ec2711cb92b10435d038f08d
Ben Fry <ben@fathom.info>
2015-08-09 20:47:19 -0400

    additional type corrections

--- app/src/processing/app/Library.java
+++ app/src/processing/app/Library.java
@@ -256,13 +256,13 @@ public class Library extends LocalContribution {
    * @param importToLibraryTable mapping from package names to Library objects
    */
 //  public void addPackageList(HashMap<String,Library> importToLibraryTable) {
-  public void addPackageList(Map<String, ArrayList<Library>> importToLibraryTable) {
+  public void addPackageList(Map<String, List<Library>> importToLibraryTable) {
 //    PApplet.println(packages);
     for (String pkg : packageList) {
 //          pw.println(pkg + "\t" + libraryFolder.getAbsolutePath());
 //      PApplet.println(pkg + "\t" + getName());
 //      Library library = importToLibraryTable.get(pkg);
-      ArrayList<Library> libraries = importToLibraryTable.get(pkg);
+      List<Library> libraries = importToLibraryTable.get(pkg);
       if (libraries == null) {
         libraries = new ArrayList<Library>();
         importToLibraryTable.put(pkg, libraries);
--- app/src/processing/app/Mode.java
+++ app/src/processing/app/Mode.java
@@ -62,7 +62,7 @@ public abstract class Mode {
 //  protected Tool formatter;
 
   // maps imported packages to their library folder
-  protected Map<String, ArrayList<Library>> importToLibraryTable;
+  protected Map<String, List<Library>> importToLibraryTable;
 
   // these menus are shared so that they needn't be rebuilt for all windows
   // each time a sketch is created, renamed, or moved.
@@ -318,7 +318,7 @@ public abstract class Mode {
   public void rebuildLibraryList() {
     //new Exception("Rebuilding library list").printStackTrace(System.out);
     // reset the table mapping imports to libraries
-    importToLibraryTable = new HashMap<String, ArrayList<Library>>();
+    importToLibraryTable = new HashMap<String, List<Library>>();
 
     Library core = getCoreLibrary();
     if (core != null) {
