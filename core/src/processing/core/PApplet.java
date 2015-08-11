3ddd3d763c5d00a38645068df0efcfe575671ea3
Ben Fry <ben@fathom.info>
2015-08-11 12:12:49 -0400

    break out the infinity and NaN cases to simplify the error message

--- core/src/processing/core/PApplet.java
+++ core/src/processing/core/PApplet.java
@@ -4804,11 +4804,19 @@ public class PApplet implements PConstants {
                                 float start2, float stop2) {
     float outgoing =
       start2 + (stop2 - start2) * ((value - start1) / (stop1 - start1));
-    if (!Float.isFinite(outgoing)) {
+    String badness = null;
+    if (outgoing != outgoing) {
+      badness = "NaN (not a number)";
+
+    } else if (outgoing == Float.NEGATIVE_INFINITY ||
+               outgoing == Float.POSITIVE_INFINITY) {
+      badness = "infinity";
+    }
+    if (badness != null) {
       final String msg =
-        String.format("map(%s, %s, %s, %s, %s) called, " +
-                      "which returns NaN or infinity",
-                      nf(value), nf(start1), nf(stop1), nf(start2), nf(stop2));
+        String.format("map(%s, %s, %s, %s, %s) called, which returns %s",
+                      nf(value), nf(start1), nf(stop1),
+                      nf(start2), nf(stop2), badness);
       PGraphics.showWarning(msg);
     }
     return outgoing;
