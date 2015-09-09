1b0083f200dd75749e9cd4a00faddd70658fa728
Jakub Valtar <jakub.valtar@gmail.com>
2015-09-09 18:07:58 -0400

    Fix PShape creation in P3D
    Related to #3776

--- core/src/processing/core/PGraphics.java
+++ core/src/processing/core/PGraphics.java
@@ -1784,7 +1784,7 @@ public class PGraphics extends PImage implements PConstants {
     if (kind == POINT) {
       if (is3D() && len != 2 && len != 3) {
         throw new IllegalArgumentException("Use createShape(POINT, x, y) or createShape(POINT, x, y, z)");
-      } else if (len != 2) {
+      } else if (is2D() && len != 2) {
         throw new IllegalArgumentException("Use createShape(POINT, x, y)");
       }
       return createShapePrimitive(kind, p);
@@ -1792,7 +1792,7 @@ public class PGraphics extends PImage implements PConstants {
     } else if (kind == LINE) {
       if (is3D() && len != 4 && len != 6) {
         throw new IllegalArgumentException("Use createShape(LINE, x1, y1, x2, y2) or createShape(LINE, x1, y1, z1, x2, y2, z1)");
-      } else if (len != 4) {
+      } else if (is2D() && len != 4) {
         throw new IllegalArgumentException("Use createShape(LINE, x1, y1, x2, y2)");
       }
       return createShapePrimitive(kind, p);
