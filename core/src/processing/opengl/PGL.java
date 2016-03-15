f6fb24b750743ac95c9060b73cf0f955a84967a9
codeanticode <andres.colubri@gmail.com>
2016-03-15 10:31:48 -0400

    Properly detect NPOT extension in GLES2

--- core/src/processing/opengl/PGL.java
+++ core/src/processing/opengl/PGL.java
@@ -2097,7 +2097,11 @@ public abstract class PGL {
     int major = getGLVersion()[0];
     if (major < 3) {
       String ext = getString(EXTENSIONS);
-      return -1 < ext.indexOf("_texture_non_power_of_two");
+      if (isES()) {
+        return -1 < ext.indexOf("_texture_npot");
+      } else {
+        return -1 < ext.indexOf("_texture_non_power_of_two");
+      }
     } else {
       return true;
     }
