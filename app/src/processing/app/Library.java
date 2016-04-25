91c8a9f043779fb31c093b14c0ba48dd40b0add8
Jakub Valtar <jakub.valtar@gmail.com>
2016-04-25 22:17:14 +0200

    Fix Libraries adding jars twice to class path

--- app/src/processing/app/Library.java
+++ app/src/processing/app/Library.java
@@ -338,10 +338,13 @@ public class Library extends LocalContribution {
       cp.append(File.pathSeparatorChar);
       cp.append(new File(libraryFolder, jar).getAbsolutePath());
     }
-    jarHeads = new File(nativeLibraryPath).list(jarFilter);
-    for (String jar : jarHeads) {
-      cp.append(File.pathSeparatorChar);
-      cp.append(new File(nativeLibraryPath, jar).getAbsolutePath());
+    File nativeLibraryFolder = new File(nativeLibraryPath);
+    if (!libraryFolder.equals(nativeLibraryFolder)) {
+      jarHeads = new File(nativeLibraryPath).list(jarFilter);
+      for (String jar : jarHeads) {
+        cp.append(File.pathSeparatorChar);
+        cp.append(new File(nativeLibraryPath, jar).getAbsolutePath());
+      }
     }
     //cp.setLength(cp.length() - 1);  // remove the last separator
     return cp.toString();
