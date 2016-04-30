4bba6d704f4df16fcadd16c7165ccd181744365c
Jakub Valtar <jakub.valtar@gmail.com>
2016-04-30 16:50:45 +0200

    Fix mode core library being null

--- java/src/processing/mode/java/pdex/ErrorCheckerService.java
+++ java/src/processing/mode/java/pdex/ErrorCheckerService.java
@@ -568,9 +568,18 @@ public class ErrorCheckerService {
     }
 
     // Mode class path
-    String coreClassPath = mode.getCoreLibrary().getClassPath();
-    if (coreClassPath != null) {
-      classPath.append(File.pathSeparator).append(coreClassPath);
+    if (neededImports == null) {
+      String searchClassPath = mode.getSearchPath();
+      if (searchClassPath != null) {
+        classPath.append(File.pathSeparator).append(searchClassPath);
+      }
+    } else {
+      Library coreLibrary = mode.getCoreLibrary();
+      String coreClassPath = coreLibrary != null ?
+          coreLibrary.getClassPath() : mode.getSearchPath();
+      if (coreClassPath != null) {
+        classPath.append(File.pathSeparator).append(coreClassPath);
+      }
     }
 
     // Core libraries
