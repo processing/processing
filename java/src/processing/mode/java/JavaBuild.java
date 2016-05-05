ff9811307c94f061d218677f35eba02b3313cadc
Jakub Valtar <jakub.valtar@gmail.com>
2016-05-06 00:19:50 +0200

    JavaBuild: fix output path when java tabs have package declaration

--- java/src/processing/mode/java/JavaBuild.java
+++ java/src/processing/mode/java/JavaBuild.java
@@ -523,11 +523,11 @@ public class JavaBuild {
           } else {
             if (packageMatch == null) {
               // use the default package name, since mixing with package-less code will break
-              packageMatch = new String[] { packageName };
+              packageMatch = new String[] { "", packageName };
               // add the package name to the source before writing it
               javaCode = "package " + packageName + ";" + javaCode;
             }
-            File packageFolder = new File(srcFolder, packageMatch[0].replace('.', '/'));
+            File packageFolder = new File(srcFolder, packageMatch[1].replace('.', File.separatorChar));
             packageFolder.mkdirs();
             Util.saveFile(javaCode, new File(packageFolder, filename));
           }
