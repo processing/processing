f86019dfb5287c8a9c8a589d7c128144e47e1094
Ben Fry <ben@fathom.info>
2016-05-16 18:31:32 -0400

    starting the next release

--- app/src/processing/app/Base.java
+++ app/src/processing/app/Base.java
@@ -3,7 +3,7 @@
 /*
   Part of the Processing project - http://processing.org
 
-  Copyright (c) 2012-15 The Processing Foundation
+  Copyright (c) 2012-16 The Processing Foundation
   Copyright (c) 2004-12 Ben Fry and Casey Reas
   Copyright (c) 2001-04 Massachusetts Institute of Technology
 
@@ -55,9 +55,9 @@ import processing.data.StringList;
 public class Base {
   // Added accessors for 0218 because the UpdateCheck class was not properly
   // updating the values, due to javac inlining the static final values.
-  static private final int REVISION = 250;
+  static private final int REVISION = 251;
   /** This might be replaced by main() if there's a lib/version.txt file. */
-  static private String VERSION_NAME = "0250"; //$NON-NLS-1$
+  static private String VERSION_NAME = "0251"; //$NON-NLS-1$
   /** Set true if this a proper release rather than a numbered revision. */
 
   /** True if heavy debugging error/log messages are enabled */
