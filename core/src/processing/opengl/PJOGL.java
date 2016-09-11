90341654b7bc28805551c11f254c9160dfd69db9
jshrake <justinshrake@gmail.com>
2016-09-10 21:25:21 -0700

    Call glGetProgramiv to retrieve program log length
    Calling glGetShaderiv to retrieve the program log length produces a
    GL_INVALID_OPERATION error and length < 0, preventing the return of the
    actual info log. This fix replaces the call to glGetShaderiv with
    glGetProgramiv.
    
    Closes #4659

--- core/src/processing/opengl/PJOGL.java
+++ core/src/processing/opengl/PJOGL.java
@@ -1701,7 +1701,7 @@ public class PJOGL extends PGL {
   @Override
   public String getProgramInfoLog(int program) {
     int[] val = { 0 };
-    gl2.glGetShaderiv(program, GL2ES2.GL_INFO_LOG_LENGTH, val, 0);
+    gl2.glGetProgramiv(program, GL2ES2.GL_INFO_LOG_LENGTH, val, 0);
     int length = val[0];
 
     if (0 < length) {
