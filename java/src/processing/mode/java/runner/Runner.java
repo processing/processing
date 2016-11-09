da0efeb07db94ddfaacacb1a483d89730ec80af3
Ben Fry <ben@fathom.info>
2016-11-09 11:52:48 -0500

    detect changes to /etc/hosts and warn user (#4738)

--- java/src/processing/mode/java/runner/Runner.java
+++ java/src/processing/mode/java/runner/Runner.java
@@ -34,6 +34,8 @@ import java.awt.GraphicsEnvironment;
 import java.awt.Point;
 import java.io.*;
 import java.net.ConnectException;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 import java.util.*;
 
 import com.sun.jdi.*;
@@ -82,6 +84,8 @@ public class Runner implements MessageConsumer {
 //    this.sketch = sketch;
     this.build = build;
 
+    checkLocalHost();
+
     if (listener instanceof Editor) {
       this.editor = (Editor) listener;
       sketchErr = editor.getConsole().getErr();
@@ -112,6 +116,29 @@ public class Runner implements MessageConsumer {
           //  "To use this library, you must use the 32-bit version of Processing."
         }
       }
+    }
+  }
+
+
+  /**
+   * Has the user screwed up their hosts file?
+   * https://github.com/processing/processing/issues/4738
+   */
+  private void checkLocalHost() throws SketchException {
+    try {
+      InetAddress address = InetAddress.getByName("localhost");
+      if (!address.getHostAddress().equals("127.0.0.1")) {
+        System.err.println("Your computer is not properly mapping 'localhost' to '127.0.0.1',");
+        System.err.println("which prevents sketches from working properly because 'localhost'");
+        System.err.println("is needed to connect the PDE to your sketch while it's running.");
+        System.err.println("If you don't recall making this change, or know how to fix it:");
+        System.err.println("https://www.google.com/search?q=add+localhost+to+hosts+file+" + Platform.getName());
+        throw new SketchException("Cannot run due to changes in your 'hosts' file. " +
+                                  "See the console for details.", false);
+      }
+
+    } catch (UnknownHostException e) {
+      e.printStackTrace();
     }
   }
 
