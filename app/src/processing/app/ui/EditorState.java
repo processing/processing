54231178fb8e9bce1332d43a28f465446920e57b
Ben Fry <ben@fathom.info>
2017-02-11 22:26:35 +0000

    simplify the divider location setup

--- app/src/processing/app/ui/EditorState.java
+++ app/src/processing/app/ui/EditorState.java
@@ -219,11 +219,12 @@ public class EditorState {
 
   void apply(Editor editor) {
     editor.setBounds(editorBounds);
-    if (dividerLocation != 0) {
-      editor.setDividerLocation(dividerLocation);
-    } else {
-      editor.setDividerLocation(2 * editor.getSize().height / 3);
+
+    if (dividerLocation == 0) {
+      dividerLocation = 2 * editor.getSize().height / 3;
     }
+    editor.setDividerLocation(dividerLocation);
+
     if (isMaximized) {
       editor.setExtendedState(Frame.MAXIMIZED_BOTH);
     }
