2b3dac0568fb79acf1ab0185920d0db3c2d0b4bc
Ben Fry <ben@fathom.info>
2017-02-20 15:30:49 -0500

    fix ordering of BufferedOutputStream; huge speed fix

--- core/src/processing/core/PApplet.java
+++ core/src/processing/core/PApplet.java
@@ -7384,12 +7384,11 @@ public class PApplet implements PConstants {
   static public OutputStream createOutput(File file) {
     try {
       createPath(file);  // make sure the path exists
-      OutputStream output =
-        new BufferedOutputStream(new FileOutputStream(file));
+      OutputStream output = new FileOutputStream(file);
       if (file.getName().toLowerCase().endsWith(".gz")) {
-        return new GZIPOutputStream(output);
+        return new BufferedOutputStream(new GZIPOutputStream(output));
       }
-      return output;
+      return new BufferedOutputStream(output);
 
     } catch (IOException e) {
       e.printStackTrace();
