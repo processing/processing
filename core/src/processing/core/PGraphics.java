8f312897ba165ccbbc24d7552e0a525fe448f569
Jakub Valtar <jakub.valtar@gmail.com>
2017-04-17 21:31:59 +0200

    pixelDensity (all): fix background(PImage) size check

--- core/src/processing/core/PGraphics.java
+++ core/src/processing/core/PGraphics.java
@@ -7352,7 +7352,7 @@ public class PGraphics extends PImage implements PConstants {
    * @param image PImage to set as background (must be same size as the sketch window)
    */
   public void background(PImage image) {
-    if ((image.pixelWidth != width) || (image.pixelHeight != height)) {
+    if ((image.pixelWidth != pixelWidth) || (image.pixelHeight != pixelHeight)) {
       throw new RuntimeException(ERROR_BACKGROUND_IMAGE_SIZE);
     }
     if ((image.format != RGB) && (image.format != ARGB)) {
