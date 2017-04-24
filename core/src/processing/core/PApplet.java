4b51c375569e4d391be20c732bb2c6259e460fd0
Ben Fry <ben@fathom.info>
2017-04-24 19:30:42 -0400

    syntax tweaks

--- core/src/processing/core/PApplet.java
+++ core/src/processing/core/PApplet.java
@@ -7169,25 +7169,25 @@ public class PApplet implements PConstants {
     return null;
   }
 
+
   /**
    * @nowebref
    */
   static public byte[] loadBytes(InputStream input) {
     try {
       ByteArrayOutputStream out = new ByteArrayOutputStream();
-      byte[] buf = new byte[4096];
+      byte[] buffer = new byte[4096];
 
-      int bytesRead = input.read(buf);
+      int bytesRead = input.read(buffer);
       while (bytesRead != -1) {
-        out.write(buf, 0, bytesRead);
-        bytesRead = input.read(buf);
+        out.write(buffer, 0, bytesRead);
+        bytesRead = input.read(buffer);
       }
       out.flush();
       return out.toByteArray();
 
     } catch (IOException e) {
       e.printStackTrace();
-      //throw new RuntimeException("Couldn't load bytes from stream");
     }
     return null;
   }
