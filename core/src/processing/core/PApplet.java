8effb5d600deef46b26fe486d5f0336aab7188b4
Paco <pacoelayudante@gmail.com>
2017-06-20 20:28:42 -0300

    make "loadXML(String)" handle "file not found"
    when file is not found, a null pointer exception crashes processing i guess is because this line

--- core/src/processing/core/PApplet.java
+++ core/src/processing/core/PApplet.java
@@ -5921,7 +5921,9 @@ public class PApplet implements PConstants {
    */
   public XML loadXML(String filename, String options) {
     try {
-      return new XML(createReader(filename), options);
+      BufferedReader br = createReader(filename);
+      if (br != null) return new XML(br, options);
+      else br = null;
 
       // can't use catch-all exception, since it might catch the
       // RuntimeException about the incorrect case sensitivity
