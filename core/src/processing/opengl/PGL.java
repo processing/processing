44b0b359d0a86089e8dc3192bb2afe8e78abab0e
Jakub Valtar <jakub.valtar@gmail.com>
2017-07-21 11:29:41 +0200

    Update shader version directive check to ignore comments

--- core/src/processing/opengl/PGL.java
+++ core/src/processing/opengl/PGL.java
@@ -2003,8 +2003,12 @@ public abstract class PGL {
   protected static boolean containsVersionDirective(String[] shSrc) {
     for (int i = 0; i < shSrc.length; i++) {
       String line = shSrc[i];
-      if (line.contains("#version")) {
-        return true;
+      int versionIndex = line.indexOf("#version");
+      if (versionIndex >= 0) {
+        int commentIndex = line.indexOf("//");
+        if (commentIndex < 0 || versionIndex < commentIndex) {
+          return true;
+        }
       }
     }
     return false;
