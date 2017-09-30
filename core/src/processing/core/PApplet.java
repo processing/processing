b0b2d89228c8bf178f5113a4af25779a14f242e1
Ben Fry <ben@fathom.info>
2017-09-29 23:15:17 -0400

    handle version number parsing for JDK 9 (fixes #5275)

--- core/src/processing/core/PApplet.java
+++ core/src/processing/core/PApplet.java
@@ -111,45 +111,17 @@ public class PApplet implements PConstants {
   static public final String javaVersionName =
     System.getProperty("java.version");
 
-//  /** Short name of Java version, i.e. 1.8. */
-//  static public final String javaVersionShort =
-//    //javaVersionName.substring(0, 3);
-//    javaVersionName.substring(0, javaVersionName.indexOf(".", 2));
-//    // can't use this one, it's 1.8.0 and breaks things
-//    //javaVersionName.substring(0, javaVersionName.indexOf("_"));
-
   static public final int javaPlatform =
-    PApplet.parseInt(PApplet.split(javaVersionName, '.')[1]);
-//  static {
-//    try {
-//      javaPlatform = PApplet.split(javaVersionName, '.')[1];
-//    } catch (Exception e) {
-//      javaPlatform = "8";  // set a default in case
-//    }
-//  }
+    parseInt(match(javaVersionName, "[1-9][0-9]*((\\.0)*\\.[1-9][0-9]*)*")[0]);
 
   /**
-   * Version of Java that's in use, whether 1.1 or 1.3 or whatever,
-   * stored as a float.
-   * <p>
-   * Note that because this is stored as a float, the values may not be
-   * <EM>exactly</EM> 1.3 or 1.4. The PDE will make 1.8 or whatever into
-   * a float automatically, so outside the PDE, make sure you're comparing
-   * against 1.3f or 1.4f, which will have the same amount of error
-   * (i.e. 1.40000001). This could just be a double, but since Processing
-   * only uses floats, it's safer as a float because specifying a double
-   * (with this narrow case especially) with the preprocessor is awkward.
-   * <p>
-   * @deprecated Java 10 is around the corner. Use javaPlatform when you need
-   * a number for comparisons, i.e. "if (javaPlatform >= 7)".
+   * Do not use; javaPlatform or javaVersionName are better options.
+   * For instance, javaPlatform is useful when you need a number for
+   * comparison, i.e. "if (javaPlatform >= 9)".
+   * @deprecated
    */
   @Deprecated
-  public static final float javaVersion =
-    new Float(javaVersionName.substring(0, 3));
-//  public static final float javaVersion =
-//    new Float(javaVersionName.substring(0, javaVersionName.indexOf(".", 2))).floatValue();
-//  // Making this a String in 3.0, in anticipation of Java 10
-//  public static final String javaVersion = "1." + javaPlatform;
+  public static final float javaVersion = 1 + javaPlatform / 10f;
 
   /**
    * Current platform in use, one of the
