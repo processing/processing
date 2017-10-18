3ebbfe5249f589afc9386f16173006e615450c27
Ben Fry <ben@fathom.info>
2017-10-17 23:09:17 -0400

    fix minor spacing issue with indent

--- core/src/processing/javafx/PSurfaceFX.java
+++ core/src/processing/javafx/PSurfaceFX.java
@@ -272,7 +272,7 @@ public class PSurfaceFX implements PSurface {
           displayDevice = devices[displayNum - 1];
         } else {
           System.err.format("Display %d does not exist, " +
-                                "using the default display instead.%n", displayNum);
+                            "using the default display instead.%n", displayNum);
           for (int i = 0; i < devices.length; i++) {
             System.err.format("Display %d is %s%n", (i+1), devices[i]);
           }
