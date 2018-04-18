f618b9213aef1f3d7f8afebd2e57292daf2fcd7b
gohai <gottfried.haider@gmail.com>
2018-04-17 18:33:51 -0700

    ARM Mali: Implement gDrawBuffer for gl3es3
    I don't know IntBuffer and OpenGL well enought to say if this will leak memory or similar.

--- core/src/processing/opengl/PJOGL.java
+++ core/src/processing/opengl/PJOGL.java
@@ -1973,6 +1973,11 @@ public class PJOGL extends PGL {
       gl2x.glDrawBuffer(buf);
     } else if (gl3 != null) {
       gl3.glDrawBuffer(buf);
+    } else if (gl3es3 != null) {
+      IntBuffer intBuffer = IntBuffer.allocate(1);
+      intBuffer.put(buf);
+      intBuffer.rewind();
+      gl3es3.glDrawBuffers(1, intBuffer);
     } else {
       throw new RuntimeException(String.format(MISSING_GLFUNC_ERROR, "glDrawBuffer()"));
     }
