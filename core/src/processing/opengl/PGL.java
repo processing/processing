ed5524cb45ae00ff3253b709a9bf79fb5a4717c1
gohai <gottfried.haider@gmail.com>
2018-04-18 07:48:28 -0700

    ARM Mali: Don't assume anisotropic filtering for gl3es3
    Fixes:
    GLDebugEvent[ id 0xb
        type Error
        severity High: dangerous undefined behavior
        source GL API
        msg Error:glGetFloatv::<pname> is not an accepted value
        when 1524036153380
        source 3.1 (ES profile, arb, debug, compat[ES2, ES3, ES31], FBO, hardware) - OpenGL ES 3.1 v1.r17p0-01rel0.a881d28363cdb20f0017ed13c980967e - hash 0xc39cf5]
        [2]: jogamp.opengl.GLDebugMessageHandler$StdErrGLDebugListener.messageSent(GLDebugMessageHandler.java:347)
        [3]: jogamp.opengl.GLDebugMessageHandler.sendMessage(GLDebugMessageHandler.java:332)
        [4]: jogamp.opengl.GLDebugMessageHandler.glDebugMessageARB(GLDebugMessageHandler.java:358)
        [5]: jogamp.opengl.es3.GLES3Impl.dispatch_glGetFloatv1(Native Method)
        [6]: jogamp.opengl.es3.GLES3Impl.glGetFloatv(GLES3Impl.java:1157)
        [7]: com.jogamp.opengl.DebugGLES3.glGetFloatv(DebugGLES3.java:3260)
        [8]: processing.opengl.PJOGL.getFloatv(PJOGL.java:1106)
        [9]: processing.opengl.PGraphicsOpenGL.getGLParameters(PGraphicsOpenGL.java:6892)
        [10]: processing.opengl.PGraphicsOpenGL.beginDraw(PGraphicsOpenGL.java:1454)
        [11]: processing.core.PApplet.handleDraw(PApplet.java:2388)
        [12]: processing.opengl.PSurfaceJOGL$DrawListener.display(PSurfaceJOGL.java:866)
        [13]: jogamp.opengl.GLDrawableHelper.displayImpl(GLDrawableHelper.java:692)
        [14]: jogamp.opengl.GLDrawableHelper.display(GLDrawableHelper.java:674)
        [15]: jogamp.opengl.GLAutoDrawableBase$2.run(GLAutoDrawableBase.java:443)
        [16]: jogamp.opengl.GLDrawableHelper.invokeGLImpl(GLDrawableHelper.java:1293)
        [17]: jogamp.opengl.GLDrawableHelper.invokeGL(GLDrawableHelper.java:1147)
        [18]: com.jogamp.newt.opengl.GLWindow.display(GLWindow.java:759)
        [19]: com.jogamp.opengl.util.AWTAnimatorImpl.display(AWTAnimatorImpl.java:81)
        [20]: com.jogamp.opengl.util.AnimatorBase.display(AnimatorBase.java:452)
        [21]: com.jogamp.opengl.util.FPSAnimator$MainTask.run(FPSAnimator.java:178)
        [22]: java.util.TimerThread.mainLoop(Timer.java:555)
        [23]: java.util.TimerThread.run(Timer.java:505)

--- core/src/processing/opengl/PGL.java
+++ core/src/processing/opengl/PGL.java
@@ -2232,7 +2232,7 @@ public abstract class PGL {
 
   protected boolean hasAnisoSamplingSupport() {
     int major = getGLVersion()[0];
-    if (major < 3) {
+    if (isES() || major < 3) {
       String ext = getString(EXTENSIONS);
       return -1 < ext.indexOf("_texture_filter_anisotropic");
     } else {
