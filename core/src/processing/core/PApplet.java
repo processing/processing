fa91706bf23c4080884b56c0a62b468a15e5d047
Ben Fry <ben@fathom.info>
2018-05-17 16:18:45 -0400

    remove unused import

--- core/src/processing/core/PApplet.java
+++ core/src/processing/core/PApplet.java
@@ -66,7 +66,6 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.ThreadFactory;
-import java.util.concurrent.atomic.AtomicInteger;
 import java.util.regex.*;
 import java.util.zip.*;
 
