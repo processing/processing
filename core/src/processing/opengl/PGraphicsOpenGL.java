a0f7665edd4e7b354eede9c4bc011deda8bbe947
codeanticode <andres.colubri@gmail.com>
2019-01-10 08:19:13 -0500

    check if pixels is null

--- core/src/processing/opengl/PGraphicsOpenGL.java
+++ core/src/processing/opengl/PGraphicsOpenGL.java
@@ -6429,7 +6429,7 @@ public class PGraphicsOpenGL extends PGraphics {
     if (tex == null || tex.contextIsOutdated()) {
       tex = addTexture(img);
       if (tex != null) {
-        boolean dispose = !img.loaded;
+        boolean dispose = img.pixels == null;
         img.loadPixels();
         tex.set(img.pixels, img.format);
         img.setModified();
