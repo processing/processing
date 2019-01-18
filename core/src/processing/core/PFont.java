1c8b05666d6fac801297b7ecfdcd433f79d3cc5e
Ben Fry <ben@fathom.info>
2019-01-18 15:59:15 -0800

    more notes about font issues and performance

--- core/src/processing/core/PFont.java
+++ core/src/processing/core/PFont.java
@@ -894,15 +894,19 @@ public class PFont implements PConstants {
 
 
   /**
-   * Make an internal list of all installed fonts. This can take a while with
-   * a lot of fonts installed, but running it on a separate thread may not
-   * help much. As of the commit that's adding this note, loadFonts() will
-   * only be called by PFont.list() and when loading a font by name, both of
-   * which are occasions when we'd need to block until this was finished
-   * anyway. It's also possible that running getAllFonts() on a non-EDT thread
-   * could cause graphics system issues. Further, the first fonts are usually
-   * loaded at the beginning of a sketch, meaning that sketch startup time
-   * will still be affected, even with threading (and blocking) in place.
+   * Make an internal list of all installed fonts.
+   *
+   * This can take a while with a lot of fonts installed, but running it on
+   * a separate thread may not help much. As of the commit that's adding this
+   * note, loadFonts() will only be called by PFont.list() and when loading a
+   * font by name, both of which are occasions when we'd need to block until
+   * this was finished anyway. It's also possible that running getAllFonts()
+   * on a non-EDT thread could cause graphics system issues. Further, the first
+   * fonts are usually loaded at the beginning of a sketch, meaning that sketch
+   * startup time will still be affected, even with threading in place.
+   *
+   * Where we're getting killed on font performance is due to this bug:
+   * https://bugs.openjdk.java.net/browse/JDK-8179209
    */
   static public void loadFonts() {
     if (fonts == null) {
