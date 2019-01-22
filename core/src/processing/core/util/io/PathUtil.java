58539d8392e021a611cef59a5998d92757010929
A Pottinger <samp@gleap.org>
2019-01-22 15:39:45 -0700

    Started testing in core.util.io

--- core/src/processing/core/util/io/PathUtil.java
+++ core/src/processing/core/util/io/PathUtil.java
@@ -1,10 +1,44 @@
+/* -*- mode: java; c-basic-offset: 2; indent-tabs-mode: nil -*- */
+
+/*
+  Part of the Processing project - http://processing.org
+
+  Copyright (c) 2004-11 Ben Fry and Casey Reas
+  Copyright (c) 2001-04 Massachusetts Institute of Technology
+
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 2.1 of the License, or (at your option) any later version.
+
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General
+  Public License along with this library; if not, write to the
+  Free Software Foundation, Inc., 59 Temple Place, Suite 330,
+  Boston, MA  02111-1307  USA
+*/
+
 package processing.core.util.io;
 
 import java.io.File;
 
+
+/**
+ * Convenience functions for managing file paths.
+ */
 public class PathUtil {
   private static final String UNKNOWN_FILE_EXTENSION = "unknown";
 
+  /**
+   * Determine the extension of a file given in a path.
+   *
+   * @param path The path like directory/subdirectory/image.png.
+   * @return The extension of the file at path like "png" or "unknown" if could not be determined.
+   */
   public static String parseExtension(String path) {
     String lower = path.toLowerCase();
     String extension;
@@ -27,14 +61,39 @@ public class PathUtil {
     return extension;
   }
 
+  /**
+   * Clean up irregularities like leading period or capitalization from an extension string.
+   *
+   * @param extension The extension to clean up like ".PNG".
+   * @return The cleaned up extension like "png".
+   */
   public static String cleanExtension(String extension) {
-    return extension == null ? UNKNOWN_FILE_EXTENSION : extension.toLowerCase();
+    extension = extension == null ? UNKNOWN_FILE_EXTENSION : extension.toLowerCase();
+
+    if (extension.startsWith(".")) {
+      extension = extension.substring(1);
+    }
+
+    return extension;
   }
 
-  static public void createPath(String path) {
+  /**
+   * Create the directories so that files can be created at a path.
+   *
+   * @param path The path like folder1/folder2/image.png. In this example, folder1 and folder2 would
+   *    both be created if not already existing.
+   */
+  public static void createPath(String path) {
     createPath(new File(path));
   }
 
+  /**
+   * Create the directories so that files can be created at a path.
+   *
+   * @param file The file describing the path that should be created. A path of
+   *    folder1/folder2/image.png would cause folder1 and folder2 to be created if not already
+   *    existing.
+   */
   public static void createPath(File file) {
     try {
       String parent = file.getParent();
