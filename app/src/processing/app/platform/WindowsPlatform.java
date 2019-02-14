18930dbff6f1fcc332736c408d1d2fa6762daa11
A Pottinger <samp@gleap.org>
2019-02-14 13:21:35 -0800

    Fixed Swing high DPI scaling issues on Windows with OpenJDK 11.
    As part of #5753, JEP 263 (introduced in JDK 9), offers native support for HiDPI scaling on Linux and Windows. THis causes both the JDK to naturally scale elements on HiHPI displays and then Processing again scales on its own under the assumption of Java 8 behavior, causing 2x the "correct" scale. This change migrates WindowsPlatform to simply allow the JDK to do all scaling.

--- app/src/processing/app/platform/WindowsPlatform.java
+++ app/src/processing/app/platform/WindowsPlatform.java
@@ -64,6 +64,10 @@ public class WindowsPlatform extends DefaultPlatform {
     "\\" + APP_NAME.toLowerCase() + ".exe \"%1\"";
   static final String REG_DOC = APP_NAME + ".Document";
 
+  // Starting with Java 9, the scaling is done automatically. If DPI is
+  // used to scaling within the application, one ends up with 2x the
+  // expected scale. See JEP 263.
+  private static final int WINDOWS_NATIVE_DPI = 96;
 
   public void initBase(Base base) {
     super.initBase(base);
@@ -627,56 +631,8 @@ public class WindowsPlatform extends DefaultPlatform {
   // . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .
 
 
-  // Need to extend com.sun.jna.platform.win32.User32 to access
-  // Win32 function GetDpiForSystem()
-  interface ExtUser32 extends StdCallLibrary, com.sun.jna.platform.win32.User32 {
-    ExtUser32 INSTANCE = (ExtUser32) Native.loadLibrary("user32", ExtUser32.class, W32APIOptions.DEFAULT_OPTIONS);
-
-    public int GetDpiForSystem();
-
-    public int SetProcessDpiAwareness(int value);
-
-    public final int DPI_AWARENESS_INVALID = -1;
-    public final int DPI_AWARENESS_UNAWARE = 0;
-    public final int DPI_AWARENESS_SYSTEM_AWARE = 1;
-    public final int DPI_AWARENESS_PER_MONITOR_AWARE = 2;
-
-    public Pointer SetThreadDpiAwarenessContext(Pointer dpiContext);
-
-    public final Pointer DPI_AWARENESS_CONTEXT_UNAWARE = new Pointer(-1);
-    public final Pointer DPI_AWARENESS_CONTEXT_SYSTEM_AWARE = new Pointer(-2);
-    public final Pointer DPI_AWARENESS_CONTEXT_PER_MONITOR_AWARE = new Pointer(-3);
-  }
-
-
-  static private int detected = detectSystemDPI();
-
-
   public int getSystemDPI() {
-    if (detected == -1) {
-      return super.getSystemDPI();
-    }
-    return detected;
+    return WINDOWS_NATIVE_DPI;
   }
 
-
-  public static int detectSystemDPI() {
-    try {
-      ExtUser32.INSTANCE.SetProcessDpiAwareness(ExtUser32.DPI_AWARENESS_SYSTEM_AWARE);
-    } catch (Throwable e) {
-      // Ignore error
-    }
-    try {
-      ExtUser32.INSTANCE.SetThreadDpiAwarenessContext(ExtUser32.DPI_AWARENESS_CONTEXT_SYSTEM_AWARE);
-    } catch (Throwable e) {
-      // Ignore error (call valid only on Windows 10)
-    }
-    try {
-      return ExtUser32.INSTANCE.GetDpiForSystem();
-    } catch (Throwable e) {
-      // DPI detection failed, fall back with default
-      System.out.println("DPI detection failed, fallback to 96 dpi");
-      return -1;
-    }
-  }
 }
